3d62f102d493a222b20624c44aa50918e67c45d5
Ivan Vecera <ivecera@redhat.com>
2017-10-12 15:26:28 -0400

    [net] ipv6: fib_rules: Check if rule is a default rule
    Message-id: <20171012152638.24411-12-ivecera@redhat.com>
    Patchwork-id: 191769
    O-Subject: [RHEL7.5 net PATCH 11/21] ipv6: fib_rules: Check if rule is a default rule
    Bugzilla: 1500886
    RH-Acked-by: Eric Garver <egarver@redhat.com>
    RH-Acked-by: Sabrina Dubroca <sdubroca@redhat.com>
    RH-Acked-by: Hangbin Liu <haliu@redhat.com>
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    
    BZ#1500886
    
    Backport note:
    - We need to include explictly net/fib_rules.h in net/ip6_fib to make
      'struct fib_rule' known. In upstream this files is included
      indirectly via net/l3mdev.h that is missing in RHEL. This explicit
      include needs to be wrapped by __GENKSYMS__ #ifdef as it causes
      checksum changes of several whitelisted symbols
    - Due to missing VRF in RHEL we need to comment the reference to
      'rule->l3mdev' out
    
    Upstream commit(s):
    commit e3ea973159d53559c5ae9a9dbc824da9aba6cac0
    Author: Ido Schimmel <idosch@mellanox.com>
    Date:   Thu Aug 3 13:28:15 2017 +0200
    
        ipv6: fib_rules: Check if rule is a default rule
    
        As explained in commit 3c71006d15fd ("ipv4: fib_rules: Check if rule is
        a default rule"), drivers supporting IPv6 FIB offload need to be able to
        sanitize the rules they don't support and potentially flush their
        tables.
    
        Add an IPv6 helper to check if a FIB rule is a default rule.
    
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/net/ip6_fib.h
+++ include/net/ip6_fib.h
@@ -20,6 +20,9 @@
 #include <net/flow.h>
 #include <net/netlink.h>
 #include <net/inetpeer.h>
+#ifndef __GENKSYMS__
+#include <net/fib_rules.h>
+#endif
 
 #ifdef CONFIG_IPV6_MULTIPLE_TABLES
 #define FIB6_TABLE_HASHSZ 256
@@ -298,6 +301,7 @@ extern int			ipv6_route_open(struct inode *inode,
 #ifdef CONFIG_IPV6_MULTIPLE_TABLES
 int fib6_rules_init(void);
 void fib6_rules_cleanup(void);
+bool fib6_rule_default(const struct fib_rule *rule);
 #else
 static inline int               fib6_rules_init(void)
 {
@@ -306,6 +310,10 @@ static inline int               fib6_rules_init(void)
 static inline void              fib6_rules_cleanup(void)
 {
 	return ;
+}
+static inline bool fib6_rule_default(const struct fib_rule *rule)
+{
+	return true;
 }
 #endif
 #endif
