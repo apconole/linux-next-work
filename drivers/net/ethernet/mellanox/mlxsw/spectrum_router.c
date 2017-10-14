fa6082b7cc85f79a830f271e445a6eb9253c3933
Ivan Vecera <ivecera@redhat.com>
2017-10-13 20:58:33 -0400

    [netdrv] mlxsw: spectrum_router: Move VRF refcounting
    Message-id: <20171013205835.26049-244-ivecera@redhat.com>
    Patchwork-id: 193596
    O-Subject: [RHEL7.5 PATCH 243/245] mlxsw: spectrum_router: Move VRF refcounting
    Bugzilla: 1486860
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    BZ#1486860
    
    Upstream commit(s):
    commit 28a04c7b7bbecaab642fcb6a2d7354eb70ea7fbe
    Author: Petr Machata <petrm@mellanox.com>
    Date:   Mon Oct 2 12:14:56 2017 +0200
    
        mlxsw: spectrum_router: Move VRF refcounting
    
        When creating a new RIF, bumping RIF count of the containing VR is the
        last thing to be done. Symmetrically, when destroying a RIF, RIF count
        is first dropped and only then the rest of the cleanup proceeds.
    
        That's a problem for loopback RIFs. Those hold two VR references: one
        for overlay and one for underlay. mlxsw_sp_rif_destroy() releases the
        overlay one, and the deconfigure() callback the underlay one. But if
        both overlay and underlay are the same, and if there are no other
        artifacts holding the VR alive, this put actually destroys the VR. Later
        on, when mlxsw_sp_rif_destroy() calls mlxsw_sp_vr_put() for the same VR,
        the VR will already have been released and the kernel crashes with NULL
        pointer dereference.
    
        The underlying problem is that the RIF under destruction ends up
        referencing the overlay VR much longer than it claims: all the way until
        the call to mlxsw_sp_vr_put(). So line up the reference counting
        properly to reflect this. Make corresponding changes in
        mlxsw_sp_rif_create() as well for symmetry.
    
        Fixes: 6ddb7426a7d4 ("mlxsw: spectrum_router: Introduce loopback RIFs")
        Signed-off-by: Petr Machata <petrm@mellanox.com>
        Reviewed-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -4942,6 +4942,7 @@ mlxsw_sp_rif_create(struct mlxsw_sp *mlxsw_sp,
 	vr = mlxsw_sp_vr_get(mlxsw_sp, tb_id ? : RT_TABLE_MAIN);
 	if (IS_ERR(vr))
 		return ERR_CAST(vr);
+	vr->rif_count++;
 
 	err = mlxsw_sp_rif_index_alloc(mlxsw_sp, &rif_index);
 	if (err)
@@ -4973,7 +4974,6 @@ mlxsw_sp_rif_create(struct mlxsw_sp *mlxsw_sp,
 
 	mlxsw_sp_rif_counters_alloc(rif);
 	mlxsw_sp->router->rifs[rif_index] = rif;
-	vr->rif_count++;
 
 	return rif;
 
@@ -4984,6 +4984,7 @@ err_fid_get:
 	kfree(rif);
 err_rif_alloc:
 err_rif_index_alloc:
+	vr->rif_count--;
 	mlxsw_sp_vr_put(vr);
 	return ERR_PTR(err);
 }
@@ -4998,7 +4999,6 @@ void mlxsw_sp_rif_destroy(struct mlxsw_sp_rif *rif)
 	mlxsw_sp_router_rif_gone_sync(mlxsw_sp, rif);
 	vr = &mlxsw_sp->router->vrs[rif->vr_id];
 
-	vr->rif_count--;
 	mlxsw_sp->router->rifs[rif->rif_index] = NULL;
 	mlxsw_sp_rif_counters_free(rif);
 	ops->deconfigure(rif);
@@ -5006,6 +5006,7 @@ void mlxsw_sp_rif_destroy(struct mlxsw_sp_rif *rif)
 		/* Loopback RIFs are not associated with a FID. */
 		mlxsw_sp_fid_put(fid);
 	kfree(rif);
+	vr->rif_count--;
 	mlxsw_sp_vr_put(vr);
 }
 
