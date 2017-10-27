09f7abc68d1d3e7bcd964928497b5b9f36b5df12
David Howells <dhowells@redhat.com>
2017-10-26 22:03:38 -0400

    [security] keys: encrypted: use constant-time HMAC comparison
    Message-id: <150905541835.6336.12674861901837882739.stgit@warthog.procyon.org.uk>
    Patchwork-id: 195195
    O-Subject: [RHEL7.5 PATCH 30/55] KEYS: encrypted: use constant-time HMAC comparison
    Bugzilla: 1503242
    RH-Acked-by: Herton R. Krzesinski <herton@redhat.com>
    RH-Acked-by: Bruno Eduardo de Oliveira Meneguele <brdeoliv@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Herbert Xu <herbert.xu@redhat.com>
    
    Bugzilla: 1503242
    Upstream Status: Linus
    Build Info: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=14378351
    Tested: keyutils testsuite and all LTP key and keyctl tests
    
    commit 0f534e4a13496b02ae284f50fcb0263f6ea37007
    Author: Eric Biggers <ebiggers@google.com>
    Date:   Thu Jun 8 14:48:32 2017 +0100
    
        KEYS: encrypted: use constant-time HMAC comparison
    
        MACs should, in general, be compared using crypto_memneq() to prevent
        timing attacks.
    
        Cc: Mimi Zohar <zohar@linux.vnet.ibm.com>
        Cc: Herbert Xu <herbert@gondor.apana.org.au>
        Signed-off-by: Eric Biggers <ebiggers@google.com>
        Signed-off-by: David Howells <dhowells@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- security/keys/encrypted-keys/encrypted.c
+++ security/keys/encrypted-keys/encrypted.c
@@ -30,6 +30,7 @@
 #include <linux/scatterlist.h>
 #include <linux/crypto.h>
 #include <linux/ctype.h>
+#include <crypto/algapi.h>
 #include <crypto/hash.h>
 #include <crypto/sha.h>
 #include <crypto/aes.h>
@@ -523,8 +524,8 @@ static int datablob_hmac_verify(struct encrypted_key_payload *epayload,
 	ret = calc_hmac(digest, derived_key, sizeof derived_key, p, len);
 	if (ret < 0)
 		goto out;
-	ret = memcmp(digest, epayload->format + epayload->datablob_len,
-		     sizeof digest);
+	ret = crypto_memneq(digest, epayload->format + epayload->datablob_len,
+			    sizeof(digest));
 	if (ret) {
 		ret = -EINVAL;
 		dump_hmac("datablob",
