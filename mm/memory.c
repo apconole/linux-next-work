f43cad29994d434cf91ba578fc0d791b854d92b3
Larry Woodman <lwoodman@redhat.com>
2017-11-10 21:46:34 -0500

    [mm] dax: wrprotect pmd_t in dax_mapping_entry_mkclean
    Message-id: <1510350394-22203-5-git-send-email-lwoodman@redhat.com>
    Patchwork-id: 197566
    O-Subject: [RHEL7.5 PATCH 4/4] dax: wrprotect pmd_t in dax_mapping_entry_mkclean
    Bugzilla: 1457572
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    commit 286ce36f47cd76d0c9c52eb46bf6079022c782c8
    Author: Ross Zwisler <ross.zwisler@linux.intel.com>
    Date:   Wed Nov 1 11:46:22 2017 -0600
    
        dax: wrprotect pmd_t in dax_mapping_entry_mkclean
    
        commit f729c8c9b24f0540a6e6b86e68f3888ba90ef7e7
        Author: Ross Zwisler <ross.zwisler@linux.intel.com>
        Date:   Tue Jan 10 16:57:24 2017 -0800
    
            dax: wrprotect pmd_t in dax_mapping_entry_mkclean
    
            Currently dax_mapping_entry_mkclean() fails to clean and write protect
            the pmd_t of a DAX PMD entry during an *sync operation.  This can result
            in data loss in the following sequence:
    
            1) mmap write to DAX PMD, dirtying PMD radix tree entry and making the
               pmd_t dirty and writeable
            2) fsync, flushing out PMD data and cleaning the radix tree entry. We
               currently fail to mark the pmd_t as clean and write protected.
            3) more mmap writes to the PMD.  These don't cause any page faults since
               the pmd_t is dirty and writeable.  The radix tree entry remains clean.
            4) fsync, which fails to flush the dirty PMD data because the radix tree
               entry was clean.
            5) crash - dirty data that should have been fsync'd as part of 4) could
               still have been in the processor cache, and is lost.
    
            Fix this by marking the pmd_t clean and write protected in
            dax_mapping_entry_mkclean(), which is called as part of the fsync
            operation 2).  This will cause the writes in step 3) above to generate
            page faults where we'll re-dirty the PMD radix tree entry, resulting in
            flushes in the fsync that happens in step 4).
    
            Fixes: 4b4bb46d00b3 ("dax: clear dirty entry tags on cache flush")
            Link: http://lkml.kernel.org/r/1482272586-21177-3-git-send-email-ross.zwisler@linux.intel.com
            Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
            Reviewed-by: Jan Kara <jack@suse.cz>
            Cc: Alexander Viro <viro@zeniv.linux.org.uk>
            Cc: Christoph Hellwig <hch@lst.de>
            Cc: Dan Williams <dan.j.williams@intel.com>
            Cc: Dave Chinner <david@fromorbit.com>
            Cc: Jan Kara <jack@suse.cz>
            Cc: Matthew Wilcox <mawilcox@microsoft.com>
            Cc: Dave Hansen <dave.hansen@intel.com>
            Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
            Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3632,8 +3632,8 @@ out:
 	return -EINVAL;
 }
 
-int follow_pte(struct mm_struct *mm, unsigned long address, pte_t **ptepp,
-	       spinlock_t **ptlp)
+static inline int follow_pte(struct mm_struct *mm, unsigned long address,
+			     pte_t **ptepp, spinlock_t **ptlp)
 {
 	int res;
 
