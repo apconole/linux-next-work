86f6e6e79dea1e2e34ee770e7c4ddfeb3667708c
Jerome Marchand <jmarchan@redhat.com>
2017-11-14 14:41:42 -0500

    [kernel] tracing: Call clear_boot_tracer() at lateinit_sync
    Message-id: <20171114144142.19109-1-jmarchan@redhat.com>
    Patchwork-id: 197897
    O-Subject: [RHEL7.5 PATCH] tracing: Call clear_boot_tracer() at lateinit_sync
    Bugzilla: 1477115
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla:
    https://bugzilla.redhat.com/show_bug.cgi?id=1477115
    
    Brew build:
    https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14538652
    
    This is a backport of:
    commit 4bb0f0e73c8c30917d169c4a0f1ac083690c545b
    Author: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Date:   Tue Aug 1 12:01:52 2017 -0400
    
        tracing: Call clear_boot_tracer() at lateinit_sync
    
        The clear_boot_tracer function is used to reset the default_bootup_tracer
        string to prevent it from being accessed after boot, as it originally points
        to init data. But since clear_boot_tracer() is called via the
        init_lateinit() call, it races with the initcall for registering the hwlat
        tracer. If someone adds "ftrace=hwlat" to the kernel command line, depending
        on how the linker sets up the text, the saved command line may be cleared,
        and the hwlat tracer never is initialized.
    
        Simply have the clear_boot_tracer() be called by initcall_lateinit_sync() as
        that's for tasks to be called after lateinit.
    
        Link: https://bugzilla.kernel.org/show_bug.cgi?id=196551
    
        Cc: stable@vger.kernel.org
        Fixes: e7c15cd8a ("tracing: Added hardware latency tracer")
        Reported-by: Zamir SUN <sztsian@gmail.com>
        Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -6718,4 +6718,4 @@ __init static int clear_boot_tracer(void)
 
 early_initcall(tracer_alloc_buffers);
 fs_initcall(tracer_init_debugfs);
-late_initcall(clear_boot_tracer);
+late_initcall_sync(clear_boot_tracer);
