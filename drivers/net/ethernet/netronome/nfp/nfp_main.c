1ce593bdeb0ae612039e9cd38f0b19d1ee0163de
John Linville <linville@redhat.com>
2017-11-14 20:06:58 -0500

    [netdrv] nfp: move mutex init out of net code
    Message-id: <79fe06f1794dfa2a251116375c457331ad15963a.1510687152.git.linville@redhat.com>
    Patchwork-id: 198030
    O-Subject: [RHEL7 PATCH 104/206] nfp: move mutex init out of net code
    Bugzilla: 1454745 1454760
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454745
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454760
    
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14544377
    
    Move mutex init to main file close to structure allocation.
    This will allow mutex to be taken before net code runs (e.g.
    from devlink callbacks).  While at it remember to destroy
    the mutex.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 346cfe8482bf491f6e3e88ea89bc0d6be2b02efd)
    Signed-off-by: John W. Linville <linville@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/netronome/nfp/nfp_main.c
+++ drivers/net/ethernet/netronome/nfp/nfp_main.c
@@ -41,6 +41,7 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/pci.h>
 #include <linux/firmware.h>
 #include <linux/vermagic.h>
@@ -341,6 +342,7 @@ static int nfp_pci_probe(struct pci_dev *pdev,
 	}
 	INIT_LIST_HEAD(&pf->vnics);
 	INIT_LIST_HEAD(&pf->ports);
+	mutex_init(&pf->lock);
 	pci_set_drvdata(pdev, pf);
 	pf->pdev = pdev;
 
@@ -379,6 +381,7 @@ err_cpp_free:
 	nfp_cpp_free(pf->cpp);
 err_disable_msix:
 	pci_set_drvdata(pdev, NULL);
+	mutex_destroy(&pf->lock);
 	kfree(pf);
 err_rel_regions:
 	pci_release_regions(pdev);
@@ -403,6 +406,7 @@ static void nfp_pci_remove(struct pci_dev *pdev)
 	nfp_cpp_free(pf->cpp);
 
 	kfree(pf->eth_tbl);
+	mutex_destroy(&pf->lock);
 	kfree(pf);
 	pci_release_regions(pdev);
 	pci_disable_device(pdev);
