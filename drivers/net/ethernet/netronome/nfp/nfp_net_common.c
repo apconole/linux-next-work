116f24fc97e19a7408b2b8a67b4168cbd3ab9d67
John Linville <linville@redhat.com>
2017-11-14 20:07:48 -0500

    [netdrv] nfp: bpf: release the reference on offloaded programs
    Message-id: <8e28eb41dbae558260923cd8699d6bbd9df61e3c.1510687152.git.linville@redhat.com>
    Patchwork-id: 198061
    O-Subject: [RHEL7 PATCH 154/206] nfp: bpf: release the reference on offloaded programs
    Bugzilla: 1454760 1454745
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454745
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454760
    
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14544377
    
    The xdp_prog member of the adapter's data path structure is used
    for XDP in driver mode.  In case a XDP program is loaded with in
    HW-only mode, we need to store it somewhere else.  Add a new XDP
    prog pointer in the main structure and use that when we need to
    know whether any XDP program is loaded, not only a driver mode
    one.  Only release our reference on adapter free instead of
    immediately after netdev unregister to allow offload to be disabled
    first.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 6a8ef5428c9fb5b589c2c912889d157abb50dd61)
    Signed-off-by: John W. Linville <linville@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/netronome/nfp/nfp_net_common.c
+++ drivers/net/ethernet/netronome/nfp/nfp_net_common.c
@@ -3286,15 +3286,10 @@ static int
 nfp_net_xdp_setup_drv(struct nfp_net *nn, struct bpf_prog *prog,
 		      struct netlink_ext_ack *extack)
 {
-	struct bpf_prog *old_prog = nn->dp.xdp_prog;
 	struct nfp_net_dp *dp;
-	int err;
 
-	if (!prog && !nn->dp.xdp_prog)
-		return 0;
-	if (prog && nn->dp.xdp_prog) {
-		prog = xchg(&nn->dp.xdp_prog, prog);
-		bpf_prog_put(prog);
+	if (!prog == !nn->dp.xdp_prog) {
+		WRITE_ONCE(nn->dp.xdp_prog, prog);
 		return 0;
 	}
 
@@ -3308,14 +3303,7 @@ nfp_net_xdp_setup_drv(struct nfp_net *nn, struct bpf_prog *prog,
 	dp->rx_dma_off = prog ? XDP_PACKET_HEADROOM - nn->dp.rx_offset : 0;
 
 	/* We need RX reconfig to remap the buffers (BIDIR vs FROM_DEV) */
-	err = nfp_net_ring_reconfig(nn, dp, extack);
-	if (err)
-		return err;
-
-	if (old_prog)
-		bpf_prog_put(old_prog);
-
-	return 0;
+	return nfp_net_ring_reconfig(nn, dp, extack);
 }
 
 static int
@@ -3325,7 +3313,7 @@ nfp_net_xdp_setup(struct nfp_net *nn, struct bpf_prog *prog, u32 flags,
 	struct bpf_prog *offload_prog;
 	int err;
 
-	if (nn->dp.xdp_prog && (flags ^ nn->xdp_flags) & XDP_FLAGS_MODES)
+	if (nn->xdp_prog && (flags ^ nn->xdp_flags) & XDP_FLAGS_MODES)
 		return -EBUSY;
 
 	offload_prog = flags & XDP_FLAGS_DRV_MODE ? NULL : prog;
@@ -3335,6 +3323,10 @@ nfp_net_xdp_setup(struct nfp_net *nn, struct bpf_prog *prog, u32 flags,
 		return err;
 
 	nfp_app_xdp_offload(nn->app, nn, offload_prog);
+
+	if (nn->xdp_prog)
+		bpf_prog_put(nn->xdp_prog);
+	nn->xdp_prog = prog;
 	nn->xdp_flags = flags;
 
 	return 0;
@@ -3351,8 +3343,8 @@ static int nfp_net_xdp(struct net_device *netdev, struct netdev_xdp *xdp)
 		return nfp_net_xdp_setup(nn, xdp->prog, xdp->flags,
 					 xdp->extack);
 	case XDP_QUERY_PROG:
-		xdp->prog_attached = !!nn->dp.xdp_prog;
-		xdp->prog_id = nn->dp.xdp_prog ? nn->dp.xdp_prog->aux->id : 0;
+		xdp->prog_attached = !!nn->xdp_prog;
+		xdp->prog_id = nn->xdp_prog ? nn->xdp_prog->aux->id : 0;
 		return 0;
 	default:
 		return -EINVAL;
@@ -3514,6 +3506,9 @@ struct nfp_net *nfp_net_alloc(struct pci_dev *pdev, bool needs_netdev,
  */
 void nfp_net_free(struct nfp_net *nn)
 {
+	if (nn->xdp_prog)
+		bpf_prog_put(nn->xdp_prog);
+
 	if (nn->dp.netdev)
 		free_netdev(nn->dp.netdev);
 	else
