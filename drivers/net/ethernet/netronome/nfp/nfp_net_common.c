08cb6a3847c557a17c80a3fba3ec676407416605
John Linville <linville@redhat.com>
2017-11-14 20:06:51 -0500

    [netdrv] nfp: introduce nfp_port
    Message-id: <0bd825b97571bb31a42cf4160fe2af21c477b6f1.1510687152.git.linville@redhat.com>
    Patchwork-id: 197976
    O-Subject: [RHEL7 PATCH 097/206] nfp: introduce nfp_port
    Bugzilla: 1454760 1454745
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454745
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1454760
    
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14544377
    
    Encapsulate port information into struct nfp_port.  nfp_port will
    soon be extended to contain devlink_port information.  It also makes
    it easier to reuse port-related code between vNICs and representors.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit eb488c26d713b2a9ebba6c12bbefd04e01197693)
    Signed-off-by: John W. Linville <linville@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/net/ethernet/netronome/nfp/nfp_net_common.c
+++ drivers/net/ethernet/netronome/nfp/nfp_net_common.c
@@ -70,6 +70,7 @@
 #include "nfpcore/nfp_nsp.h"
 #include "nfp_net_ctrl.h"
 #include "nfp_net.h"
+#include "nfp_port.h"
 
 /**
  * nfp_net_get_fw_version() - Read and parse the FW version
@@ -2856,26 +2857,6 @@ nfp_net_features_check(struct sk_buff *skb, struct net_device *dev,
 	return features;
 }
 
-static int
-nfp_net_get_phys_port_name(struct net_device *netdev, char *name, size_t len)
-{
-	struct nfp_net *nn = netdev_priv(netdev);
-	int err;
-
-	if (!nn->eth_port)
-		return -EOPNOTSUPP;
-
-	if (!nn->eth_port->is_split)
-		err = snprintf(name, len, "p%d", nn->eth_port->label_port);
-	else
-		err = snprintf(name, len, "p%ds%d", nn->eth_port->label_port,
-			       nn->eth_port->label_subport);
-	if (err >= len)
-		return -EINVAL;
-
-	return 0;
-}
-
 /**
  * nfp_net_set_vxlan_port() - set vxlan port in SW and reconfigure HW
  * @nn:   NFP Net device to reconfigure
@@ -3037,7 +3018,7 @@ static int nfp_net_xdp(struct net_device *netdev, struct netdev_xdp *xdp)
 	}
 }
 
-static const struct net_device_ops nfp_net_netdev_ops = {
+const struct net_device_ops nfp_net_netdev_ops = {
 	.ndo_size		= sizeof(struct net_device_ops),
 	.ndo_open		= nfp_net_netdev_open,
 	.ndo_stop		= nfp_net_netdev_close,
@@ -3050,7 +3031,7 @@ static const struct net_device_ops nfp_net_netdev_ops = {
 	.ndo_set_mac_address	= eth_mac_addr,
 	.ndo_set_features	= nfp_net_set_features,
 	.ndo_features_check	= nfp_net_features_check,
-	.extended.ndo_get_phys_port_name	= nfp_net_get_phys_port_name,
+	.extended.ndo_get_phys_port_name	= nfp_port_get_phys_port_name,
 	.extended.ndo_udp_tunnel_add	= nfp_net_add_vxlan_port,
 	.extended.ndo_udp_tunnel_del	= nfp_net_del_vxlan_port,
 	.extended.ndo_xdp		= nfp_net_xdp,
