8654833acc75af4aff4f2ec5e4e07afe1b8ca5b3
Gopal Tiwari <gtiwari@redhat.com>
2017-11-16 13:38:15 -0500

    [hid] iio: hid-sensor-hub: Implement batch mode
    Message-id: <1510839526-11340-29-git-send-email-gtiwari@redhat.com>
    Patchwork-id: 198277
    O-Subject: [ PATCH BZ 1475903 28/59] iio: hid-sensor-hub: Implement batch mode
    Bugzilla: 1475903
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1475903
    
    commit 138bc7969c24c6cbba28e919c2376ad10a46fc60
    Author: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Date:   Fri Apr 7 19:22:06 2017 -0700
    
        iio: hid-sensor-hub: Implement batch mode
    
        HID sensor hubs using Integrated Senor Hub (ISH) has added capability to
        support batch mode. This allows host processor to go to sleep for extended
        duration, while the sensor hub is storing samples in its internal buffers.
    
        'Commit f4f4673b7535 ("iio: add support for hardware fifo")' implements
        feature in IIO core to implement such feature. This feature is used in
        bmc150-accel-core.c to implement batch mode. This implementation allows
        software device buffer watermark to be used as a hint to adjust hardware
        FIFO.
    
        But HID sensor hubs don't allow to change internal buffer size of FIFOs.
        Instead an additional usage id to set "maximum report latency" is defined.
        This allows host to go to sleep upto this latency period without getting
        any report. Since there is no ABI to set this latency, a new attribute
        "hwfifo_timeout" is added so that user mode can specify a latency.
    
        This change checks presence of usage id to get/set maximum report latency
        and if present, it will expose hwfifo_timeout.
    
        Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
        Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>
    Signed-off-by: Jeremy McNicoll <jeremymc@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- include/linux/hid-sensor-hub.h
+++ include/linux/hid-sensor-hub.h
@@ -174,11 +174,13 @@ struct hid_sensor_common {
 	atomic_t user_requested_state;
 	int poll_interval;
 	int raw_hystersis;
+	int latency_ms;
 	struct iio_trigger *trigger;
 	struct hid_sensor_hub_attribute_info poll;
 	struct hid_sensor_hub_attribute_info report_state;
 	struct hid_sensor_hub_attribute_info power_state;
 	struct hid_sensor_hub_attribute_info sensitivity;
+	struct hid_sensor_hub_attribute_info report_latency;
 	struct work_struct work;
 };
 
@@ -206,5 +208,9 @@ int hid_sensor_read_samp_freq_value(struct hid_sensor_common *st,
 					int *val1, int *val2);
 
 s32 hid_sensor_read_poll_value(struct hid_sensor_common *st);
+
+bool hid_sensor_batch_mode_supported(struct hid_sensor_common *st);
+int hid_sensor_set_report_latency(struct hid_sensor_common *st, int latency);
+int hid_sensor_get_report_latency(struct hid_sensor_common *st);
 
 #endif
