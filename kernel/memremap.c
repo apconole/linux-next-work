e4fb93add8612a5e41663aff8e1af4a2d3aaaf67
Larry Woodman <lwoodman@redhat.com>
2017-11-18 13:33:25 -0500

    [kernel] fix cache mode of dax pmd mappings
    Message-id: <1511012011-10209-18-git-send-email-lwoodman@redhat.com>
    Patchwork-id: 199234
    O-Subject: [RHEL7.5 PATCH 17/23] fix cache mode of dax pmd mappings
    Bugzilla: 1457572 1457561
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    
    commit 9049771f7d5490a302589976984810064c83ab40
         Author: Dan Williams <dan.j.williams@intel.com>
         Date:   Wed Sep 7 08:51:21 2016 -0700
    
            mm: fix cache mode of dax pmd mappings
    
            track_pfn_insert() in vmf_insert_pfn_pmd() is marking dax mappings as
            uncacheable rendering them impractical for application usage.  DAX-pte
            mappings are cached and the goal of establishing DAX-pmd mappings is to
            attain more performance, not dramatically less (3 orders of magnitude).
    
            track_pfn_insert() relies on a previous call to reserve_memtype() to
            establish the expected page_cache_mode for the range.  While memremap()
            arranges for reserve_memtype() to be called, devm_memremap_pages() does
            not.  So, teach track_pfn_insert() and untrack_pfn() how to handle
            tracking without a vma, and arrange for devm_memremap_pages() to
            establish the write-back-cache reservation in the memtype tree.
    
            Cc: <stable@vger.kernel.org>
            Cc: Matthew Wilcox <mawilcox@microsoft.com>
            Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
            Cc: Nilesh Choudhury <nilesh.choudhury@oracle.com>
            Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
            Reported-by: Toshi Kani <toshi.kani@hpe.com>
            Reported-by: Kai Zhang <kai.ka.zhang@oracle.com>
            Acked-by: Andrew Morton <akpm@linux-foundation.org>
            Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Larry Woodman <lwoodman@redhat.com>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- kernel/memremap.c
+++ kernel/memremap.c
@@ -275,6 +275,7 @@ static void devm_memremap_pages_release(struct device *dev, void *data)
 
 	mem_hotplug_begin();
 	arch_remove_memory(align_start, align_size);
+	untrack_pfn(NULL, PHYS_PFN(align_start), align_size);
 	mem_hotplug_done();
 	pgmap_radix_release(res);
 	dev_WARN_ONCE(dev, pgmap->altmap && pgmap->altmap->alloc,
@@ -311,6 +312,7 @@ void *devm_memremap_pages(struct device *dev, struct resource *res,
 		struct percpu_ref *ref, struct vmem_altmap *altmap)
 {
 	resource_size_t key, align_start, align_size, align_end;
+	pgprot_t pgprot = PAGE_KERNEL;
 	struct dev_pagemap *pgmap;
 	struct page_map *page_map;
 	int error, nid, is_ram;
@@ -389,6 +391,11 @@ void *devm_memremap_pages(struct device *dev, struct resource *res,
 	if (nid < 0)
 		nid = numa_mem_id();
 
+	error = track_pfn_remap(NULL, &pgprot, PHYS_PFN(align_start), 0,
+			align_size);
+	if (error)
+		goto err_pfn_remap;
+
 	mem_hotplug_begin();
 	error = arch_add_memory(nid, align_start, align_size, true);
 	mem_hotplug_done();
@@ -411,6 +418,8 @@ void *devm_memremap_pages(struct device *dev, struct resource *res,
 	return __va(res->start);
 
  err_add_memory:
+	untrack_pfn(NULL, PHYS_PFN(align_start), align_size);
+ err_pfn_remap:
  err_radix:
 	pgmap_radix_release(res);
 	devres_free(page_map);
