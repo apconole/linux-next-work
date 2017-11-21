e2e95d656077ef1346a95e4dc723f0fdc0b39420
Jeff Moyer <jmoyer@redhat.com>
2017-11-21 14:58:19 -0500

    [kernel] block: remove block_device_operations ->direct_access()
    Message-id: <20171121145952.29691-46-jmoyer@redhat.com>
    Patchwork-id: 199712
    O-Subject: [RHEL7.5 PATCH 045/138] block: remove block_device_operations ->direct_access()
    Bugzilla: 1457559
    RH-Acked-by: Al Stone <ahs3@redhat.com>
    RH-Acked-by: Ming Lei <ming.lei@redhat.com>
    
    From: Dan Williams <dan.j.williams@intel.com>
    
    BZ: 1457559
    Brew: 14590444
    
    commit d4b29fd78ea6fc2be219be3af1a992149b4ff0f6
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Fri Jan 27 17:22:03 2017 -0800
    
        block: remove block_device_operations ->direct_access()
    
        Now that all the producers and consumers of dax interfaces have been
        converted to using dax_operations on a dax_device, remove the block
        device direct_access enabling.
    
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -956,18 +956,6 @@ static long dm_dax_direct_access(struct dax_device *dax_dev, pgoff_t pgoff,
 	return ret;
 }
 
-static long dm_blk_direct_access(struct block_device *bdev, sector_t sector,
-		void **kaddr, pfn_t *pfn, long size)
-{
-	struct mapped_device *md = bdev->bd_disk->private_data;
-	struct dax_device *dax_dev = md->dax_dev;
-	long nr_pages = size / PAGE_SIZE;
-
-	nr_pages = dm_dax_direct_access(dax_dev, sector / PAGE_SECTORS,
-			nr_pages, kaddr, pfn);
-	return nr_pages < 0 ? nr_pages : nr_pages * PAGE_SIZE;
-}
-
 /*
  * Flush current->bio_list when the target map method blocks.
  * This fixes deadlocks in snapshot and possibly in other targets.
@@ -3080,7 +3068,6 @@ static const struct block_device_operations dm_blk_dops = {
 	.open = dm_blk_open,
 	.release = dm_blk_close,
 	.ioctl = dm_blk_ioctl,
-	.direct_access = dm_blk_direct_access,
 	.getgeo = dm_blk_getgeo,
 	.pr_ops = &dm_pr_ops,
 	.owner = THIS_MODULE
