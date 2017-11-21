208a3189f9189176f90c06f4ef3fadeb7c14e574
Jeff Moyer <jmoyer@redhat.com>
2017-11-21 14:58:44 -0500

    [dax] fix false CONFIG_BLOCK dependency
    Message-id: <20171121145952.29691-71-jmoyer@redhat.com>
    Patchwork-id: 199744
    O-Subject: [RHEL7.5 PATCH 070/138] dax: fix false CONFIG_BLOCK dependency
    Bugzilla: 1457559
    RH-Acked-by: Al Stone <ahs3@redhat.com>
    RH-Acked-by: Ming Lei <ming.lei@redhat.com>
    
    From: Dan Williams <dan.j.williams@intel.com>
    
    BZ: 1457559
    Brew: 14590444
    
    commit 9d109081c261d87fc84e0cce245796796ae4c460
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Sat May 13 16:18:21 2017 -0700
    
        dax: fix false CONFIG_BLOCK dependency
    
        In the BLOCK=n case the dax core does not need to / must not emit the
        block-device-dax helpers. Otherwise it leads to compile errors.
    
        Cc: Arnd Bergmann <arnd@arndb.de>
        Reported-by: Fabian Frederick <fabf@skynet.be>
        Fixes: ef51042472f5 ("block, dax: move 'select DAX' from BLOCK to FS_DAX")
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/dax/super.c
+++ drivers/dax/super.c
@@ -47,6 +47,7 @@ void dax_read_unlock(int id)
 }
 EXPORT_SYMBOL_GPL(dax_read_unlock);
 
+#ifdef CONFIG_BLOCK
 int bdev_dax_pgoff(struct block_device *bdev, sector_t sector, size_t size,
 		pgoff_t *pgoff)
 {
@@ -115,6 +116,7 @@ int __bdev_dax_supported(struct super_block *sb, int blocksize)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(__bdev_dax_supported);
+#endif
 
 /**
  * struct dax_device - anchor object for dax services
