1d41f4c18b86cbd856b7c9d0ab124aafe4d2e6ff
Gopal Tiwari <gtiwari@redhat.com>
2017-11-30 05:05:46 -0500

    [i2c] do not use internal data from driver core
    Message-id: <1512018377-6838-45-git-send-email-gtiwari@redhat.com>
    Patchwork-id: 200613
    O-Subject: [RHEL-7.5 V4 BZ 1456705 44/75] i2c: do not use internal data from driver core
    Bugzilla: 1456705
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1456705
    
    commit 95026658c46ea2d94498d0dac1282e28cd47c64a
    Author: Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Date:   Mon Mar 7 17:19:17 2016 +0530
    
        i2c: do not use internal data from driver core
    
        The variable p is a data structure which is used by the driver core
        internally and it is not expected that busses will be directly accessing
        these driver core internal only data.
    
        Signed-off-by: Sudip Mukherjee <sudip.mukherjee@codethink.co.uk>
        Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        [wsa: removed the unlikely()]
        Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -67,6 +67,7 @@ static struct device_type i2c_client_type;
 static int i2c_detect(struct i2c_adapter *adapter, struct i2c_driver *driver);
 
 static struct static_key i2c_trace_msg = STATIC_KEY_INIT_FALSE;
+static bool is_registered;
 
 void i2c_transfer_trace_reg(void)
 {
@@ -1457,7 +1458,7 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	int res = 0;
 
 	/* Can't register until after driver model init */
-	if (unlikely(WARN_ON(!i2c_bus_type.p))) {
+	if (WARN_ON(!is_registered)) {
 		res = -EAGAIN;
 		goto out_list;
 	}
@@ -1802,7 +1803,7 @@ int i2c_register_driver(struct module *owner, struct i2c_driver *driver)
 	int res;
 
 	/* Can't register until after driver model init */
-	if (unlikely(WARN_ON(!i2c_bus_type.p)))
+	if (WARN_ON(!is_registered))
 		return -EAGAIN;
 
 	/* add the driver to the list of i2c drivers in the driver core */
@@ -2016,6 +2017,7 @@ static int i2c_check_for_quirks(struct i2c_adapter *adap, struct i2c_msg *msgs, 
 		}
 	}
 
+	is_registered = true;
 	return 0;
 }
 
