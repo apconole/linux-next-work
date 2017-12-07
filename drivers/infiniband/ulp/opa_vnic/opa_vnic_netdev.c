a6b6772f56568e193734e6541b6ca3f684db2b68
Don Dutile <ddutile@redhat.com>
2017-12-07 17:13:01 -0500

    [infiniband] ib/opa-vnic: used RHEL extended struct for min/max_mtu params
    Message-id: <20171207171303.12223-2-ddutile@redhat.com>
    Patchwork-id: 201753
    O-Subject: [PATCH RHEL-7.5 1/3] IB/opa-vnic: used RHEL extended struct for min/max_mtu params
    Bugzilla: 1520270 1452831
    RH-Acked-by: Honggang Li <honli@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    
    RHEL's netdev struct doesn't have [min|max]_mtu params in the netdev
    struct, but in its extended-for-kabi extended struct.  Modify upstream
    code to use RHEL-based struct & struct elements
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1520270
            : https://bugzilla.redhat.com/show_bug.cgi?id=1452831
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=14700541
    
    Signed-off-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- drivers/infiniband/ulp/opa_vnic/opa_vnic_netdev.c
+++ drivers/infiniband/ulp/opa_vnic/opa_vnic_netdev.c
@@ -140,10 +140,10 @@ void opa_vnic_process_vema_config(struct opa_vnic_adapter *adapter)
 
 	/* Handle MTU limit change */
 	rtnl_lock();
-	netdev->max_mtu = max_t(unsigned int, info->vesw.eth_mtu_non_vlan,
-				netdev->min_mtu);
-	if (netdev->mtu > netdev->max_mtu)
-		dev_set_mtu(netdev, netdev->max_mtu);
+	netdev->extended->max_mtu = max_t(unsigned int, info->vesw.eth_mtu_non_vlan,
+				netdev->extended->min_mtu);
+	if (netdev->mtu > netdev->extended->max_mtu)
+		dev_set_mtu(netdev, netdev->extended->max_mtu);
 	rtnl_unlock();
 
 	/* Update flow to default port redirection table */
