abb42edbda85aca747acf3bd313051b061f603e2
Jeremy McNicoll <jeremymc@redhat.com>
2017-12-07 12:40:40 -0500

    [acpi] acpi / scan: Enable GPEs before scanning the namespace
    Message-id: <1512650446-15556-108-git-send-email-jeremymc@redhat.com>
    Patchwork-id: 201736
    O-Subject: [PATCH RHEL7.5 v3 107/113] ACPI / scan: Enable GPEs before scanning the namespace
    Bugzilla: 1172010
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jeremy McNicoll <jeremymc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1172010
    Conflict: acpi_gpe_apply_masked_gpes() , acpi_update_all_gpes() added
         and not moved as these were not part of existing code.
    
    commit eb7f43c4adb4a789f99f53916182c3401b4e33c7
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Thu Aug 10 00:34:23 2017 +0200
    
        ACPI / scan: Enable GPEs before scanning the namespace
    
        On some systems the platform firmware expects GPEs to be enabled
        before the enumeration of devices and if that expectation is not
        met, the systems in question may not boot in some situations.
    
        For this reason, change the initialization ordering of the ACPI
        subsystem to make it enable GPEs before scanning the namespace
        for the first time in order to enumerate devices.
    
        Reported-by: Mika Westerberg <mika.westerberg@linux.intel.com>
        Suggested-by: Mika Westerberg <mika.westerberg@linux.intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Acked-by: Lv Zheng <lv.zheng@intel.com>
        Tested-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    
    Signed-off-by: Jeremy McNicoll <jeremymc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -1999,6 +1999,9 @@ int __init acpi_scan_init(void)
 	acpi_pnp_init();
 	acpi_int340x_thermal_init();
 
+	acpi_gpe_apply_masked_gpes();
+	acpi_update_all_gpes();
+
 	mutex_lock(&acpi_scan_lock);
 	/*
 	 * Enumerate devices in the ACPI namespace.
