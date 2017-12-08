7e723eeba0694984d1333696b576112f4277cde1
Prarit Bhargava <prarit@redhat.com>
2017-12-07 22:38:24 -0500

    [x86] x86/topology: Add topology_max_smt_threads()
    Message-id: <20171207223825.20171-2-prarit@redhat.com>
    Patchwork-id: 202038
    O-Subject: [RHEL7.5 BZ 1519503 1/2] x86/topology: Add topology_max_smt_threads()
    Bugzilla: 1519503
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1519503
    
    commit 70b8301f6b8f7bc053377a9cbd0c4e42e29d9807
    Author: Andi Kleen <ak@linux.intel.com>
    Date:   Thu May 19 17:09:55 2016 -0700
    
        x86/topology: Add topology_max_smt_threads()
    
        For SMT specific workarounds it is useful to know if SMT is active
        on any online CPU in the system. This currently requires a loop
        over all online CPUs.
    
        Add a global variable that is updated with the maximum number
        of smt threads on any CPU on online/offline, and use it for
        topology_max_smt_threads()
    
        The single call is easier to use than a loop.
    
        Not exported to user space because user space already can use
        the existing sibling interfaces to find this out.
    
        Signed-off-by: Andi Kleen <ak@linux.intel.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
        Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
        Cc: Jiri Olsa <jolsa@redhat.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Stephane Eranian <eranian@google.com>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Vince Weaver <vincent.weaver@maine.edu>
        Cc: acme@kernel.org
        Cc: jolsa@kernel.org
        Link: http://lkml.kernel.org/r/1463703002-19686-2-git-send-email-andi@firstfloor.org
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Frank Ramsay <framsay@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -113,6 +113,9 @@ unsigned int __max_logical_packages __read_mostly;
 EXPORT_SYMBOL(__max_logical_packages);
 static unsigned int logical_packages __read_mostly;
 
+/* Maximum number of SMT threads on any online core */
+int __max_smt_threads __read_mostly;
+
 /* Flag to indicate if a complete sched domain rebuild is required */
 bool x86_topology_update;
 
@@ -458,7 +461,7 @@ void set_cpu_sibling_map(int cpu)
 	bool has_mp = has_smt || boot_cpu_data.x86_max_cores > 1;
 	struct cpuinfo_x86 *c = &cpu_data(cpu);
 	struct cpuinfo_x86 *o;
-	int i;
+	int i, threads;
 
 	cpumask_set_cpu(cpu, cpu_sibling_setup_mask);
 
@@ -513,6 +516,10 @@ void set_cpu_sibling_map(int cpu)
 		if (match_die(c, o) && !topology_same_node(c, o))
 			x86_has_numa_in_package = true;
 	}
+
+	threads = cpumask_weight(topology_sibling_cpumask(cpu));
+	if (threads > __max_smt_threads)
+		__max_smt_threads = threads;
 }
 
 /* maps the cpu to the sched domain representing multi-core */
@@ -1429,6 +1436,21 @@ __init void prefill_possible_map(void)
 
 #ifdef CONFIG_HOTPLUG_CPU
 
+/* Recompute SMT state for all CPUs on offline */
+static void recompute_smt_state(void)
+{
+	int max_threads, cpu;
+
+	max_threads = 0;
+	for_each_online_cpu (cpu) {
+		int threads = cpumask_weight(topology_sibling_cpumask(cpu));
+
+		if (threads > max_threads)
+			max_threads = threads;
+	}
+	__max_smt_threads = max_threads;
+}
+
 static void remove_siblinginfo(int cpu)
 {
 	int sibling;
@@ -1453,6 +1475,7 @@ static void remove_siblinginfo(int cpu)
 	c->phys_proc_id = 0;
 	c->cpu_core_id = 0;
 	cpumask_clear_cpu(cpu, cpu_sibling_setup_mask);
+	recompute_smt_state();
 }
 
 static void __ref remove_cpu_from_maps(int cpu)
