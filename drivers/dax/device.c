7c8140fc7d93f311edaa4da7d9ef62fad5052548
Jeff Moyer <jmoyer@redhat.com>
2017-12-11 18:03:42 -0500

    [mm] device-dax: implement ->split() to catch invalid munmap attempts
    Message-id: <20171211180342.12886-4-jmoyer@redhat.com>
    Patchwork-id: 202138
    O-Subject: [RHEL7.5 PATCH 3/3] device-dax: implement ->split() to catch invalid munmap attempts
    Bugzilla: 1523344
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Luiz Capitulino <lcapitulino@redhat.com>
    
    From: Dan Williams <dan.j.williams@intel.com>
    
    BZ: 1523344
    Brew: 14709117
    
    RHEL: Flag the vma with VM_SPLIT.
    
    commit 9702cffdbf2129516db679e4467db81e1cd287da
    Author: Dan Williams <dan.j.williams@intel.com>
    Date:   Wed Nov 29 16:10:32 2017 -0800
    
        device-dax: implement ->split() to catch invalid munmap attempts
    
        Similar to how device-dax enforces that the 'address', 'offset', and
        'len' parameters to mmap() be aligned to the device's fundamental
        alignment, the same constraints apply to munmap().  Implement ->split()
        to fail munmap calls that violate the alignment constraint.
    
        Otherwise, we later fail VM_BUG_ON checks in the unmap_page_range() path
        with crash signatures of the form:
    
            vma ffff8800b60c8a88 start 00007f88c0000000 end 00007f88c0e00000
            next           (null) prev           (null) mm ffff8800b61150c0
            prot 8000000000000027 anon_vma           (null) vm_ops ffffffffa0091240
            pgoff 0 file ffff8800b638ef80 private_data           (null)
            flags: 0x380000fb(read|write|shared|mayread|maywrite|mayexec|mayshare|softdirty|mixedmap|hugepage)
            ------------[ cut here ]------------
            kernel BUG at mm/huge_memory.c:2014!
            [..]
            RIP: 0010:__split_huge_pud+0x12a/0x180
            [..]
            Call Trace:
             unmap_page_range+0x245/0xa40
             ? __vma_adjust+0x301/0x990
             unmap_vmas+0x4c/0xa0
             unmap_region+0xae/0x120
             ? __vma_rb_erase+0x11a/0x230
             do_munmap+0x276/0x410
             vm_munmap+0x6a/0xa0
             SyS_munmap+0x1d/0x30
    
        Link: http://lkml.kernel.org/r/151130418681.4029.7118245855057952010.stgit@dwillia2-desk3.amr.corp.intel.com
        Fixes: dee410792419 ("/dev/dax, core: file operations and dax-mmap")
        Signed-off-by: Dan Williams <dan.j.williams@intel.com>
        Reported-by: Jeff Moyer <jmoyer@redhat.com>
        Cc: <stable@vger.kernel.org>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>

--- drivers/dax/device.c
+++ drivers/dax/device.c
@@ -432,9 +432,21 @@ static int dev_dax_fault(struct vm_area_struct *vma,
 	return dev_dax_huge_fault(vmf, PE_SIZE_PTE);
 }
 
+static int dev_dax_split(struct vm_area_struct *vma, unsigned long addr)
+{
+	struct file *filp = vma->vm_file;
+	struct dev_dax *dev_dax = filp->private_data;
+	struct dax_region *dax_region = dev_dax->region;
+
+	if (!IS_ALIGNED(addr, dax_region->align))
+		return -EINVAL;
+	return 0;
+}
+
 static const struct vm_operations_struct dax_vm_ops = {
 	.fault = dev_dax_fault,
 	.huge_fault = dev_dax_huge_fault,
+	.split = dev_dax_split,
 };
 
 static int dax_mmap(struct file *filp, struct vm_area_struct *vma)
@@ -456,7 +468,7 @@ static int dax_mmap(struct file *filp, struct vm_area_struct *vma)
 
 	vma->vm_ops = &dax_vm_ops;
 	vma->vm_flags |= VM_MIXEDMAP | VM_HUGEPAGE;
-	vma->vm_flags2 |= VM_PFN_MKWRITE | VM_HUGE_FAULT;
+	vma->vm_flags2 |= VM_PFN_MKWRITE | VM_HUGE_FAULT | VM_SPLIT;
 	return 0;
 }
 
