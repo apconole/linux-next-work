7e710871cad32bafc79a0594090dcedc2e966aee
Eric W. Biederman <ebiederm@redhat.com>
2017-12-15 06:33:03 -0500

    [fs] introduce iterate_dir() and dir_context
    Message-id: <20171215063702.4517-5-ebiederm@redhat.com>
    Patchwork-id: 202310
    O-Subject: [PATCH v6 RHEL7.5 fs 005/244] [readdir] introduce iterate_dir() and dir_context
    Bugzilla: 1487751
    RH-Acked-by: David Howells <dhowells@redhat.com>
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1487751
    Upstream Status: linux.git 5c0ba4e0762e6dabd14a5c276652e2defec38de7
    Build Info: https://brewweb.devel.redhat.com/taskinfo?taskID=14763400
    Testing: https://beaker.engineering.redhat.com/jobs/2200978
    
    commit 5c0ba4e0762e6dabd14a5c276652e2defec38de7
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Wed May 15 13:52:59 2013 -0400
    
        [readdir] introduce iterate_dir() and dir_context
    
        iterate_dir(): new helper, replacing vfs_readdir().
    
        struct dir_context: contains the readdir callback (and will get more stuff
        in it), embedded into whatever data that callback wants to deal with;
        eventually, we'll be passing it to ->readdir() replacement instead of
        (data,filldir) pair.
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    RHEL Notes:
        In fs/nfsd/nfs4recover.c:nfs4_list_rec_dir() this does not match
        the upstream change exactly.  Some minor changes were needed as
        the code in nfsd4_list_rec_dir() has a few updates in RHEL7 from
        other backports that were not present when iterate_dir was
        originally introduced to RHEL7.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- fs/nfsd/nfs4recover.c
+++ fs/nfsd/nfs4recover.c
@@ -262,7 +262,10 @@ nfsd4_list_rec_dir(recdir_func *f, struct nfsd_net *nn)
 {
 	const struct cred *original_cred;
 	struct dentry *dir = nn->rec_file->f_path.dentry;
-	LIST_HEAD(names);
+	struct {
+		struct dir_context ctx;
+		struct list_head names;
+	} ctx;
 	struct name_list *entry, *tmp;
 	int status;
 
@@ -276,9 +279,11 @@ nfsd4_list_rec_dir(recdir_func *f, struct nfsd_net *nn)
 		return status;
 	}
 
-	status = vfs_readdir(nn->rec_file, nfsd4_build_namelist, &names);
+	INIT_LIST_HEAD(&ctx.names);	
+	ctx.ctx.actor = nfsd4_build_namelist;
+	status = iterate_dir(nn->rec_file, &ctx.ctx);
 	mutex_lock_nested(&dir->d_inode->i_mutex, I_MUTEX_PARENT);
-	list_for_each_entry_safe(entry, tmp, &names, list) {
+	list_for_each_entry_safe(entry, tmp, &ctx.names, list) {
 		if (!status) {
 			struct dentry *dentry;
 			dentry = lookup_one_len(entry->name, dir, HEXDIR_LEN-1);
@@ -295,7 +300,7 @@ nfsd4_list_rec_dir(recdir_func *f, struct nfsd_net *nn)
 	mutex_unlock(&dir->d_inode->i_mutex);
 	nfs4_reset_creds(original_cred);
 
-	list_for_each_entry_safe(entry, tmp, &names, list) {
+	list_for_each_entry_safe(entry, tmp, &ctx.names, list) {
 		dprintk("NFSD: %s. Left entry %s\n", __func__, entry->name);
 		list_del(&entry->list);
 		kfree(entry);
