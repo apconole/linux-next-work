801e72a9e9595d3da1efc2a5fce9290a365bc82a
Andrea Arcangeli <aarcange@redhat.com>
2018-01-04 00:37:38 -0500

    [x86] spec_ctrl: add debugfs ibrs_enabled ibpb_enabled
    Message-id: <20180104003814.16314-101-aarcange@redhat.com>
    Patchwork-id: 203152
    O-Subject: [RHEL7.5 PATCH 100/136] x86/spec_ctrl: add debugfs ibrs_enabled ibpb_enabled
    Bugzilla: 1519801 1519798 1519786
    CVE: CVE-2017-5715 CVE-2017-5753 CVE-2017-5754
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    
    ibrs_enabled:
    0) disabled
    1) enabled in host kernel
    2) enabled in host kernel & userland
    
    ibrs is always disabled before entering guest mode, it's up to the
    guest kernel to manage that.
    
    ibpb_enabled:
    0) disabled
    1) enabled
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Rafael Aquini <aquini@redhat.com>

--- arch/x86/kernel/spec_ctrl.c
+++ arch/x86/kernel/spec_ctrl.c
@@ -50,6 +50,21 @@ void set_spec_ctrl_pcp_ibpb(bool enable)
 	set_spec_ctrl_pcp(enable, SPEC_CTRL_PCP_IBPB);
 }
 
+static void spec_ctrl_flush_all_cpus(u32 msr_nr, u64 val)
+{
+	int cpu;
+	get_online_cpus();
+	for_each_online_cpu(cpu)
+		wrmsrl_on_cpu(cpu, msr_nr, val);
+	put_online_cpus();
+}
+
+static void flush_all_cpus_ibrs(bool enable)
+{
+	spec_ctrl_flush_all_cpus(MSR_IA32_SPEC_CTRL,
+				 enable ? FEATURE_ENABLE_IBRS : 0);
+}
+
 void spec_ctrl_init(struct cpuinfo_x86 *c)
 {
 	bool implicit_ibpb = false;
@@ -112,3 +127,138 @@ void spec_ctrl_init(struct cpuinfo_x86 *c)
 		printk(KERN_INFO "FEATURE IBPB_SUPPORT Not Present\n");
 	}
 }
+
+static ssize_t __enabled_read(struct file *file, char __user *user_buf,
+			      size_t count, loff_t *ppos, unsigned int *field)
+{
+	char buf[32];
+	unsigned int len;
+
+	len = sprintf(buf, "%d\n", READ_ONCE(*field));
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static ssize_t ibrs_enabled_read(struct file *file, char __user *user_buf,
+				 size_t count, loff_t *ppos)
+{
+	return __enabled_read(file, user_buf, count, ppos, &ibrs_enabled);
+}
+
+static ssize_t ibrs_enabled_write(struct file *file,
+				  const char __user *user_buf,
+				  size_t count, loff_t *ppos)
+{
+	char buf[32];
+	ssize_t len;
+	unsigned int enable;
+
+	len = min(count, sizeof(buf) - 1);
+	if (copy_from_user(buf, user_buf, len))
+		return -EFAULT;
+
+	buf[len] = '\0';
+	if (kstrtouint(buf, 0, &enable))
+		return -EINVAL;
+
+	if (enable > IBRS_MAX)
+		return -EINVAL;
+
+	mutex_lock(&spec_ctrl_mutex);
+	if (ibrs_enabled == enable)
+		goto out_unlock;
+
+	if (!boot_cpu_has(X86_FEATURE_SPEC_CTRL)) {
+		count = -ENODEV;
+		goto out_unlock;
+	}
+
+	if (enable == IBRS_ENABLED) {
+		set_spec_ctrl_pcp_ibrs_user(false);
+		set_spec_ctrl_pcp_ibrs(true);
+	} else {
+		set_spec_ctrl_pcp_ibrs(false);
+		if (enable == IBRS_DISABLED) {
+			set_spec_ctrl_pcp_ibrs_user(false);
+			flush_all_cpus_ibrs(false);
+		} else {
+			WARN_ON(enable != IBRS_ENABLED_USER);
+			set_spec_ctrl_pcp_ibrs_user(true);
+			flush_all_cpus_ibrs(true);
+		}
+	}
+	WRITE_ONCE(ibrs_enabled, enable);
+
+out_unlock:
+	mutex_unlock(&spec_ctrl_mutex);
+	return count;
+}
+
+static const struct file_operations fops_ibrs_enabled = {
+	.read = ibrs_enabled_read,
+	.write = ibrs_enabled_write,
+	.llseek = default_llseek,
+};
+
+static ssize_t ibpb_enabled_read(struct file *file, char __user *user_buf,
+				 size_t count, loff_t *ppos)
+{
+	unsigned int tmp = ibpb_enabled;
+	return __enabled_read(file, user_buf, count, ppos, &tmp);
+}
+
+static ssize_t ibpb_enabled_write(struct file *file,
+				  const char __user *user_buf,
+				  size_t count, loff_t *ppos)
+{
+	char buf[32];
+	ssize_t len;
+	unsigned int enable;
+
+	len = min(count, sizeof(buf) - 1);
+	if (copy_from_user(buf, user_buf, len))
+		return -EFAULT;
+
+	buf[len] = '\0';
+	if (kstrtouint(buf, 0, &enable))
+		return -EINVAL;
+
+	if (enable > 1)
+		return -EINVAL;
+
+	mutex_lock(&spec_ctrl_mutex);
+	if (ibpb_enabled == !!enable)
+		goto out_unlock;
+
+	if (!boot_cpu_has(X86_FEATURE_IBPB_SUPPORT)) {
+		count = -ENODEV;
+		goto out_unlock;
+	}
+
+	if (enable) {
+		set_spec_ctrl_pcp_ibpb(true);
+		WRITE_ONCE(ibpb_enabled, true);
+	} else {
+		set_spec_ctrl_pcp_ibpb(false);
+		WRITE_ONCE(ibpb_enabled, false);
+	}
+
+out_unlock:
+	mutex_unlock(&spec_ctrl_mutex);
+	return count;
+}
+
+static const struct file_operations fops_ibpb_enabled = {
+	.read = ibpb_enabled_read,
+	.write = ibpb_enabled_write,
+	.llseek = default_llseek,
+};
+
+static int __init debugfs_spec_ctrl(void)
+{
+	debugfs_create_file("ibrs_enabled", S_IRUSR | S_IWUSR,
+			    arch_debugfs_dir, NULL, &fops_ibrs_enabled);
+	debugfs_create_file("ibpb_enabled", S_IRUSR | S_IWUSR,
+			    arch_debugfs_dir, NULL, &fops_ibpb_enabled);
+	return 0;
+}
+late_initcall(debugfs_spec_ctrl);
