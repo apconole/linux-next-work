e887e99dae3a03bd5b0a9cef1a78fd60079bde9e
Brian Foster <bfoster@redhat.com>
2018-01-16 15:08:36 -0500

    [fs] xfs: truncate transaction does not modify the inobt
    Message-id: <20180116150839.33084-8-bfoster@redhat.com>
    Patchwork-id: 203899
    O-Subject: [RHEL7 PATCH 07/10] xfs: truncate transaction does not modify the inobt
    Bugzilla: 1397653
    RH-Acked-by: Carlos Maiolino <cmaiolino@redhat.com>
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    
    commit a606ebdb859e78beb757dfefa08001df366e2ef5
    Author: Brian Foster <bfoster@redhat.com>
    Date:   Mon Jan 8 10:41:37 2018 -0800
    
        xfs: truncate transaction does not modify the inobt
    
        The truncate transaction does not ever modify the inode btree, but
        includes an associated log reservation. Update
        xfs_calc_itruncate_reservation() to remove the reservation
        associated with inobt updates.
    
        Signed-off-by: Brian Foster <bfoster@redhat.com>
        Reviewed-by: Dave Chinner <dchinner@redhat.com>
        Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
        Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>

--- fs/xfs/libxfs/xfs_trans_resv.c
+++ fs/xfs/libxfs/xfs_trans_resv.c
@@ -205,8 +205,6 @@ xfs_calc_write_reservation(
  *    the super block to reflect the freed blocks: sector size
  *    worst case split in allocation btrees per extent assuming 4 extents:
  *		4 exts * 2 trees * (2 * max depth - 1) * block size
- *    the inode btree: max depth * blocksize
- *    the allocation btrees: 2 trees * (max depth - 1) * block size
  */
 STATIC uint
 xfs_calc_itruncate_reservation(
@@ -218,12 +216,7 @@ xfs_calc_itruncate_reservation(
 				      XFS_FSB_TO_B(mp, 1))),
 		    (xfs_calc_buf_res(9, mp->m_sb.sb_sectsize) +
 		     xfs_calc_buf_res(XFS_ALLOCFREE_LOG_COUNT(mp, 4),
-				      XFS_FSB_TO_B(mp, 1)) +
-		    xfs_calc_buf_res(5, 0) +
-		    xfs_calc_buf_res(XFS_ALLOCFREE_LOG_COUNT(mp, 1),
-				     XFS_FSB_TO_B(mp, 1)) +
-		    xfs_calc_buf_res(2 + mp->m_ialloc_blks +
-				     mp->m_in_maxlevels, 0)));
+			              XFS_FSB_TO_B(mp, 1))));
 }
 
 /*
