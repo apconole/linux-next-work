0f83d806648ed7313ea2ff892961428e10f319f9
Suravee Suthikulpanit <ssuthiku@redhat.com>
2018-02-03 02:40:55 -0500

    [x86] kvm: svm: Fix up enable_smi_window due to out-of-order backport
    Message-id: <1517625655-14316-1-git-send-email-ssuthiku@redhat.com>
    Patchwork-id: 204867
    O-Subject: [ RHEL7.5 BZ 1135003 v2 8/7] kvm: svm: Fix up enable_smi_window due to out-of-order backport
    Bugzilla: 1135003
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1135003
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15198265
    Upstream: RHEL only
    Testing: Test launching nested KVM guests on AMD EPYC system.
    
    Due to out-of-order backport of Red Hat commit 9a0671c02bb8
    ("[x86] kvm: svm: detect opening of SMI window using STGI intercept"),
    we need to add this logic in order to match upstream.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Bandan Das <bsd@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Ladi Prosek <lprosek@redhat.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Jerry Snitselaar <jsnitsel@redhat.com>
    Cc: rhvirt-patches@redhat.com
    Suggested-by: Radim Krcmar <rkrcmar@redhat.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Bruno E. O. Meneguele <brdeoliv@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5556,6 +5556,8 @@ static int enable_smi_window(struct kvm_vcpu *vcpu)
 	struct vcpu_svm *svm = to_svm(vcpu);
 
 	if (!gif_set(svm)) {
+		if (vgif_enabled(svm))
+			set_intercept(svm, INTERCEPT_STGI);
 		/* STGI will cause a vm exit */
 		return 1;
 	}
