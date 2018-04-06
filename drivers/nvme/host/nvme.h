c1f05f4a82923b92dcda96bb7e6905de07ef4a31
David Milburn <dmilburn@redhat.com>
2018-04-06 19:29:08 -0400

    [nvme] unexport starting async event work
    Message-id: <1523042961-48197-84-git-send-email-dmilburn@redhat.com>
    Patchwork-id: 208648
    O-Subject: [RHEL7.6 PATCH BZ 1519689 83/96] nvme: unexport starting async event work
    Bugzilla: 1519689
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Ewan Milne <emilne@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Keith Busch <keith.busch@intel.com>
    
    Async event work is for core use only and should not be called directly
    from drivers.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Guan Junxiong <guanjunxiong@huawei.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit d99ca609a1b55f87a5e62a11ed70e4d091d815f0)
    Signed-off-by: David Milburn <dmilburn@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -301,7 +301,6 @@ void nvme_remove_namespaces(struct nvme_ctrl *ctrl);
 
 void nvme_complete_async_event(struct nvme_ctrl *ctrl, __le16 status,
 		union nvme_result *res);
-void nvme_queue_async_events(struct nvme_ctrl *ctrl);
 
 void nvme_stop_queues(struct nvme_ctrl *ctrl);
 void nvme_start_queues(struct nvme_ctrl *ctrl);
