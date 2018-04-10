fdf2a3372b55db3d3add4d710bdd80d8a30a6ee9
Mauricio Oliveira <maolivei@redhat.com>
2018-04-10 00:37:46 -0400

    [powerpc] rfi-flush: Call setup_rfi_flush() after LPM migration
    Message-id: <1523320666-1904-6-git-send-email-maolivei@redhat.com>
    Patchwork-id: 208784
    O-Subject: [RHEL7.6 BZ1561785 PATCH 5/5] powerpc/rfi-flush: Call setup_rfi_flush() after LPM migration
    Bugzilla: 1561785
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1561785
    Upstream Status: https://git.kernel.org/torvalds/c/921bc6cf807ceb2ab8005319cf39f33494d6b100
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15742855
    Tested: https://bugzilla.redhat.com/show_bug.cgi?id=1561785#c4
    Conflicts: setup.c: 's/pSeries_setup_rfi_flush/pseries_setup_rfi_flush/'
     - hunk 1:
       - update context lines
       - update remove line for capital S used downstream
     - hunk 2:
       - insert new hunk
       - update usage of capital used S downstream
    
    commit 921bc6cf807ceb2ab8005319cf39f33494d6b100
    Author: Michael Ellerman <mpe@ellerman.id.au>
    Date:   Wed Mar 14 19:40:42 2018 -0300
    
        powerpc/rfi-flush: Call setup_rfi_flush() after LPM migration
    
        We might have migrated to a machine that uses a different flush type,
        or doesn't need flushing at all.
    
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Signed-off-by: Mauricio Faria de Oliveira <mauricfo@linux.vnet.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Mauricio Oliveira <maolivei@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/powerpc/platforms/pseries/setup.c
+++ arch/powerpc/platforms/pseries/setup.c
@@ -522,7 +522,7 @@ static void __init find_and_init_phbs(void)
 	}
 }
 
-static void pSeries_setup_rfi_flush(void)
+void pseries_setup_rfi_flush(void)
 {
 	struct h_cpu_char_result result;
 	enum l1d_flush_type types;
@@ -571,7 +571,7 @@ static void __init pSeries_setup_arch(void)
 
 	fwnmi_init();
 
-	pSeries_setup_rfi_flush();
+	pseries_setup_rfi_flush();
 
 	/* By default, only probe PCI (can be overriden by rtas_pci) */
 	pci_add_flags(PCI_PROBE_ONLY);
