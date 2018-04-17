d39f6b300e969b5ebe65cbefd6379ba964f8a321
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2018-04-17 14:11:23 -0400

    [input] synaptics-rmi4 - add parameters for dribble packets and palm detect gesture
    Message-id: <20180417141301.4870-44-benjamin.tissoires@redhat.com>
    Patchwork-id: 209602
    O-Subject: [PATCH RHEL7.6 043/141] Input: synaptics-rmi4 - add parameters for dribble packets and palm detect gesture
    Bugzilla: 1554926
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Aristeu Rozanski <aris@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1554926
    Upstream: since v4.10
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15808598
    Test: by me on a t450s, X280 and the IT folks on the X1 Carbon 6 and T480
    
    commit 2775e523246e11c5ce90b69226c5e67aa43e64a5
    Author: Andrew Duggan <aduggan@synaptics.com>
    Date:   Tue Nov 8 16:48:48 2016 -0800
    
        Input: synaptics-rmi4 - add parameters for dribble packets and palm detect gesture
    
        The rmi_f11 driver currently disables dribble packets and the palm detect
        gesture for all devices. This patch creates a parameter in the 2d sensor
        platform data for controlling this functionality on a per device basis.
    
        For more information on dribble packets:
        Commit 05ba999fcabb ("HID: rmi: disable dribble packets on Synaptics
        touchpads")
    
        For more information on the palm detect gesture:
        Commit f097deef59a6 ("HID: rmi: disable palm detect gesture when present")
    
        Signed-off-by: Andrew Duggan <aduggan@synaptics.com>
        Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
        Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
        Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/input/rmi4/rmi_2d_sensor.h
+++ drivers/input/rmi4/rmi_2d_sensor.h
@@ -67,6 +67,8 @@ struct rmi_2d_sensor {
 	u8 report_rel;
 	u8 x_mm;
 	u8 y_mm;
+	enum rmi_reg_state dribble;
+	enum rmi_reg_state palm_detect;
 };
 
 void rmi_2d_sensor_abs_process(struct rmi_2d_sensor *sensor,
