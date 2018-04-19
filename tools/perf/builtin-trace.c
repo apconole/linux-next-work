8157ead8656c9dd9d11b4380a24271b5d70acb8d
Jiri Olsa <jolsa@redhat.com>
2018-04-19 18:57:03 -0400

    [tools] perf trace: Call machine__exit() at exit
    Message-id: <20180419185703.28105-246-jolsa@redhat.com>
    Patchwork-id: 210103
    O-Subject: [PATCH RHEL7.6 BZ1540862 245/245] perf trace: Call machine__exit() at exit
    Bugzilla: 1540862
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1540862
    https://bugzilla.redhat.com/show_bug.cgi?id=1540862
    
    upstream
    ========
    commit 33974a414ce2324554f75dbd204ff0868f499e32
    Author: Andrei Vagin <avagin@openvz.org>
    Date: Tue Nov 7 16:22:45 2017 -0800
    
    description
    ===========
    Otherwise 'perf trace' leaves a temporary file /tmp/perf-vdso.so-XXXXXX.
    
      $ perf trace -o log true
      $ ls -l /tmp/perf-vdso.*
      -rw------- 1 root root 8192 Nov  8 03:08 /tmp/perf-vdso.so-5bCpD0
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- tools/perf/builtin-trace.c
+++ tools/perf/builtin-trace.c
@@ -1132,6 +1132,14 @@ static int trace__symbols_init(struct trace *trace, struct perf_evlist *evlist)
 	return err;
 }
 
+static void trace__symbols__exit(struct trace *trace)
+{
+	machine__exit(trace->host);
+	trace->host = NULL;
+
+	symbol__exit();
+}
+
 static int syscall__alloc_arg_fmts(struct syscall *sc, int nr_args)
 {
 	int idx;
@@ -2432,6 +2440,8 @@ out_disable:
 	}
 
 out_delete_evlist:
+	trace__symbols__exit(trace);
+
 	perf_evlist__delete(evlist);
 	trace->evlist = NULL;
 	trace->live = false;
