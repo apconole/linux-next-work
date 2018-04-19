torvalds/linux

commit 64b12a68a9f74bb32d8efd7af1ad8a2ba02fc884

mmc: core: fix prepared requests while doing bkops
While starting the bkops the previously prepared request should be canceled
and restarted after the bkops. As the prepared resource might already
setup the dma channels and ready to be started. Now with the arrival of bkops
request this prepared request can be serviced ONLY after the bkops. So
holding on to the prepared request in the host driver is confusing at
this point in time, so it makes sense to cleanup such dangling requests and
reissue this request once bkops is done.
Canceling the prepared request would give opportunity to the host drivers
to perform cleanup on the prepared request.

Without this patch host drivers like mmci gets confused when a blocking
request like send_ext_csd(CMD8) is issued while there is already a prepared
request. With the help of this patch, the driver can better manage such
blocking requests and cleanup the prepared requests which are not started yet.

Without this patch I hit below crash on Qualcomm APQ8064 based IFC6410 board
with mmci host driver.

mmci-pl18x 12400000.sdcc: error during DMA transfer!
Unable to handle kernel paging request at virtual address 40000000
pgd = c0204000
[40000000] *pgd=00000000
Internal error: Oops: 805 [#1] SMP ARM
Modules linked in: ipv6 ath6kl_sdio ath6kl_core
CPU: 0 PID: 0 Comm: swapper/0 Tainted: G        W      3.17.0-rc7-linaro-multi-v7 #1
task: c0c9d7e0 ti: c0c92000 task.ti: c0c92000
PC is at v7_dma_inv_range+0x34/0x4c
LR is at __dma_page_dev_to_cpu+0x80/0x100
pc : [<c021efc0>]    lr : [<c021af18>]    psr: 400f0193
sp : c0c93e20  ip : c0c9a478  fp : c08ea538
r10: c0c9f548  r9 : 00000002  r8 : e97d9000
r7 : 00000200  r6 : c0c9d504  r5 : c0db0880  r4 : 00000000
r3 : 0000003f  r2 : 00000040  r1 : 40000200  r0 : 40000000
Flags: nZcv  IRQs off  FIQs on  Mode SVC_32  ISA ARM  Segment kernel
Control: 10c5787d  Table: a9ef406a  DAC: 00000015
Process swapper/0 (pid: 0, stack limit = 0xc0c92250)
Stack: (0xc0c93e20 to 0xc0c94000)

drivers/mmc/core/core.c
@@ -550,8 +550,18 @@ struct mmc_async_req *mmc_start_req(struct mmc_host *host,
 		if (host->card && mmc_card_mmc(host->card) &&
 		    ((mmc_resp_type(host->areq->mrq->cmd) == MMC_RSP_R1) ||
 		     (mmc_resp_type(host->areq->mrq->cmd) == MMC_RSP_R1B)) &&
-		    (host->areq->mrq->cmd->resp[0] & R1_EXCEPTION_EVENT))
+		    (host->areq->mrq->cmd->resp[0] & R1_EXCEPTION_EVENT)) {
+
+			/* Cancel the prepared request */
+			if (areq)
+				mmc_post_req(host, areq->mrq, -EINVAL);
+
 			mmc_start_bkops(host->card, true);
+
+			/* prepare the request again */
+			if (areq)
+				mmc_pre_req(host, areq->mrq, !host->areq);
+		}
 	}
 
 	if (!err && areq)

apconole/linux-next-work

commit 2e8e99f987fc3c1b2cfb859cad70b6fc15bb128c

[mmc] core/mmci: restore pre/post_req behaviour
Message-id: <1521632127-15027-35-git-send-email-gtiwari@redhat.com>
Patchwork-id: 207117
O-Subject: [RHEL7.6 V3 BZ 1549495 034/105] mmc: core/mmci: restore pre/post_req behaviour
Bugzilla: 1549495
RH-Acked-by: Jeremy McNicoll <jmcnicol@redhat.com>
RH-Acked-by: Jarod Wilson <jarod@redhat.com>
RH-Acked-by: Tony Camuso <tcamuso@redhat.com>

Bugzilla: http://bugzilla.redhat.com/1549495

commit e13934bdf45935f51243515c275629922dc43839
Author: Linus Walleij <linus.walleij@linaro.org>
Date:   Fri Jan 27 15:04:54 2017 +0100

    mmc: core/mmci: restore pre/post_req behaviour

    commit 64b12a68a9f74bb32d8efd7af1ad8a2ba02fc884
    "mmc: core: fix prepared requests while doing bkops"
    is fixing a bug in the wrong way. A bug in the MMCI
    device driver is fixed by amending the MMC core.

    Thinking about it: what the pre- and post-callbacks
    are doing is to essentially map and unmap SG lists
    for DMA transfers. Why would we not be able to do that
    just because a BKOPS command is sent inbetween?
    Having to unprepare/prepare the next asynchronous
    request for DMA seems wrong.

    Looking the backtrace in that commit we can see what
    the real problem actually is:

    mmci_data_irq() is calling mmci_dma_unmap() twice
    which is goung to call arm_dma_unmap_sg() twice
    and v7_dma_inv_range() twice for the same sglist
    and that will crash.

    This happens because a request is prepared, then
    a BKOPS is sent. The IRQ completing the BKOPS command
    goes through mmci_data_irq() and thinks that a DMA
    operation has just been completed because
    dma_inprogress() reports true. It then proceeds to
    unmap the sglist.

    But that was wrong! dma_inprogress() should NOT be
    true because no DMA was actually in progress! We had
    just prepared the sglist, and the DMA channel
    dma_current has been configured, but NOT started!

    Because of this, the sglist is already unmapped when
    we get our actual data completion IRQ, and we are
    unmapping the sglist once more, and we get this crash.

    Therefore, we need to revert this solution pushing
    the problem to the core and causing problems, and
    instead augment the implementation such that
    dma_inprogress() only reports true if some DMA has
    actually been started.

    After this we can keep the request prepared during the
    BKOPS and we need not unprepare/reprepare it.

    Fixes: 64b12a68a9f7 ("mmc: core: fix prepared requests while doing bkops")

drivers/mmc/core/core.c
@@ -675,16 +675,7 @@ struct mmc_async_req *mmc_start_req(struct mmc_host *host,
 		    ((mmc_resp_type(host->areq->mrq->cmd) == MMC_RSP_R1) ||
 		     (mmc_resp_type(host->areq->mrq->cmd) == MMC_RSP_R1B)) &&
 		    (host->areq->mrq->cmd->resp[0] & R1_EXCEPTION_EVENT)) {
-
-			/* Cancel the prepared request */
-			if (areq)
-				mmc_post_req(host, areq->mrq, -EINVAL);
-
 			mmc_start_bkops(host->card, true);
-
-			/* prepare the request again */
-			if (areq)
-				mmc_pre_req(host, areq->mrq);
 		}
 	}
 