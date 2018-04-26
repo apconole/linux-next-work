5ff614caa4e1db92da138e92c0ebddd48e05c20a
Hendrik Brueckner <brueckner@redhat.com>
2018-04-26 13:42:01 -0400

    [s390] report spectre mitigation via syslog
    Message-id: <1524750125-24215-13-git-send-email-brueckner@redhat.com>
    Patchwork-id: 211562
    O-Subject: [RHEL7.6 PATCH 12/16] [s390] s390: report spectre mitigation via syslog
    Bugzilla: 1558325
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    
    Description
    -----------
    commit bc035599718412cfba9249aa713f90ef13f13ee9
    Author: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Date:   Tue Mar 20 16:33:43 2018 +0100
    
        s390: report spectre mitigation via syslog
    
        Add a boot message if either of the spectre defenses is active.
        The message is
            "Spectre V2 mitigation: execute trampolines."
        or  "Spectre V2 mitigation: limited branch prediction."
    
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Bugzilla
    --------
    BZ 1558325
    https://bugzilla.redhat.com/show_bug.cgi?id=1558325
    
    Upstream status of the patch
    ----------------------------
    The patch is upstream as of kernel version
    http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commitdiff;h=bc035599718412cfba9249aa713f90ef13f13ee9
    
    Brew
    ----
    https://brewweb.devel.redhat.com/taskinfo?taskID=15870589
    
    Test status
    -----------
    The patch has been tested and fixes the problem.
    The fix has been verified by the IBM test department.
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/s390/kernel/nospec-branch.c
+++ arch/s390/kernel/nospec-branch.c
@@ -40,6 +40,16 @@ static int __init nogmb_setup_early(char *str)
 }
 early_param("nogmb", nogmb_setup_early);
 
+static int __init nospec_report(void)
+{
+	if (IS_ENABLED(CC_USING_EXPOLINE) && !nospec_disable)
+		pr_info("Spectre V2 mitigation: execute trampolines.\n");
+	if (__test_facility(82, S390_lowcore.alt_stfle_fac_list))
+		pr_info("Spectre V2 mitigation: limited branch prediction.\n");
+	return 0;
+}
+arch_initcall(nospec_report);
+
 #ifdef CONFIG_EXPOLINE
 
 int nospec_disable = IS_ENABLED(CONFIG_EXPOLINE_OFF);
