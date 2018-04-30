246c488d4c8d8e9499a61ddfe5ad28bd3bed73f9
Ivan Vecera <ivecera@redhat.com>
2018-04-30 15:07:55 -0400

    [net] ipv6: addrconf: don't evaluate keep_addr_on_down twice
    Message-id: <20180430150755.15807-15-ivecera@redhat.com>
    Patchwork-id: 212012
    O-Subject: [RHEL7.6 net PATCH 14/14] ipv6: addrconf: don't evaluate keep_addr_on_down twice
    Bugzilla: 1500871
    RH-Acked-by: Paolo Abeni <pabeni@redhat.com>
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1500871
    
    Backport note: Context conflict due to missing commit 8ef802aa8ec8
    ("ipv6: prepare RCU lookups for idev->addr_list")
    
    Upstream commit(s):
    commit 0aef78aa7b39829a2a46c1c0e6e96276d744e07a
    Author: Ivan Vecera <cera@cera.cz>
    Date:   Tue Apr 24 19:51:29 2018 +0200
    
        ipv6: addrconf: don't evaluate keep_addr_on_down twice
    
        The addrconf_ifdown() evaluates keep_addr_on_down state twice. There
        is no need to do it.
    
        Cc: David Ahern <dsahern@gmail.com>
        Signed-off-by: Ivan Vecera <cera@cera.cz>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -3333,8 +3333,7 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 	struct inet6_dev *idev;
 	struct inet6_ifaddr *ifa, *tmp;
 	struct list_head del_list;
-	int _keep_addr;
-	bool keep_addr;
+	bool keep_addr = false;
 	int state, i;
 
 	ASSERT_RTNL();
@@ -3361,15 +3360,18 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 
 	}
 
-	/* aggregate the system setting and interface setting */
-	_keep_addr = net->ipv6.devconf_all->keep_addr_on_down;
-	if (!_keep_addr)
-		_keep_addr = idev->cnf.keep_addr_on_down;
-
 	/* combine the user config with event to determine if permanent
 	 * addresses are to be removed from address hash table
 	 */
-	keep_addr = !(how || _keep_addr <= 0 || idev->cnf.disable_ipv6);
+	if (!how && !idev->cnf.disable_ipv6) {
+		/* aggregate the system setting and interface setting */
+		int _keep_addr = net->ipv6.devconf_all->keep_addr_on_down;
+
+		if (!_keep_addr)
+			_keep_addr = idev->cnf.keep_addr_on_down;
+
+		keep_addr = (_keep_addr > 0);
+	}
 
 	/* Step 2: clear hash table */
 	for (i = 0; i < IN6_ADDR_HSIZE; i++) {
@@ -3421,11 +3423,6 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 		in6_ifa_put(ifa);
 		write_lock_bh(&idev->lock);
 	}
-
-	/* re-combine the user config with event to determine if permanent
-	 * addresses are to be removed from the interface list
-	 */
-	keep_addr = (!how && _keep_addr > 0 && !idev->cnf.disable_ipv6);
 
 	INIT_LIST_HEAD(&del_list);
 	list_for_each_entry_safe(ifa, tmp, &idev->addr_list, if_list) {
