1c65e631942df17cb750f296f924a8e195a5995c
Aristeu Rozanski <aris@redhat.com>
2018-05-02 18:17:02 -0400

    [mm] hwpoison: call shake_page() unconditionally
    Message-id: <20180502181658.636843426@redhat.com>
    Patchwork-id: 212179
    O-Subject: [RHEL7 PATCH 4/6] mm: hwpoison: call shake_page() unconditionally
    Bugzilla: 1525701
    RH-Acked-by: Luiz Capitulino <lcapitulino@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    BZ#1525701
    brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15925114
    Tested: with reproducer, KT1
    
    Conflicts: work around missing commits 64d37a2baf5e5c0f1009c0ef290a9027de721d66
    	   and cc637b1704d78b068c2eb700eec384c69ea56cdf
    
    commit 8bcb74de764aaa261d6af3ce5ac723e435f00ff4
    Author: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Date:   Wed May 3 14:56:19 2017 -0700
    
        mm: hwpoison: call shake_page() unconditionally
    
        shake_page() is called before going into core error handling code in
        order to ensure that the error page is flushed from lru_cache lists
        where pages stay during transferring among LRU lists.
    
        But currently it's not fully functional because when the page is linked
        to lru_cache by calling activate_page(), its PageLRU flag is set and
        shake_page() is skipped.  The result is to fail error handling with
        "still referenced by 1 users" message.
    
        When the page is linked to lru_cache by isolate_lru_page(), its PageLRU
        is clear, so that's fine.
    
        This patch makes shake_page() unconditionally called to avoild the
        failure.
    
        Fixes: 23a003bfd23ea9ea0b7756b920e51f64b284b468 ("mm/madvise: pass return code of memory_failure() to userspace")
        Link: http://lkml.kernel.org/r/20170417055948.GM31394@yexl-desktop
        Link: http://lkml.kernel.org/r/1493197841-23986-2-git-send-email-n-horiguchi@ah.jp.nec.com
        Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
        Reported-by: kernel test robot <lkp@intel.com>
        Cc: Xiaolong Ye <xiaolong.ye@intel.com>
        Cc: Chen Gong <gong.chen@linux.intel.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -233,6 +233,9 @@ static int kill_proc(struct task_struct *t, unsigned long addr, int trapno,
  */
 void shake_page(struct page *p, int access)
 {
+	if (PageHuge(p))
+		return;
+
 	if (!PageSlab(p)) {
 		lru_add_drain_all();
 		if (PageLRU(p))
@@ -1159,21 +1162,14 @@ int memory_failure(unsigned long pfn, int trapno, int flags)
 	 * The check (unnecessarily) ignores LRU pages being isolated and
 	 * walked by the page reclaim code, however that's not a big loss.
 	 */
-	if (!PageHuge(p)) {
-		if (!PageLRU(p))
-			shake_page(p, 0);
-		if (!PageLRU(p)) {
-			/*
-			 * shake_page could have turned it free.
-			 */
-			if (is_free_buddy_page(p)) {
-				if (flags & MF_COUNT_INCREASED)
-					action_result(pfn, "free buddy", DELAYED);
-				else
-					action_result(pfn, "free buddy, 2nd try", DELAYED);
-				return 0;
-			}
-		}
+	shake_page(p, 0);
+	/* shake_page could have turned it free. */
+	if (!PageLRU(p) && is_free_buddy_page(p)) {
+		if (flags & MF_COUNT_INCREASED)
+			action_result(pfn, "free buddy", DELAYED);
+		else
+			action_result(pfn, "free buddy, 2nd try", DELAYED);
+		return 0;
 	}
 
 	/*
