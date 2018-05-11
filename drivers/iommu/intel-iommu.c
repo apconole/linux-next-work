64092670f7bc72b6d2fff075c4c233e155eb307e
Jerry Snitselaar <jsnitsel@redhat.com>
2018-05-11 18:39:39 -0400

    [iommu] vt-d: avoid dev iotlb logic for domains with no dev iotlbs
    Message-id: <20180511183947.31600-2-jsnitsel@redhat.com>
    Patchwork-id: 214691
    O-Subject: [RHEL7.6 PATCH 1/9] iommu/vt-d: avoid dev iotlb logic for domains with no dev iotlbs
    Bugzilla: 1519117
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/1519117
    Upstream Status: 0824c5920b16fe11034f3b5d2d48456d282d83f9
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15886387
    Tested: kt1, and hp system where problem was experienced. Also tested by Intel where experienced
            during ixgbe testing.
    Conflicts: Some differences due to lack of PASID table support code, and
               RHEL7 places ats_enabled in pci_dev_rh shadow struct
    
    commit 0824c5920b16fe11034f3b5d2d48456d282d83f9
    Author: Omer Peleg <omer@cs.technion.ac.il>
    Date:   Wed Apr 20 19:03:35 2016 +0300
    
        iommu/vt-d: avoid dev iotlb logic for domains with no dev iotlbs
    
        This patch avoids taking the device_domain_lock in iommu_flush_dev_iotlb()
        for domains with no dev iotlb devices.
    
        Signed-off-by: Omer Peleg <omer@cs.technion.ac.il>
        [gvdl@google.com: fixed locking issues]
        Signed-off-by: Godfrey van der Linden <gvdl@google.com>
        [mad@cs.technion.ac.il: rebased and reworded the commit message]
        Signed-off-by: Adam Morrison <mad@cs.technion.ac.il>
        Reviewed-by: Shaohua Li <shli@fb.com>
        Reviewed-by: Ben Serebrin <serebrin@google.com>
        Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    
    Cc: Myron Stowe <myron.stowe@redhat.com>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Don Dutile <ddutile@redhat.com>
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -391,6 +391,7 @@ struct dmar_domain {
 					 * domain ids are 16 bit wide according
 					 * to VT-d spec, section 9.3 */
 
+	bool has_iotlb_device;
 	struct list_head devices;	/* all devices' list */
 	struct iova_domain iovad;	/* iova's that belong to this domain */
 
@@ -1460,9 +1461,34 @@ iommu_support_dev_iotlb (struct dmar_domain *domain, struct intel_iommu *iommu,
 	return info;
 }
 
+static void domain_update_iotlb(struct dmar_domain *domain)
+{
+	struct device_domain_info *info;
+	bool has_iotlb_device = false;
+
+	assert_spin_locked(&device_domain_lock);
+
+	list_for_each_entry(info, &domain->devices, link) {
+		struct pci_dev *pdev;
+
+		if (!info->dev || !dev_is_pci(info->dev))
+			continue;
+
+		pdev = to_pci_dev(info->dev);
+		if (pdev->pci_dev_rh->ats_enabled) {
+			has_iotlb_device = true;
+			break;
+		}
+	}
+
+	domain->has_iotlb_device = has_iotlb_device;
+}
+
 static void iommu_enable_dev_iotlb(struct device_domain_info *info)
 {
 	struct pci_dev *pdev;
+
+	assert_spin_locked(&device_domain_lock);
 
 	if (!info || !dev_is_pci(info->dev))
 		return;
@@ -1472,16 +1498,20 @@ static void iommu_enable_dev_iotlb(struct device_domain_info *info)
 		return;
 
 	info->ats.enabled = 1;
+	domain_update_iotlb(info->domain);
 	info->ats.qdep = pci_ats_queue_depth(pdev);
 }
 
 static void iommu_disable_dev_iotlb(struct device_domain_info *info)
 {
+	assert_spin_locked(&device_domain_lock);
+
 	if (!info->ats.enabled)
 		return;
 
 	pci_disable_ats(to_pci_dev(info->dev));
 	info->ats.enabled = 0;
+	domain_update_iotlb(info->domain);
 }
 
 static void iommu_flush_dev_iotlb(struct dmar_domain *domain,
@@ -1490,6 +1520,9 @@ static void iommu_flush_dev_iotlb(struct dmar_domain *domain,
 	u16 sid, qdep;
 	unsigned long flags;
 	struct device_domain_info *info;
+
+	if (!domain->has_iotlb_device)
+		return;
 
 	spin_lock_irqsave(&device_domain_lock, flags);
 	list_for_each_entry(info, &domain->devices, link) {
@@ -1709,6 +1742,7 @@ static struct dmar_domain *alloc_domain(int flags)
 	memset(domain, 0, sizeof(*domain));
 	domain->nid = -1;
 	domain->flags = flags;
+	domain->has_iotlb_device = false;
 	INIT_LIST_HEAD(&domain->devices);
 
 	return domain;
