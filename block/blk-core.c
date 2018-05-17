cf1f6ae7c11c0b1d652c571bfc75ff399f9be2f6
Ming Lei <ming.lei@redhat.com>
2018-05-17 01:07:40 -0400

    [block] add kblock_mod_delayed_work_on()
    Message-id: <20180517010754.30618-12-ming.lei@redhat.com>
    Patchwork-id: 215391
    O-Subject: [RHEL7.6 V2 11/25] block: add kblock_mod_delayed_work_on()
    Bugzilla: 1568817
    RH-Acked-by: David Milburn <dmilburn@redhat.com>
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    
    From: Jens Axboe <axboe@fb.com>
    
    BZ: 1568817
    
    commit 818cd1cbaa7b00bbc35452a76bebc681a65f1912
    Author: Jens Axboe <axboe@fb.com>
    Date:   Mon Apr 10 09:54:55 2017 -0600
    
        block: add kblock_mod_delayed_work_on()
    
        This modifies (or adds, if not currently pending) an existing
        delayed work item.
    
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Bart Van Assche <Bart.VanAssche@sandisk.com>
        Signed-off-by: Jens Axboe <axboe@fb.com>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -3274,6 +3274,13 @@ int kblockd_schedule_work(struct work_struct *work)
 }
 EXPORT_SYMBOL(kblockd_schedule_work);
 
+int kblockd_mod_delayed_work_on(int cpu, struct delayed_work *dwork,
+				unsigned long delay)
+{
+	return mod_delayed_work_on(cpu, kblockd_workqueue, dwork, delay);
+}
+EXPORT_SYMBOL(kblockd_mod_delayed_work_on);
+
 int kblockd_schedule_delayed_work(struct delayed_work *dwork,
 				  unsigned long delay)
 {
--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1642,6 +1642,7 @@ struct work_struct;
 int kblockd_schedule_work(struct work_struct *work);
 int kblockd_schedule_delayed_work(struct delayed_work *dwork, unsigned long delay);
 int kblockd_schedule_delayed_work_on(int cpu, struct delayed_work *dwork, unsigned long delay);
+int kblockd_mod_delayed_work_on(int cpu, struct delayed_work *dwork, unsigned long delay);
 
 #ifdef CONFIG_BLK_CGROUP
 /*
