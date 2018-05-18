4ada33b6da5149461e7bbe23ebadb9298c32b6b0
Torez Smith <torez@redhat.com>
2018-05-18 13:11:17 -0400

    [usb] xhci: Fix command ring stop regression in 4.11
    Message-id: <1526649550-17190-209-git-send-email-torez@redhat.com>
    Patchwork-id: 215976
    O-Subject: [RHEL7.6 PATCH BZ1540721 208/681] xhci: Fix command ring stop regression in 4.11
    Bugzilla: 1540721
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1540721
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16352184
    Testing: on local desktop and Dell Precision 5510 laptop
    
    commit 604d02a2a66ab7f93fd3b2bde3698c29ef057b65
    Author: Mathias Nyman <mathias.nyman@linux.intel.com>
    Date:   Wed May 17 18:32:05 2017 +0300
    
        xhci: Fix command ring stop regression in 4.11
    
        In 4.11 TRB completion codes were renamed to match spec.
    
        Completion codes for command ring stopped and endpoint stopped
        were mixed, leading to failures while handling a stopped command ring.
    
        Use the correct completion code for command ring stopped events.
    
        Fixes: 0b7c105a04ca ("usb: host: xhci: rename completion codes to match spec")
        Cc: <stable@vger.kernel.org> # 4.11
        Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -1772,7 +1772,7 @@ static int xhci_configure_endpoint_result(struct xhci_hcd *xhci,
 
 	switch (*cmd_status) {
 	case COMP_COMMAND_ABORTED:
-	case COMP_STOPPED:
+	case COMP_COMMAND_RING_STOPPED:
 		xhci_warn(xhci, "Timeout while waiting for configure endpoint command\n");
 		ret = -ETIME;
 		break;
@@ -1822,7 +1822,7 @@ static int xhci_evaluate_context_result(struct xhci_hcd *xhci,
 
 	switch (*cmd_status) {
 	case COMP_COMMAND_ABORTED:
-	case COMP_STOPPED:
+	case COMP_COMMAND_RING_STOPPED:
 		xhci_warn(xhci, "Timeout while waiting for evaluate context command\n");
 		ret = -ETIME;
 		break;
@@ -3441,7 +3441,7 @@ static int xhci_discover_or_reset_device(struct usb_hcd *hcd,
 	ret = reset_device_cmd->status;
 	switch (ret) {
 	case COMP_COMMAND_ABORTED:
-	case COMP_STOPPED:
+	case COMP_COMMAND_RING_STOPPED:
 		xhci_warn(xhci, "Timeout waiting for reset device command\n");
 		ret = -ETIME;
 		goto command_cleanup;
@@ -3826,7 +3826,7 @@ static int xhci_setup_device(struct usb_hcd *hcd, struct usb_device *udev,
 	 */
 	switch (command->status) {
 	case COMP_COMMAND_ABORTED:
-	case COMP_STOPPED:
+	case COMP_COMMAND_RING_STOPPED:
 		xhci_warn(xhci, "Timeout while waiting for setup device command\n");
 		ret = -ETIME;
 		break;
