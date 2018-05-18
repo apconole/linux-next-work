42703b9715911efaa2e14849370a45adbb3750d2
Torez Smith <torez@redhat.com>
2018-05-18 13:11:47 -0400

    [usb] r8152: move the initialization to reset_resume function
    Message-id: <1526649550-17190-239-git-send-email-torez@redhat.com>
    Patchwork-id: 216019
    O-Subject: [RHEL7.6 PATCH BZ1540721 238/681] r8152: move the initialization to reset_resume function
    Bugzilla: 1540721
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1540721
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16352184
    Testing: on local desktop and Dell Precision 5510 laptop
    
    Conflicts: Adjustments for line changes and fuzz due to code shifts.
    
    commit befb2de18195d1ab6b843dd6ca921a222a32a588
    Author: hayeswang <hayeswang@realtek.com>
    Date:   Fri Jun 9 17:11:45 2017 +0800
    
        r8152: move the initialization to reset_resume function
    
        Move tp->rtl_ops.init() from rtl8152_resume() to rtl8152_reset_resume().
        The initialization is only necessary for reset_resume().
    
        Signed-off-by: Hayes Wang <hayeswang@realtek.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -3828,6 +3828,10 @@ static int rtl8152_reset_resume(struct usb_interface *intf)
 	struct r8152 *tp = usb_get_intfdata(intf);
 
 	clear_bit(SELECTIVE_SUSPEND, &tp->flags);
+	mutex_lock(&tp->control);
+	tp->rtl_ops.init(tp);
+	queue_delayed_work(system_long_wq, &tp->hw_phy_work, 0);
+	mutex_unlock(&tp->control);
 	return rtl8152_resume(intf);
 }
 
