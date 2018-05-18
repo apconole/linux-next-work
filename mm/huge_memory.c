91004f4b650f6e66d99d0eb8c3c96112f0052a44
Rafael Aquini <aquini@redhat.com>
2018-05-18 19:39:46 -0400

    [mm] pmd dirty emulation in page fault handler
    Message-id: <0c3277b04566d7b4cf0b2aea773ad549360ef310.1526665347.git.aquini@redhat.com>
    Patchwork-id: 216501
    O-Subject: [RHEL7 RHBZ 1562137 PATCH 26/27] mm: pmd dirty emulation in page fault handler
    Bugzilla: 1562137
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    RH-Acked-by: Larry Woodman <lwoodman@redhat.com>
    
    This patch is a backport of the following upstream commit:
    commit 20f664aabeb88d582b623a625f83b0454fa34f07
    Author: Minchan Kim <minchan@kernel.org>
    Date:   Tue Jan 10 16:57:51 2017 -0800
    
        mm/pmd dirty emulation in page fault handler
    
        Andreas reported [1] made a test in jemalloc hang in THP mode in arm64:
    
          http://lkml.kernel.org/r/mvmmvfy37g1.fsf@hawking.suse.de
    
        The problem is currently page fault handler doesn't supports dirty bit
        emulation of pmd for non-HW dirty-bit architecture so that application
        stucks until VM marked the pmd dirty.
    
        How the emulation work depends on the architecture.  In case of arm64,
        when it set up pte firstly, it sets pte PTE_RDONLY to get a chance to
        mark the pte dirty via triggering page fault when store access happens.
        Once the page fault occurs, VM marks the pmd dirty and arch code for
        setting pmd will clear PTE_RDONLY for application to proceed.
    
        IOW, if VM doesn't mark the pmd dirty, application hangs forever by
        repeated fault(i.e., store op but the pmd is PTE_RDONLY).
    
        This patch enables pmd dirty-bit emulation for those architectures.
    
        [1] b8d3c4c3009d, mm/huge_memory.c: don't split THP page when MADV_FREE syscall is called
    
        Fixes: b8d3c4c3009d ("mm/huge_memory.c: don't split THP page when MADV_FREE syscall is called")
        Link: http://lkml.kernel.org/r/1482506098-6149-1-git-send-email-minchan@kernel.org
        Signed-off-by: Minchan Kim <minchan@kernel.org>
        Reported-by: Andreas Schwab <schwab@suse.de>
        Tested-by: Andreas Schwab <schwab@suse.de>
        Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
        Acked-by: Michal Hocko <mhocko@suse.com>
        Cc: Jason Evans <je@fb.com>
        Cc: Will Deacon <will.deacon@arm.com>
        Cc: Catalin Marinas <catalin.marinas@arm.com>
        Cc: <stable@vger.kernel.org> [4.5+]
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Rafael Aquini <aquini@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1265,15 +1265,17 @@ void huge_pmd_set_accessed(struct vm_fault *vmf, pmd_t orig_pmd)
 	spinlock_t *ptl;
 	pmd_t entry;
 	unsigned long haddr;
+	bool write = vmf->flags & FAULT_FLAG_WRITE;
 
 	ptl = pmd_lock(vma->vm_mm, vmf->pmd);
 	if (unlikely(!pmd_same(*vmf->pmd, orig_pmd)))
 		goto unlock;
 
 	entry = pmd_mkyoung(orig_pmd);
+	if (write)
+		entry = pmd_mkdirty(entry);
 	haddr = address & HPAGE_PMD_MASK;
-	if (pmdp_set_access_flags(vmf->vma, haddr, vmf->pmd, entry,
-				vmf->flags & FAULT_FLAG_WRITE))
+	if (pmdp_set_access_flags(vmf->vma, haddr, vmf->pmd, entry, write))
 		update_mmu_cache_pmd(vma, address, vmf->pmd);
 
 unlock:
