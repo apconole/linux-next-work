17ba2d67590493fd9f1bb3829b2994c8449b4b41
Torez Smith <torez@redhat.com>
2018-05-18 13:10:55 -0400

    [usb] core: replace %p with %pK
    Message-id: <1526649550-17190-187-git-send-email-torez@redhat.com>
    Patchwork-id: 215970
    O-Subject: [RHEL7.6 PATCH BZ1540721 186/681] USB: core: replace %p with %pK
    Bugzilla: 1540721
    RH-Acked-by: Jerry Snitselaar <jsnitsel@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1540721
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16352184
    Testing: on local desktop and Dell Precision 5510 laptop
    
    commit 2f964780c03b73de269b08d12aff96a9618d13f3
    Author: Vamsi Krishna Samavedam <vskrishn@codeaurora.org>
    Date:   Tue May 16 14:38:08 2017 +0200
    
        USB: core: replace %p with %pK
    
        Format specifier %p can leak kernel addresses while not valuing the
        kptr_restrict system settings. When kptr_restrict is set to (1), kernel
        pointers printed using the %pK format specifier will be replaced with
        Zeros. Debugging Note : &pK prints only Zeros as address. If you need
        actual address information, write 0 to kptr_restrict.
    
        echo 0 > /proc/sys/kernel/kptr_restrict
    
        [Found by poking around in a random vendor kernel tree, it would be nice
        if someone would actually send these types of patches upstream - gkh]
    
        Signed-off-by: Vamsi Krishna Samavedam <vskrishn@codeaurora.org>
        Cc: stable <stable@vger.kernel.org>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Torez Smith <torez@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -474,11 +474,11 @@ static void snoop_urb(struct usb_device *udev,
 
 	if (userurb) {		/* Async */
 		if (when == SUBMIT)
-			dev_info(&udev->dev, "userurb %p, ep%d %s-%s, "
+			dev_info(&udev->dev, "userurb %pK, ep%d %s-%s, "
 					"length %u\n",
 					userurb, ep, t, d, length);
 		else
-			dev_info(&udev->dev, "userurb %p, ep%d %s-%s, "
+			dev_info(&udev->dev, "userurb %pK, ep%d %s-%s, "
 					"actual_length %u status %d\n",
 					userurb, ep, t, d, length,
 					timeout_or_status);
@@ -1894,7 +1894,7 @@ static int proc_reapurb(struct usb_dev_state *ps, void __user *arg)
 	if (as) {
 		int retval;
 
-		snoop(&ps->dev->dev, "reap %p\n", as->userurb);
+		snoop(&ps->dev->dev, "reap %pK\n", as->userurb);
 		retval = processcompl(as, (void __user * __user *)arg);
 		free_async(as);
 		return retval;
@@ -1911,7 +1911,7 @@ static int proc_reapurbnonblock(struct usb_dev_state *ps, void __user *arg)
 
 	as = async_getcompleted(ps);
 	if (as) {
-		snoop(&ps->dev->dev, "reap %p\n", as->userurb);
+		snoop(&ps->dev->dev, "reap %pK\n", as->userurb);
 		retval = processcompl(as, (void __user * __user *)arg);
 		free_async(as);
 	} else {
@@ -2042,7 +2042,7 @@ static int proc_reapurb_compat(struct usb_dev_state *ps, void __user *arg)
 	if (as) {
 		int retval;
 
-		snoop(&ps->dev->dev, "reap %p\n", as->userurb);
+		snoop(&ps->dev->dev, "reap %pK\n", as->userurb);
 		retval = processcompl_compat(as, (void __user * __user *)arg);
 		free_async(as);
 		return retval;
@@ -2059,7 +2059,7 @@ static int proc_reapurbnonblock_compat(struct usb_dev_state *ps, void __user *ar
 
 	as = async_getcompleted(ps);
 	if (as) {
-		snoop(&ps->dev->dev, "reap %p\n", as->userurb);
+		snoop(&ps->dev->dev, "reap %pK\n", as->userurb);
 		retval = processcompl_compat(as, (void __user * __user *)arg);
 		free_async(as);
 	} else {
@@ -2488,7 +2488,7 @@ static long usbdev_do_ioctl(struct file *file, unsigned int cmd,
 #endif
 
 	case USBDEVFS_DISCARDURB:
-		snoop(&dev->dev, "%s: DISCARDURB %p\n", __func__, p);
+		snoop(&dev->dev, "%s: DISCARDURB %pK\n", __func__, p);
 		ret = proc_unlinkurb(ps, p);
 		break;
 
