a8559ba585393b244697e5e87de6d39c9b24f2b7
Waiman Long <longman@redhat.com>
2018-05-22 13:19:00 -0400

    [kernel] nospec: Allow getting/setting on non-current task
    Message-id: <1526995149-19569-13-git-send-email-longman@redhat.com>
    Patchwork-id: 216790
    O-Subject: [RHEL7.6 PATCH 12/21] nospec: Allow getting/setting on non-current task
    Bugzilla: 1566905
    CVE: CVE-2018-3639
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1566905
    CVE: CVE-2018-3639 ("Speculative Store Bypass")
    
    commit 7bbf1373e228840bb0295a2ca26d548ef37f448e
    Author: Kees Cook <keescook@chromium.org>
    Date:   Tue May 1 15:19:04 2018 -0700
    
        nospec: Allow getting/setting on non-current task
    
        Adjust arch_prctl_get/set_spec_ctrl() to operate on tasks other than
        current.
    
        This is needed both for /proc/$pid/status queries and for seccomp (since
        thread-syncing can trigger seccomp in non-current threads).
    
        Signed-off-by: Kees Cook <keescook@chromium.org>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/nospec.h
+++ include/linux/nospec.h
@@ -6,8 +6,11 @@
 #ifndef _LINUX_NOSPEC_H
 #define _LINUX_NOSPEC_H
 
+struct task_struct;
+
 /* Speculation control prctl */
-int arch_prctl_spec_ctrl_get(unsigned long which);
-int arch_prctl_spec_ctrl_set(unsigned long which, unsigned long ctrl);
+int arch_prctl_spec_ctrl_get(struct task_struct *task, unsigned long which);
+int arch_prctl_spec_ctrl_set(struct task_struct *task, unsigned long which,
+			     unsigned long ctrl);
 
 #endif /* _LINUX_NOSPEC_H */
