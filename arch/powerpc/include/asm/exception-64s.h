fb188372926888bd5ed22d00a462d66754e6d6b6
Mauricio Oliveira <maolivei@redhat.com>
2018-05-25 17:17:21 -0400

    [powerpc] 64s: Add support for a store forwarding barrier at kernel entry/exit
    Message-id: <1527268644-29465-2-git-send-email-maolivei@redhat.com>
    Patchwork-id: 217470
    O-Subject: [RHEL-7.6 PATCH v2 1/4] powerpc/64s: Add support for a store forwarding barrier at kernel entry/exit
    Bugzilla: 1581036
    CVE: CVE-2018-3639
    RH-Acked-by: David Gibson <dgibson@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1581036
    Upstream Status: https://git.kernel.org/torvalds/c/a048a07d7f4535baa4cbad6bc024f175317ab938
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16454579
    Tested: https://bugzilla.redhat.com/show_bug.cgi?id=1581036#c4
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    
     - exception-64s.h
    	hunk 1: update context lines for beginning of file
    	hunk 2: update context lines for  RFI_TO_USER() with CHECK_TARGET_MSR_PR()
    	hunk 3: update context lines for HRFI_TO_USER() with CHECK_TARGET_MSR_PR()
    	hunk 4:	update context lines to match __EXCEPTION_PROLOG_1()
    		instead of the non-existing   __EXCEPTION_PROLOG_1_PRE()
    		due to the lack of non-required upstream commit f14e953b1
    		("powerpc/64s: Add support to take additional parameter in MASKABLE_* macro")
    
     - feature-fixups.h
    	hunks 1 and 2: update context lines
    
     - exceptions-64s.S
    	hunk 1: update context lines.
    		annotate with TODO (handle in next patches for clarity).
    	hunk 2: update context lines for system_call_pSeries assembly routine
    		(insert INTERRUPT_TO_KERNEL macro before the KVMTEST() macro).
    		merge the equivalent of the next hunk in this hunk
    		(the 'else' block), see the next hunk for description.
    	hunk 3: merged into previous hunk.
    		insert INTERRUPT_TO_KERNEL in non-KVM config, for completeness.
    		use volatile r12 register as temporary register for r13 backup.
    	hunk 4: update context lines for above rfi_flush_fallback.
    		convert TRAMP_REAL_BEGIN(label) to ".globl label/label:".
    		insert leading/trailing changes for r13 = PACA_AUX/PACA
    		(downstream shadow/auxiliary paca structure for kABI).
    		s/PACA_EXRFI/PACA_AUX_EXRFI/
    		(offset in downstream shadow/auxiliary paca structure for kABI).
    	NEW:	add hunk for system_call_relon_pSeries routine
    		(similarly to non-KVM case in system_call_pSeries)
    
     - security.c
    	hunk 1: use 'linux/debugfs.h' instead of 'asm/debugfs.h'
    
     - feature-fixups.c
    	hunk 1: update context lines
    
     - pseries/setup.c
    	hunk 1: s/overridden/overriden/
    
    commit a048a07d7f4535baa4cbad6bc024f175317ab938
    Author: Nicholas Piggin <npiggin@gmail.com>
    Date:   Tue May 22 09:00:00 2018 +1000
    
        powerpc/64s: Add support for a store forwarding barrier at kernel entry/exit
    
        On some CPUs we can prevent a vulnerability related to store-to-load
        forwarding by preventing store forwarding between privilege domains,
        by inserting a barrier in kernel entry and exit paths.
    
        This is known to be the case on at least Power7, Power8 and Power9
        powerpc CPUs.
    
        Barriers must be inserted generally before the first load after moving
        to a higher privilege, and after the last store before moving to a
        lower privilege, HV and PR privilege transitions must be protected.
    
        Barriers are added as patch sections, with all kernel/hypervisor entry
        points patched, and the exit points to lower privilge levels patched
        similarly to the RFI flush patching.
    
        Firmware advertisement is not implemented yet, so CPU flush types
        are hard coded.
    
        Thanks to Michal Suchanek for bug fixes and review.
    
        Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
        Signed-off-by: Mauricio Faria de Oliveira <mauricfo@linux.vnet.ibm.com>
        Signed-off-by: Michael Neuling <mikey@neuling.org>
        Signed-off-by: Michal Suchanek <msuchanek@suse.de>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Mauricio Oliveira <maolivei@redhat.com>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -51,6 +51,27 @@
 #define EX_PPR		88	/* SMT thread status register (priority) */
 #define EX_CTR		96
 
+#define STF_ENTRY_BARRIER_SLOT						\
+	STF_ENTRY_BARRIER_FIXUP_SECTION;				\
+	nop;								\
+	nop;								\
+	nop
+
+#define STF_EXIT_BARRIER_SLOT						\
+	STF_EXIT_BARRIER_FIXUP_SECTION;					\
+	nop;								\
+	nop;								\
+	nop;								\
+	nop;								\
+	nop;								\
+	nop
+
+/*
+ * r10 must be free to use, r13 must be paca
+ */
+#define INTERRUPT_TO_KERNEL						\
+	STF_ENTRY_BARRIER_SLOT
+
 /*
  * The nop instructions allow us to insert one or more instructions to flush the
  * L1-D cache when return to userspace or a guest.
@@ -79,16 +100,19 @@
 
 #define RFI_TO_USER							\
 	CHECK_TARGET_MSR_PR(SPRN_SRR1, 1);				\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	rfid;								\
 	b	rfi_flush_fallback
 
 #define RFI_TO_USER_OR_KERNEL						\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	rfid;								\
 	b	rfi_flush_fallback
 
 #define RFI_TO_GUEST							\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	rfid;								\
 	b	rfi_flush_fallback
@@ -99,21 +123,25 @@
 
 #define HRFI_TO_USER							\
 	CHECK_TARGET_MSR_PR(SPRN_HSRR1, 1);				\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	hrfid;								\
 	b	hrfi_flush_fallback
 
 #define HRFI_TO_USER_OR_KERNEL						\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	hrfid;								\
 	b	hrfi_flush_fallback
 
 #define HRFI_TO_GUEST							\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	hrfid;								\
 	b	hrfi_flush_fallback
 
 #define HRFI_TO_UNKNOWN							\
+	STF_EXIT_BARRIER_SLOT;						\
 	RFI_FLUSH_SLOT;							\
 	hrfid;								\
 	b	hrfi_flush_fallback
@@ -241,6 +269,7 @@ END_FTR_SECTION_NESTED(ftr,ftr,943)
 #define __EXCEPTION_PROLOG_1(area, extra, vec)				\
 	OPT_SAVE_REG_TO_PACA(area+EX_PPR, r9, CPU_FTR_HAS_PPR);		\
 	OPT_SAVE_REG_TO_PACA(area+EX_CFAR, r10, CPU_FTR_CFAR);		\
+	INTERRUPT_TO_KERNEL;						\
 	SAVE_CTR(r10, area);						\
 	mfcr	r9;							\
 	extra(vec);							\
