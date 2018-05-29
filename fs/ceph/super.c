71166ff630e8a006e1995ed9ceefd474d0e7aa13
Ilya Dryomov <idryomov@redhat.com>
2018-05-29 16:17:43 -0400

    [fs] ceph: optimize memory usage
    Message-id: <1527610684-14570-82-git-send-email-idryomov@redhat.com>
    Patchwork-id: 218053
    O-Subject: [RHEL7.6 fs PATCH 081/102] ceph: optimize memory usage
    Bugzilla: 1580500
    RH-Acked-by: Zheng Yan <zyan@redhat.com>
    RH-Acked-by: Sage Weil <sweil@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    From: Chengguang Xu <cgxu519@gmx.com>
    
    In current code, regular file and directory use same struct
    ceph_file_info to store fs specific data so the struct has to
    include some fields which are only used for directory
    (e.g., readdir related info), when having plenty of regular files,
    it will lead to memory waste.
    
    This patch introduces dedicated ceph_dir_file_info cache for
    readdir related thins. So that regular file does not include those
    unused fields anymore.
    
    Signed-off-by: Chengguang Xu <cgxu519@gmx.com>
    Reviewed-by: "Yan, Zheng" <zyan@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit bb48bd4dc45f9ee1e44d8e9fcb01023e0d0ba80d)
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    	fs/ceph/dir.c [ struct dir_context not in RHEL; context:
    	  d_inode() not in RHEL, readdir interface in RHEL vs upstream ]
    	fs/ceph/super.c [ context: ceph fscache not in RHEL ]
    ---

--- fs/ceph/super.c
+++ fs/ceph/super.c
@@ -666,6 +666,7 @@ struct kmem_cache *ceph_cap_cachep;
 struct kmem_cache *ceph_cap_flush_cachep;
 struct kmem_cache *ceph_dentry_cachep;
 struct kmem_cache *ceph_file_cachep;
+struct kmem_cache *ceph_dir_file_cachep;
 
 static void ceph_inode_init_once(void *foo)
 {
@@ -701,8 +702,14 @@ static int __init init_caches(void)
 	if (!ceph_file_cachep)
 		goto bad_file;
 
+	ceph_dir_file_cachep = KMEM_CACHE(ceph_dir_file_info, SLAB_MEM_SPREAD);
+	if (!ceph_dir_file_cachep)
+		goto bad_dir_file;
+
 	return 0;
 
+bad_dir_file:
+	kmem_cache_destroy(ceph_file_cachep);
 bad_file:
 	kmem_cache_destroy(ceph_dentry_cachep);
 bad_dentry:
@@ -726,6 +733,7 @@ static void destroy_caches(void)
 	kmem_cache_destroy(ceph_cap_flush_cachep);
 	kmem_cache_destroy(ceph_dentry_cachep);
 	kmem_cache_destroy(ceph_file_cachep);
+	kmem_cache_destroy(ceph_dir_file_cachep);
 }
 
 
