57caddcf807f7cada7c9a63b0834c5c38cda134b
Waiman Long <longman@redhat.com>
2018-05-31 14:33:27 -0400

    [x86] speculation: Handle HT correctly on AMD
    Message-id: <1527777225-30739-17-git-send-email-longman@redhat.com>
    Patchwork-id: 218475
    O-Subject: [RHEL7.6 PATCH 16/34] x86/speculation: Handle HT correctly on AMD
    Bugzilla: 1584569
    CVE: CVE-2018-3639
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1584569
    CVE: CVE-2018-3639 ("Speculative Store Bypass")
    
    commit 1f50ddb4f4189243c05926b842dc1a0332195f31
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Wed May 9 21:53:09 2018 +0200
    
        x86/speculation: Handle HT correctly on AMD
    
        The AMD64_LS_CFG MSR is a per core MSR on Family 17H CPUs. That means when
        hyperthreading is enabled the SSBD bit toggle needs to take both cores into
        account. Otherwise the following situation can happen:
    
        CPU0                CPU1
    
        disable SSB
                    disable SSB
                    enable  SSB <- Enables it for the Core, i.e. for CPU0 as well
    
        So after the SSB enable on CPU1 the task on CPU0 runs with SSB enabled
        again.
    
        On Intel the SSBD control is per core as well, but the synchronization
        logic is implemented behind the per thread SPEC_CTRL MSR. It works like
        this:
    
          CORE_SPEC_CTRL = THREAD0_SPEC_CTRL | THREAD1_SPEC_CTRL
    
        i.e. if one of the threads enables a mitigation then this affects both and
        the mitigation is only disabled in the core when both threads disabled it.
    
        Add the necessary synchronization logic for AMD family 17H.  Unfortunately
        that requires a spinlock to serialize the access to the MSR, but the locks
        are only shared between siblings.
    
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Borislav Petkov <bp@suse.de>
        Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -237,6 +237,8 @@ static void notrace start_secondary(void *unused)
 	 */
 	check_tsc_sync_target();
 
+	speculative_store_bypass_ht_init();
+
 	/*
 	 * We need to hold vector_lock so there the set of online cpus
 	 * does not change while we are assigning vectors to cpus.  Holding
@@ -1328,6 +1330,8 @@ void __init native_smp_prepare_cpus(unsigned int max_cpus)
 	set_mtrr_aps_delayed_init();
 
 	smp_quirk_init_udelay();
+
+	speculative_store_bypass_ht_init();
 out:
 	preempt_enable();
 }
