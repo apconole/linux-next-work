80d972ae93874a03b4691b13de9390a8b2f69143
Ivan Vecera <ivecera@redhat.com>
2018-05-31 10:52:04 -0400

    [net] sched: don't set q pointer for shared blocks
    Message-id: <20180531105205.21390-13-ivecera@redhat.com>
    Patchwork-id: 218437
    O-Subject: [RHEL7.6 net PATCH 12/13] net: sched: don't set q pointer for shared blocks
    Bugzilla: 1584592
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1584292
    
    Backport notes:
    - Using idr_alloc_ext() instead of idr_alloc_u32() as the conversion
      idr_alloc_ext->idr_alloc_u32 is not part of this series
    - Adjustments due to absence of extack support in RHEL
    
    Upstream commit(s):
    commit bb047ddd145860ff24820320a21f03cf8c071b22
    Author: Jiri Pirko <jiri@mellanox.com>
    Date:   Tue Feb 13 12:00:16 2018 +0100
    
        net: sched: don't set q pointer for shared blocks
    
        It is pointless to set block->q for block which are shared among
        multiple qdiscs. So remove the assignment in that case. Do a bit of code
        reshuffle to make block->index initialized at that point so we can use
        tcf_block_shared() helper.
    
        Reported-by: Cong Wang <xiyou.wangcong@gmail.com>
        Fixes: 4861738775d7 ("net: sched: introduce shared filter blocks infrastructure")
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -371,18 +371,12 @@ struct tcf_net {
 
 static unsigned int tcf_net_id;
 
-static int tcf_block_insert(struct tcf_block *block, struct net *net,
-			    u32 block_index)
+static int tcf_block_insert(struct tcf_block *block, struct net *net)
 {
 	struct tcf_net *tn = net_generic(net, tcf_net_id);
-	int err;
 
-	err = idr_alloc_ext(&tn->idr, block, NULL, block_index,
-			    block_index + 1, GFP_KERNEL);
-	if (err)
-		return err;
-	block->index = block_index;
-	return 0;
+	return idr_alloc_ext(&tn->idr, block, NULL, block->index,
+			     block->index + 1, GFP_KERNEL);
 }
 
 static void tcf_block_remove(struct tcf_block *block, struct net *net)
@@ -392,7 +386,8 @@ static void tcf_block_remove(struct tcf_block *block, struct net *net)
 	idr_remove_ext(&tn->idr, block->index);
 }
 
-static struct tcf_block *tcf_block_create(struct net *net, struct Qdisc *q)
+static struct tcf_block *tcf_block_create(struct net *net, struct Qdisc *q,
+					  u32 block_index)
 {
 	struct tcf_block *block;
 	struct tcf_chain *chain;
@@ -412,10 +407,13 @@ static struct tcf_block *tcf_block_create(struct net *net, struct Qdisc *q)
 		err = -ENOMEM;
 		goto err_chain_create;
 	}
-	block->net = qdisc_net(q);
 	block->refcnt = 1;
 	block->net = net;
-	block->q = q;
+	block->index = block_index;
+
+	/* Don't store q pointer for blocks which are shared */
+	if (!tcf_block_shared(block))
+		block->q = q;
 	return block;
 
 err_chain_create:
@@ -510,13 +508,12 @@ int tcf_block_get_ext(struct tcf_block **p_block, struct Qdisc *q,
 	}
 
 	if (!block) {
-		block = tcf_block_create(net, q);
+		block = tcf_block_create(net, q, ei->block_index);
 		if (IS_ERR(block))
 			return PTR_ERR(block);
 		created = true;
-		if (ei->block_index) {
-			err = tcf_block_insert(block, net,
-					       ei->block_index);
+		if (tcf_block_shared(block)) {
+			err = tcf_block_insert(block, net);
 			if (err)
 				goto err_block_insert;
 		}
