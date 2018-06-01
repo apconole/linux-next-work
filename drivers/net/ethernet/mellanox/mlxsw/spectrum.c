3d5455f1dc2bfe45a07661e7aa1fc7a627be160a
Ivan Vecera <ivecera@redhat.com>
2018-06-01 18:47:55 -0400

    [netdrv] mlxsw: Move "resources_query_enable" out of mlxsw_config_profile
    Message-id: <20180601184800.22484-221-ivecera@redhat.com>
    Patchwork-id: 220179
    O-Subject: [RHEL7.6 PATCH 219/224] mlxsw: Move "resources_query_enable" out of mlxsw_config_profile
    Bugzilla: 1520304
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1520304
    
    Upstream commit(s):
    commit ad3f20b28602fd46b3a27b5a61be270a3794cc36
    Author: Jiri Pirko <jiri@mellanox.com>
    Date:   Sun Apr 1 17:34:57 2018 +0300
    
        mlxsw: Move "resources_query_enable" out of mlxsw_config_profile
    
        As struct mlxsw_config_profile is mapped to the payload of the FW
        command of the same name, resources_query_enable flag does not belong
        there. Move it to struct mlxsw_driver.
    
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: Ido Schimmel <idosch@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.c
@@ -3804,7 +3804,6 @@ static const struct mlxsw_config_profile mlxsw_sp_config_profile = {
 			.type		= MLXSW_PORT_SWID_TYPE_ETH,
 		}
 	},
-	.resource_query_enable		= 1,
 };
 
 static void
@@ -3987,6 +3986,7 @@ static struct mlxsw_driver mlxsw_sp_driver = {
 	.kvd_sizes_get			= mlxsw_sp_kvd_sizes_get,
 	.txhdr_len			= MLXSW_TXHDR_LEN,
 	.profile			= &mlxsw_sp_config_profile,
+	.res_query_enabled		= true,
 };
 
 bool mlxsw_sp_port_dev_check(const struct net_device *dev)
