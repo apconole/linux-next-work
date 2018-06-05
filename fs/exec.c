57d7fde0aac82484962d338f7f2bec2a2c00be82
Bhupesh Sharma <bhsharma@redhat.com>
2018-06-04 20:17:13 -0400

    [kernel] handle suicide on late failure exits in execve() in search_binary_handler()
    Message-id: <1528143433-7940-8-git-send-email-bhsharma@redhat.com>
    Patchwork-id: 220548
    O-Subject: [RHEL7.6 PATCH 7/7] handle suicide on late failure exits in execve() in search_binary_handler()
    Bugzilla: 1496408
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    RH-Acked-by: Joe Lawrence <joe.lawrence@redhat.com>
    RH-Acked-by: Oleg Nesterov <oleg@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1496408
    Upstream Status: commit 19d860a140beac48a1377f179e693abe86a9dac9
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=16579582
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    	fs/binfmt_elf.c (set_brk definition differs in RHEL7.6 as
    	                 compared to upstream)
    	fs/exec.c (search_binary_handler implementation differs as
    	           compared to upstream as we have not picked up all
    		   the relevant upstream patches to minimize the
    		   backport changes)
    
    commit 19d860a140beac48a1377f179e693abe86a9dac9
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Sun May 4 20:11:36 2014 -0400
    
        handle suicide on late failure exits in execve() in search_binary_handler()
    
        ... rather than doing that in the guts of ->load_binary().
        [updated to fix the bug spotted by Shentino - for SIGSEGV we really need
        something stronger than send_sig_info(); again, better do that in one place]
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
        (cherry picked from commit 19d860a140beac48a1377f179e693abe86a9dac9)
    
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1446,23 +1446,23 @@ int search_binary_handler(struct linux_binprm *bprm)
 		read_unlock(&binfmt_lock);
 		bprm->recursion_depth++;
 		retval = fmt->load_binary(bprm);
-		bprm->recursion_depth--;
-		if (retval >= 0) {
-			put_binfmt(fmt);
-			return retval;
-		}
 		read_lock(&binfmt_lock);
 		put_binfmt(fmt);
-		if (retval != -ENOEXEC || bprm->mm == NULL)
-			break;
-		if (!bprm->file) {
+		bprm->recursion_depth--;
+		if (retval < 0 && !bprm->mm) {
+			/* we got to flush_old_exec() and failed after it */
+			read_unlock(&binfmt_lock);
+			force_sigsegv(SIGSEGV, current);
+			return retval;
+		}
+		if (retval != -ENOEXEC || !bprm->file) {
 			read_unlock(&binfmt_lock);
 			return retval;
 		}
 	}
 	read_unlock(&binfmt_lock);
 
-	if (need_retry && retval == -ENOEXEC && bprm->mm) {
+	if (need_retry) {
 		if (printable(bprm->buf[0]) && printable(bprm->buf[1]) &&
 		    printable(bprm->buf[2]) && printable(bprm->buf[3]))
 			return retval;
