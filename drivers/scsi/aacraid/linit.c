f9e2e9a81119f8184e07465a0a71345f58fe1bcc
Raghava Aditya Renukunta <rrenukun@redhat.com>
2018-06-04 23:05:17 -0400

    [scsi] aacraid: Fix controller initialization failure
    Message-id: <20180604230516.25521.27591.stgit@dhcp187-32.khw.lab.eng.bos.redhat.com>
    Patchwork-id: 220609
    O-Subject: [RHEL7.6 e-stor PATCH 40/86] scsi: aacraid: Fix controller initialization failure
    Bugzilla: 1519664
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Backport linux.git commit 45348de2c8a7a1e64c5be27b22c9786b4152dd41
    
    Commit: 45348de2c8a7a1e64c5be27b22c9786b4152dd41 Mon Sep 17 00:00:00 2001
    Author: Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
    Date: 2017-10-16 17:22:31 -0700
    
    scsi: aacraid: Fix controller initialization failure
    
    This is a fix to an issue where the driver sends its periodic WELLNESS
    command to the controller after the driver shut it down.This causes the
    controller to crash. The window where this can happen is small, but it
    can be hit at around 4 hours of constant resets.
    
    [change]
    
    Cc: <stable@vger.kernel.org>
    Fixes: fbd185986eba (aacraid: Fix AIF triggered IOP_RESET)
    Signed-off-by: Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
    Reviewed-by: Dave Carroll <david.carroll@microsemi.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/scsi/aacraid/linit.c
+++ drivers/scsi/aacraid/linit.c
@@ -1645,8 +1645,9 @@ static void __aac_shutdown(struct aac_dev * aac)
 	int i;
 	int cpu;
 
+	mutex_lock(&aac->ioctl_mutex);
 	aac->adapter_shutdown = 1;
-	aac_send_shutdown(aac);
+	mutex_unlock(&aac->ioctl_mutex);
 
 	if (aac->aif_thread) {
 		int i;
@@ -1659,7 +1660,11 @@ static void __aac_shutdown(struct aac_dev * aac)
 		}
 		kthread_stop(aac->thread);
 	}
+
+	aac_send_shutdown(aac);
+
 	aac_adapter_disable_int(aac);
+
 	cpu = cpumask_first(cpu_online_mask);
 	if (aac_is_src(aac)) {
 		if (aac->max_msix > 1) {
