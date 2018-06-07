43ae625a5fd3dd41bedcaec8894b53cad4819587
Xiubo Li <xiubli@redhat.com>
2018-06-07 03:30:50 -0400

    [target] tcmu: Make dev_config configurable
    Message-id: <1528342275-37997-24-git-send-email-xiubli@redhat.com>
    Patchwork-id: 220866
    O-Subject: [RHEL 7.6 PATCHv2 23/48] tcmu: Make dev_config configurable
    Bugzilla: 1559232
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Mike Christie <mchristi@redhat.com>
    
    From: Mike Christie <mchristi@redhat.com>
    
    - BZ:
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1559232
    
    - Upstream differences:
    
    In the downstream patch:
    
        commit 05c63629230e62644d2e3cf4ec8a978e51b7243a
        Author: Maurizio Lombardi <mlombard@redhat.com>
        Date:   Wed Dec 6 12:56:52 2017 -0500
    
            [target] tcmu: reconfigure netlink attr changes
    
    We backported most of the core reconfig code. This patch is a mix
    of upstream commits to sync us up.
    
    - Upstream commits:
    
    commit ee01825220f01c0befea25f08325962fa9374ee2
    Author: Bryant G. Ly <bryantly@linux.vnet.ibm.com>
    Date:   Tue Jun 6 09:28:51 2017 -0500
    
        tcmu: Make dev_config configurable
    
    and parts of:
    
    commit 2d76443e02f260d7a5bd0ede1851ae5534f0c68d
    Author: Mike Christie <mchristi@redhat.com>
    Date:   Mon Jun 12 01:34:28 2017 -0500
    
        tcmu: reconfigure netlink attr changes
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/target/target_core_user.c
+++ drivers/target/target_core_user.c
@@ -1915,6 +1915,42 @@ static ssize_t tcmu_qfull_time_out_store(struct config_item *item,
 }
 CONFIGFS_ATTR(tcmu_, qfull_time_out);
 
+static ssize_t tcmu_dev_config_show(struct config_item *item, char *page)
+{
+	struct se_dev_attrib *da = container_of(to_config_group(item),
+						struct se_dev_attrib, da_group);
+	struct tcmu_dev *udev = TCMU_DEV(da->da_dev);
+
+	return snprintf(page, PAGE_SIZE, "%s\n", udev->dev_config);
+}
+
+static ssize_t tcmu_dev_config_store(struct config_item *item, const char *page,
+				     size_t count)
+{
+	struct se_dev_attrib *da = container_of(to_config_group(item),
+						struct se_dev_attrib, da_group);
+	struct tcmu_dev *udev = TCMU_DEV(da->da_dev);
+	int ret, len;
+
+	len = strlen(page);
+	if (!len || len > TCMU_CONFIG_LEN - 1)
+		return -EINVAL;
+
+	/* Check if device has been configured before */
+	if (tcmu_dev_configured(udev)) {
+		ret = tcmu_netlink_event(udev, TCMU_CMD_RECONFIG_DEVICE,
+					 TCMU_ATTR_DEV_CFG, page);
+		if (ret) {
+			pr_err("Unable to reconfigure device\n");
+			return ret;
+		}
+	}
+	strlcpy(udev->dev_config, page, TCMU_CONFIG_LEN);
+
+	return count;
+}
+CONFIGFS_ATTR(tcmu_, dev_config);
+
 static ssize_t tcmu_dev_size_show(struct config_item *item, char *page)
 {
 	struct se_dev_attrib *da = container_of(to_config_group(item),
@@ -2032,6 +2068,7 @@ struct configfs_attribute *tcmu_attrib_attrs[] = {
 	&tcmu_attr_cmd_time_out,
 	&tcmu_attr_qfull_time_out,
 	&tcmu_attr_max_data_area_mb,
+	&tcmu_attr_dev_config,
 	&tcmu_attr_dev_size,
 	&tcmu_attr_emulate_write_cache,
 	&tcmu_attr_nl_reply_supported,
