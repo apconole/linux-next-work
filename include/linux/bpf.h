32403768c43baedc10e767af36c51e6fee74538b
Jiri Olsa <jolsa@redhat.com>
2018-06-13 21:37:48 -0400

    [kernel] bpf: refactor bpf_prog_get and type check into helper
    Message-id: <20180613214317.28803-58-jolsa@redhat.com>
    Patchwork-id: 221616
    O-Subject: [PATCH RHEL7.6 BZ1311586 057/386] bpf: refactor bpf_prog_get and type check into helper
    Bugzilla: 1311586
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Eugene Syromiatnikov <esyr@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: 1311586
    https://bugzilla.redhat.com/show_bug.cgi?id=1311586
    
    upstream
    ========
    commit 113214be7f6c98dd6d0435e4765aea8dea91662c
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date: Thu Jun 30 17:24:44 2016 +0200
    
    description
    ===========
    Since bpf_prog_get() and program type check is used in a couple of places,
    refactor this into a small helper function that we can make use of. Since
    the non RO prog->aux part is not used in performance critical paths and a
    program destruction via RCU is rather very unlikley when doing the put, we
    shouldn't have an issue just doing the bpf_prog_get() + prog->type != type
    check, but actually not taking the ref at all (due to being in fdget() /
    fdput() section of the bpf fd) is even cleaner and makes the diff smaller
    as well, so just go for that. Callsites are changed to make use of the new
    helper where possible.
    
    RHEL7
    =====
    Omitting network changes. Removing current bpf_prog_get_type stub.
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -220,6 +220,7 @@ void bpf_register_prog_type(struct bpf_prog_type_list *tl);
 void bpf_register_map_type(struct bpf_map_type_list *tl);
 
 struct bpf_prog *bpf_prog_get(u32 ufd);
+struct bpf_prog *bpf_prog_get_type(u32 ufd, enum bpf_prog_type type);
 struct bpf_prog *bpf_prog_inc(struct bpf_prog *prog);
 void bpf_prog_put(struct bpf_prog *prog);
 
@@ -280,13 +281,14 @@ static inline struct bpf_prog *bpf_prog_get(u32 ufd)
 	return ERR_PTR(-EOPNOTSUPP);
 }
 
-static inline void bpf_prog_put(struct bpf_prog *prog)
+static inline struct bpf_prog *bpf_prog_get_type(u32 ufd,
+						 enum bpf_prog_type type)
 {
+	return ERR_PTR(-EOPNOTSUPP);
 }
 
-static inline struct bpf_prog *bpf_prog_get_type(u32 ufd, enum bpf_prog_type type)
+static inline void bpf_prog_put(struct bpf_prog *prog)
 {
-	return NULL;
 }
 
 static inline struct bpf_prog *bpf_prog_add(struct bpf_prog *prog, int i)
