f1da2b8e5d35b6a12bfcd9b360fd08724441b6f9
Jiri Olsa <jolsa@redhat.com>
2018-06-13 21:39:28 -0400

    [kernel] bpf: Add hash of maps support
    Message-id: <20180613214317.28803-158-jolsa@redhat.com>
    Patchwork-id: 221588
    O-Subject: [PATCH RHEL7.6 BZ1311586 157/386] bpf: Add hash of maps support
    Bugzilla: 1311586
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Eugene Syromiatnikov <esyr@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: 1311586
    https://bugzilla.redhat.com/show_bug.cgi?id=1311586
    
    upstream
    ========
    commit bcc6b1b7ebf857a9fe56202e2be3361131588c15
    Author: Martin KaFai Lau <kafai@fb.com>
    Date:   Wed Mar 22 10:00:34 2017 -0700
    
    description
    ===========
    This patch adds hash of maps support (hashmap->bpf_map).
    BPF_MAP_TYPE_HASH_OF_MAPS is added.
    
    A map-in-map contains a pointer to another map and lets call
    this pointer 'inner_map_ptr'.
    
    Notes on deleting inner_map_ptr from a hash map:
    
    1. For BPF_F_NO_PREALLOC map-in-map, when deleting
       an inner_map_ptr, the htab_elem itself will go through
       a rcu grace period and the inner_map_ptr resides
       in the htab_elem.
    
    2. For pre-allocated htab_elem (!BPF_F_NO_PREALLOC),
       when deleting an inner_map_ptr, the htab_elem may
       get reused immediately.  This situation is similar
       to the existing prealloc-ated use cases.
    
       However, the bpf_map_fd_put_ptr() calls bpf_map_put() which calls
       inner_map->ops->map_free(inner_map) which will go
       through a rcu grace period (i.e. all bpf_map's map_free
       currently goes through a rcu grace period).  Hence,
       the inner_map_ptr is still safe for the rcu reader side.
    
    This patch also includes BPF_MAP_TYPE_HASH_OF_MAPS to the
    check_map_prealloc() in the verifier.  preallocation is a
    must for BPF_PROG_TYPE_PERF_EVENT.  Hence, even we don't expect
    heavy updates to map-in-map, enforcing BPF_F_NO_PREALLOC for map-in-map
    is impossible without disallowing BPF_PROG_TYPE_PERF_EVENT from using
    map-in-map first.
    
    RHEL7
    =====
    Droping __ro_after_init, because it's not in RHEL7.
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1197,6 +1197,7 @@ static int check_map_func_compatibility(struct bpf_map *map, int func_id)
 	case BPF_MAP_TYPE_CGROUP_ARRAY:
 		goto error;
 	case BPF_MAP_TYPE_ARRAY_OF_MAPS:
+	case BPF_MAP_TYPE_HASH_OF_MAPS:
 		if (func_id != BPF_FUNC_map_lookup_elem)
 			goto error;
 	default:
@@ -3036,7 +3037,8 @@ process_bpf_exit:
 static int check_map_prealloc(struct bpf_map *map)
 {
 	return (map->map_type != BPF_MAP_TYPE_HASH &&
-		map->map_type != BPF_MAP_TYPE_PERCPU_HASH) ||
+		map->map_type != BPF_MAP_TYPE_PERCPU_HASH &&
+		map->map_type != BPF_MAP_TYPE_HASH_OF_MAPS) ||
 		!(map->map_flags & BPF_F_NO_PREALLOC);
 }
 
