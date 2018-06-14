aac405004f250d3709d15dbd1d98907d50a79938
Jiri Olsa <jolsa@redhat.com>
2018-06-13 21:37:20 -0400

    [kernel] bpf, trace: add BPF_F_CURRENT_CPU flag for bpf_perf_event_output
    Message-id: <20180613214317.28803-30-jolsa@redhat.com>
    Patchwork-id: 221501
    O-Subject: [PATCH RHEL7.6 BZ1311586 029/386] bpf, trace: add BPF_F_CURRENT_CPU flag for bpf_perf_event_output
    Bugzilla: 1311586
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Eugene Syromiatnikov <esyr@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: 1311586
    https://bugzilla.redhat.com/show_bug.cgi?id=1311586
    
    upstream
    ========
    commit 1e33759c788c78f31d4d6f65bac647b23624734c
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date: Mon Apr 18 21:01:23 2016 +0200
    
    description
    ===========
    Add a BPF_F_CURRENT_CPU flag to optimize the use-case where user space has
    per-CPU ring buffers and the eBPF program pushes the data into the current
    CPU's ring buffer which saves us an extra helper function call in eBPF.
    Also, make sure to properly reserve the remaining flags which are not used.
    
    RHEL7
    =====
    Small hunks context conflicts.
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -340,4 +340,8 @@ enum bpf_func_id {
 #define BPF_F_FAST_STACK_CMP		(1ULL << 9)
 #define BPF_F_REUSE_STACKID		(1ULL << 10)
 
+/* BPF_FUNC_perf_event_output flags. */
+#define BPF_F_INDEX_MASK		0xffffffffULL
+#define BPF_F_CURRENT_CPU		BPF_F_INDEX_MASK
+
 #endif /* _UAPI__LINUX_BPF_H__ */
