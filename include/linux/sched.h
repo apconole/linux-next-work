halfline/rhel7

commit a41ccea8d7672606c490feb3a2e2dc7f8aa9977d

[redhat] configs: enable CONFIG_SCHEDSTATS and fix kabi
Message-id: <ed4135f91b4a83e4ede58c58be96ae0dc62d4b54.1466195345.git.jpoimboe@redhat.com>
Patchwork-id: 153604
O-Subject: [RHEL7.3 PATCH v3 11/11] redhat: enable CONFIG_SCHEDSTATS and fix kabi
Bugzilla: 1333444
RH-Acked-by: Rafael Aquini <aquini@redhat.com>
RH-Acked-by: Herton R. Krzesinski <herton@redhat.com>
RH-Acked-by: Xunlei Pang <xpang@redhat.com>

Enable CONFIG_SCHEDSTATS (with the runtime tunable disable by default).

Also fix the following kabi issues:

- Move sched_entity's statistics struct to task_struct and create a
  pointer to it from sched_entity.  This is quite a disruptive change
  because it requires modifying all the various access to se->statistics
  throughout the sched code.  But it's necessary because sched_entity is
  embedded in task_struct and can't change size.

- Extend sched_domain with its new schedstats fields.  This is an
  internal struct which is always allocated within the kernel.

- Extend rq with its new schedstats fields.  This is an internal struct
  which is always allocated within the kernel.

This patch is RHEL-only.

Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
Signed-off-by: Rafael Aquini <aquini@redhat.com>

include/linux/sched.h
@@ -938,7 +938,26 @@ struct sched_domain {
 	u64 max_newidle_lb_cost;
 	unsigned long next_decay_max_lb_cost;
 
-#ifdef CONFIG_SCHEDSTATS
+#ifdef CONFIG_SCHED_DEBUG
+	char *name;
+#endif
+	union {
+		void *private;		/* used during construction */
+		struct rcu_head rcu;	/* used during destruction */
+	};
+
+	unsigned int span_weight;
+	/*
+	 * Span of all CPUs in this domain.
+	 *
+	 * NOTE: this field is variable length. (Allocated dynamically
+	 * by attaching extra space to the end of the structure,
+	 * depending on how many CPUs the kernel has booted up with)
+	 */
+	unsigned long span[0];
+
+#ifndef __GENKSYMS__
+	/* CONFIG_SCHEDSTATS */
 	/* load_balance() stats */
 	unsigned int lb_count[CPU_MAX_IDLE_TYPES];
 	unsigned int lb_failed[CPU_MAX_IDLE_TYPES];
@@ -968,24 +987,7 @@ struct sched_domain {
 	unsigned int ttwu_wake_remote;
 	unsigned int ttwu_move_affine;
 	unsigned int ttwu_move_balance;
-#endif
-#ifdef CONFIG_SCHED_DEBUG
-	char *name;
-#endif
-	union {
-		void *private;		/* used during construction */
-		struct rcu_head rcu;	/* used during destruction */
-	};
-
-	unsigned int span_weight;
-	/*
-	 * Span of all CPUs in this domain.
-	 *
-	 * NOTE: this field is variable length. (Allocated dynamically
-	 * by attaching extra space to the end of the structure,
-	 * depending on how many CPUs the kernel has booted up with)
-	 */
-	unsigned long span[0];
+#endif /* __GENKSYMS__ */
 };
 
 static inline struct cpumask *sched_domain_span(struct sched_domain *sd)
@@ -1132,10 +1134,6 @@ struct sched_entity {
 
 	u64			nr_migrations;
 
-#ifdef CONFIG_SCHEDSTATS
-	struct sched_statistics statistics;
-#endif
-
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	struct sched_entity	*parent;
 	/* rq on which this entity is (to be) queued: */
@@ -1154,8 +1152,9 @@ struct sched_entity {
 	struct sched_avg	avg;
 #endif
 
+	RH_KABI_USE(1, struct sched_statistics *statistics)
+
 	/* reserved for Red Hat */
-	RH_KABI_RESERVE(1)
 	RH_KABI_RESERVE(2)
 	RH_KABI_RESERVE(3)
 	RH_KABI_RESERVE(4)
@@ -1730,6 +1729,7 @@ struct task_struct {
 	#ifdef CONFIG_SMP
 		struct rb_node pushable_dl_tasks;
 	#endif
+	struct sched_statistics statistics;
 #endif /* __GENKSYMS__ */
 };
 

kernel/sched/sched.h
@@ -667,7 +667,16 @@ struct rq {
 	struct hrtimer hrtick_timer;
 #endif
 
-#ifdef CONFIG_SCHEDSTATS
+#ifdef CONFIG_SMP
+	struct llist_head wake_list;
+#endif
+
+	struct sched_avg avg;
+
+	RH_KABI_EXTEND(struct dl_rq dl)
+
+#ifndef __GENKSYMS__
+	/* CONFIG_SCHEDSTATS */
 	/* latency stats */
 	struct sched_info rq_sched_info;
 	unsigned long long rq_cpu_time;
@@ -683,15 +692,7 @@ struct rq {
 	/* try_to_wake_up() stats */
 	unsigned int ttwu_count;
 	unsigned int ttwu_local;
-#endif
-
-#ifdef CONFIG_SMP
-	struct llist_head wake_list;
-#endif
-
-	struct sched_avg avg;
-
-	RH_KABI_EXTEND(struct dl_rq dl)
+#endif /* __GENKSYMS__ */
 };
 
 static inline int cpu_of(struct rq *rq)

kernel/sched/core.c
@@ -1436,11 +1436,11 @@ ttwu_stat(struct task_struct *p, int cpu, int wake_flags)
 
 	if (cpu == this_cpu) {
 		schedstat_inc(rq, ttwu_local);
-		schedstat_inc(p, se.statistics.nr_wakeups_local);
+		schedstat_inc(p, se.statistics->nr_wakeups_local);
 	} else {
 		struct sched_domain *sd;
 
-		schedstat_inc(p, se.statistics.nr_wakeups_remote);
+		schedstat_inc(p, se.statistics->nr_wakeups_remote);
 		rcu_read_lock();
 		for_each_domain(this_cpu, sd) {
 			if (cpumask_test_cpu(cpu, sched_domain_span(sd))) {
@@ -1452,15 +1452,15 @@ ttwu_stat(struct task_struct *p, int cpu, int wake_flags)
 	}
 
 	if (wake_flags & WF_MIGRATED)
-		schedstat_inc(p, se.statistics.nr_wakeups_migrate);
+		schedstat_inc(p, se.statistics->nr_wakeups_migrate);
 
 #endif /* CONFIG_SMP */
 
 	schedstat_inc(rq, ttwu_count);
-	schedstat_inc(p, se.statistics.nr_wakeups);
+	schedstat_inc(p, se.statistics->nr_wakeups);
 
 	if (wake_flags & WF_SYNC)
-		schedstat_inc(p, se.statistics.nr_wakeups_sync);
+		schedstat_inc(p, se.statistics->nr_wakeups_sync);
 
 #endif /* CONFIG_SCHEDSTATS */
 }
@@ -1798,7 +1798,8 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_SCHEDSTATS
 	/* Even if schedstat is disabled, there should not be garbage */
-	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
+	p->se.statistics = &p->statistics;
+	memset(p->se.statistics, 0, sizeof(*p->se.statistics));
 #endif
 
 	RB_CLEAR_NODE(&p->dl.rb_node);
@@ -8265,9 +8266,9 @@ void normalize_rt_tasks(void)
 
 		p->se.exec_start		= 0;
 #ifdef CONFIG_SCHEDSTATS
-		p->se.statistics.wait_start	= 0;
-		p->se.statistics.sleep_start	= 0;
-		p->se.statistics.block_start	= 0;
+		p->se.statistics->wait_start	= 0;
+		p->se.statistics->sleep_start	= 0;
+		p->se.statistics->block_start	= 0;
 #endif
 
 		if (!dl_task(p) && !rt_task(p)) {

kernel/sched/deadline.c
@@ -684,8 +684,8 @@ static void update_curr_dl(struct rq *rq)
 		return;
 	}
 
-	schedstat_set(curr->se.statistics.exec_max,
-		      max(curr->se.statistics.exec_max, delta_exec));
+	schedstat_set(curr->se.statistics->exec_max,
+		      max(curr->se.statistics->exec_max, delta_exec));
 
 	curr->se.sum_exec_runtime += delta_exec;
 	account_group_exec_runtime(curr, delta_exec);

kernel/sched/debug.c
@@ -81,16 +81,16 @@ static void print_cfs_group_stats(struct seq_file *m, int cpu, struct task_group
 	PN(se->sum_exec_runtime);
 #ifdef CONFIG_SCHEDSTATS
 	if (schedstat_enabled()) {
-		PN(se->statistics.wait_start);
-		PN(se->statistics.sleep_start);
-		PN(se->statistics.block_start);
-		PN(se->statistics.sleep_max);
-		PN(se->statistics.block_max);
-		PN(se->statistics.exec_max);
-		PN(se->statistics.slice_max);
-		PN(se->statistics.wait_max);
-		PN(se->statistics.wait_sum);
-		P(se->statistics.wait_count);
+		PN(se->statistics->wait_start);
+		PN(se->statistics->sleep_start);
+		PN(se->statistics->block_start);
+		PN(se->statistics->sleep_max);
+		PN(se->statistics->block_max);
+		PN(se->statistics->exec_max);
+		PN(se->statistics->slice_max);
+		PN(se->statistics->wait_max);
+		PN(se->statistics->wait_sum);
+		P(se->statistics->wait_count);
 	}
 #endif
 	P(se->load.weight);
@@ -133,9 +133,9 @@ print_task(struct seq_file *m, struct rq *rq, struct task_struct *p)
 		p->prio);
 
 	SEQ_printf(m, "%9Ld.%06ld %9Ld.%06ld %9Ld.%06ld",
-		SPLIT_NS(schedstat_val(p, se.statistics.wait_sum)),
+		SPLIT_NS(schedstat_val(p, se.statistics->wait_sum)),
 		SPLIT_NS(p->se.sum_exec_runtime),
-		SPLIT_NS(schedstat_val(p, se.statistics.sum_sleep_runtime)));
+		SPLIT_NS(schedstat_val(p, se.statistics->sum_sleep_runtime)));
 
 #ifdef CONFIG_NUMA_BALANCING
 	SEQ_printf(m, " %d", cpu_to_node(task_cpu(p)));
@@ -579,33 +579,33 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 	if (schedstat_enabled()) {
 		u64 avg_atom, avg_per_cpu;
 
-		PN(se.statistics.sum_sleep_runtime);
-		PN(se.statistics.wait_start);
-		PN(se.statistics.sleep_start);
-		PN(se.statistics.block_start);
-		PN(se.statistics.sleep_max);
-		PN(se.statistics.block_max);
-		PN(se.statistics.exec_max);
-		PN(se.statistics.slice_max);
-		PN(se.statistics.wait_max);
-		PN(se.statistics.wait_sum);
-		P(se.statistics.wait_count);
-		PN(se.statistics.iowait_sum);
-		P(se.statistics.iowait_count);
-		P(se.statistics.nr_migrations_cold);
-		P(se.statistics.nr_failed_migrations_affine);
-		P(se.statistics.nr_failed_migrations_running);
-		P(se.statistics.nr_failed_migrations_hot);
-		P(se.statistics.nr_forced_migrations);
-		P(se.statistics.nr_wakeups);
-		P(se.statistics.nr_wakeups_sync);
-		P(se.statistics.nr_wakeups_migrate);
-		P(se.statistics.nr_wakeups_local);
-		P(se.statistics.nr_wakeups_remote);
-		P(se.statistics.nr_wakeups_affine);
-		P(se.statistics.nr_wakeups_affine_attempts);
-		P(se.statistics.nr_wakeups_passive);
-		P(se.statistics.nr_wakeups_idle);
+		PN(se.statistics->sum_sleep_runtime);
+		PN(se.statistics->wait_start);
+		PN(se.statistics->sleep_start);
+		PN(se.statistics->block_start);
+		PN(se.statistics->sleep_max);
+		PN(se.statistics->block_max);
+		PN(se.statistics->exec_max);
+		PN(se.statistics->slice_max);
+		PN(se.statistics->wait_max);
+		PN(se.statistics->wait_sum);
+		P(se.statistics->wait_count);
+		PN(se.statistics->iowait_sum);
+		P(se.statistics->iowait_count);
+		P(se.statistics->nr_migrations_cold);
+		P(se.statistics->nr_failed_migrations_affine);
+		P(se.statistics->nr_failed_migrations_running);
+		P(se.statistics->nr_failed_migrations_hot);
+		P(se.statistics->nr_forced_migrations);
+		P(se.statistics->nr_wakeups);
+		P(se.statistics->nr_wakeups_sync);
+		P(se.statistics->nr_wakeups_migrate);
+		P(se.statistics->nr_wakeups_local);
+		P(se.statistics->nr_wakeups_remote);
+		P(se.statistics->nr_wakeups_affine);
+		P(se.statistics->nr_wakeups_affine_attempts);
+		P(se.statistics->nr_wakeups_passive);
+		P(se.statistics->nr_wakeups_idle);
 
 		avg_atom = p->se.sum_exec_runtime;
 		if (nr_switches)
@@ -655,6 +655,6 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 void proc_sched_set_task(struct task_struct *p)
 {
 #ifdef CONFIG_SCHEDSTATS
-	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
+	memset(p->se.statistics, 0, sizeof(*p->se.statistics));
 #endif
 }

kernel/sched/fair.c
@@ -702,8 +702,8 @@ static void update_curr(struct cfs_rq *cfs_rq)
 
 	curr->exec_start = now;
 
-	schedstat_set(curr->statistics.exec_max,
-		      max(delta_exec, curr->statistics.exec_max));
+	schedstat_set(curr->statistics->exec_max,
+		      max(delta_exec, curr->statistics->exec_max));
 
 	curr->sum_exec_runtime += delta_exec;
 	schedstat_add(cfs_rq, exec_clock, delta_exec);
@@ -731,22 +731,22 @@ static void update_curr_fair(struct rq *rq)
 static inline void
 update_stats_wait_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
-	schedstat_set(se->statistics.wait_start, rq_clock(rq_of(cfs_rq)));
+	schedstat_set(se->statistics->wait_start, rq_clock(rq_of(cfs_rq)));
 }
 
 static void
 update_stats_wait_end(struct cfs_rq *cfs_rq, struct sched_entity *se)
 {
-	schedstat_set(se->statistics.wait_max, max(se->statistics.wait_max,
-			rq_clock(rq_of(cfs_rq)) - se->statistics.wait_start));
-	schedstat_set(se->statistics.wait_count, se->statistics.wait_count + 1);
-	schedstat_set(se->statistics.wait_sum, se->statistics.wait_sum +
-			rq_clock(rq_of(cfs_rq)) - se->statistics.wait_start);
+	schedstat_set(se->statistics->wait_max, max(se->statistics->wait_max,
+			rq_clock(rq_of(cfs_rq)) - se->statistics->wait_start));
+	schedstat_set(se->statistics->wait_count, se->statistics->wait_count + 1);
+	schedstat_set(se->statistics->wait_sum, se->statistics->wait_sum +
+			rq_clock(rq_of(cfs_rq)) - se->statistics->wait_start);
 	if (entity_is_task(se)) {
 		trace_sched_stat_wait(task_of(se),
-			rq_clock(rq_of(cfs_rq)) - se->statistics.wait_start);
+			rq_clock(rq_of(cfs_rq)) - se->statistics->wait_start);
 	}
-	schedstat_set(se->statistics.wait_start, 0);
+	schedstat_set(se->statistics->wait_start, 0);
 }
 
 /*
@@ -778,9 +778,9 @@ update_stats_dequeue(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 			struct task_struct *tsk = task_of(se);
 
 			if (tsk->state & TASK_INTERRUPTIBLE)
-				se->statistics.sleep_start = rq_clock(rq_of(cfs_rq));
+				se->statistics->sleep_start = rq_clock(rq_of(cfs_rq));
 			if (tsk->state & TASK_UNINTERRUPTIBLE)
-				se->statistics.block_start = rq_clock(rq_of(cfs_rq));
+				se->statistics->block_start = rq_clock(rq_of(cfs_rq));
 		}
 	}
 
@@ -2668,39 +2668,39 @@ static void enqueue_sleeper(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	if (entity_is_task(se))
 		tsk = task_of(se);
 
-	if (se->statistics.sleep_start) {
-		u64 delta = rq_clock(rq_of(cfs_rq)) - se->statistics.sleep_start;
+	if (se->statistics->sleep_start) {
+		u64 delta = rq_clock(rq_of(cfs_rq)) - se->statistics->sleep_start;
 
 		if ((s64)delta < 0)
 			delta = 0;
 
-		if (unlikely(delta > se->statistics.sleep_max))
-			se->statistics.sleep_max = delta;
+		if (unlikely(delta > se->statistics->sleep_max))
+			se->statistics->sleep_max = delta;
 
-		se->statistics.sleep_start = 0;
-		se->statistics.sum_sleep_runtime += delta;
+		se->statistics->sleep_start = 0;
+		se->statistics->sum_sleep_runtime += delta;
 
 		if (tsk) {
 			account_scheduler_latency(tsk, delta >> 10, 1);
 			trace_sched_stat_sleep(tsk, delta);
 		}
 	}
-	if (se->statistics.block_start) {
-		u64 delta = rq_clock(rq_of(cfs_rq)) - se->statistics.block_start;
+	if (se->statistics->block_start) {
+		u64 delta = rq_clock(rq_of(cfs_rq)) - se->statistics->block_start;
 
 		if ((s64)delta < 0)
 			delta = 0;
 
-		if (unlikely(delta > se->statistics.block_max))
-			se->statistics.block_max = delta;
+		if (unlikely(delta > se->statistics->block_max))
+			se->statistics->block_max = delta;
 
-		se->statistics.block_start = 0;
-		se->statistics.sum_sleep_runtime += delta;
+		se->statistics->block_start = 0;
+		se->statistics->sum_sleep_runtime += delta;
 
 		if (tsk) {
 			if (tsk->in_iowait) {
-				se->statistics.iowait_sum += delta;
-				se->statistics.iowait_count++;
+				se->statistics->iowait_sum += delta;
+				se->statistics->iowait_count++;
 				trace_sched_stat_iowait(tsk, delta);
 			}
 
@@ -2973,7 +2973,7 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	 * when there are only lesser-weight tasks around):
 	 */
 	if (schedstat_enabled() && rq_of(cfs_rq)->load.weight >= 2*se->load.weight) {
-		se->statistics.slice_max = max(se->statistics.slice_max,
+		se->statistics->slice_max = max(se->statistics->slice_max,
 			se->sum_exec_runtime - se->prev_sum_exec_runtime);
 	}
 #endif
@@ -4311,7 +4311,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p, int sync)
 	if (sync && balanced)
 		return 1;
 
-	schedstat_inc(p, se.statistics.nr_wakeups_affine_attempts);
+	schedstat_inc(p, se.statistics->nr_wakeups_affine_attempts);
 	tl_per_task = cpu_avg_load_per_task(this_cpu);
 
 	if (balanced ||
@@ -4323,7 +4323,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p, int sync)
 		 * there is no bad imbalance.
 		 */
 		schedstat_inc(sd, ttwu_move_affine);
-		schedstat_inc(p, se.statistics.nr_wakeups_affine);
+		schedstat_inc(p, se.statistics->nr_wakeups_affine);
 
 		return 1;
 	}
@@ -5121,7 +5121,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	if (!cpumask_test_cpu(env->dst_cpu, tsk_cpus_allowed(p))) {
 		int cpu;
 
-		schedstat_inc(p, se.statistics.nr_failed_migrations_affine);
+		schedstat_inc(p, se.statistics->nr_failed_migrations_affine);
 
 		env->flags |= LBF_SOME_PINNED;
 
@@ -5152,7 +5152,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	env->flags &= ~LBF_ALL_PINNED;
 
 	if (task_running(env->src_rq, p)) {
-		schedstat_inc(p, se.statistics.nr_failed_migrations_running);
+		schedstat_inc(p, se.statistics->nr_failed_migrations_running);
 		return 0;
 	}
 
@@ -5170,7 +5170,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 #ifdef CONFIG_SCHEDSTATS
 		if (tsk_cache_hot) {
 			schedstat_inc(env->sd, lb_hot_gained[env->idle]);
-			schedstat_inc(p, se.statistics.nr_forced_migrations);
+			schedstat_inc(p, se.statistics->nr_forced_migrations);
 		}
 #endif
 		return 1;
@@ -5181,13 +5181,13 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 
 		if (tsk_cache_hot) {
 			schedstat_inc(env->sd, lb_hot_gained[env->idle]);
-			schedstat_inc(p, se.statistics.nr_forced_migrations);
+			schedstat_inc(p, se.statistics->nr_forced_migrations);
 		}
 
 		return 1;
 	}
 
-	schedstat_inc(p, se.statistics.nr_failed_migrations_hot);
+	schedstat_inc(p, se.statistics->nr_failed_migrations_hot);
 	return 0;
 }
 

kernel/sched/rt.c
@@ -892,8 +892,8 @@ static void update_curr_rt(struct rq *rq)
 	if (unlikely((s64)delta_exec <= 0))
 		return;
 
-	schedstat_set(curr->se.statistics.exec_max,
-		      max(curr->se.statistics.exec_max, delta_exec));
+	schedstat_set(curr->se.statistics->exec_max,
+		      max(curr->se.statistics->exec_max, delta_exec));
 
 	curr->se.sum_exec_runtime += delta_exec;
 	account_group_exec_runtime(curr, delta_exec);

kernel/sched/stop_task.c
@@ -61,8 +61,8 @@ static void put_prev_task_stop(struct rq *rq, struct task_struct *prev)
 	if (unlikely((s64)delta_exec < 0))
 		delta_exec = 0;
 
-	schedstat_set(curr->se.statistics.exec_max,
-			max(curr->se.statistics.exec_max, delta_exec));
+	schedstat_set(curr->se.statistics->exec_max,
+			max(curr->se.statistics->exec_max, delta_exec));
 
 	curr->se.sum_exec_runtime += delta_exec;
 	account_group_exec_runtime(curr, delta_exec);

apconole/linux-next-work

commit 80dd2948260a07ee1af95c041aae77b8bc5ba551

[kernel] sched/debug: fix schedstats-induced sched domain corruption
Message-id: <2e8c26337bd2d98149db49a11c7d39b745ac7234.1532013674.git.jpoimboe@redhat.com>
Patchwork-id: 225161
O-Subject: [RHEL7.6 PATCH v4] sched/debug: fix schedstats-induced sched domain corruption
Bugzilla: 1602345
RH-Acked-by: Lauro Ramos Venancio <lvenanci@redhat.com>
RH-Acked-by: Waiman Long <longman@redhat.com>
RH-Acked-by: Jiri Benc <jbenc@redhat.com>

Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1602345
Brew:     https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=17235829

Upstream status: RHEL-only, because the original patch was also
RHEL-only due to kABI constraints.

With commit a41ccea8d767 ("[redhat] configs: enable CONFIG_SCHEDSTATS
and fix kabi"), the 'span[0]' variable-length array at the end of
'struct sched_domain' was moved to the middle of the struct.  This
basically unions 'span' with the schedstats.  So if schedstats are
enabled, all scheduler domain CPU spans will get corrupted, causing all
tasks to run on random CPUs.

While this changes the layout of the sched_domain struct, it's not a
kABI concern because this is an internal struct which is dynamically
allocated by the scheduler, and which is used only by the scheduler.
Third party modules should have absolutely zero reason to use this data;
it would be very intrusive for them to do so.

Fixes: a41ccea8d767 ("[redhat] configs: enable CONFIG_SCHEDSTATS and fix kabi")
Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
---
v2:
- Add "RHEL7.6" to patch subject
v3:
- Add upstream status (RHEL-only)
v4:
- Explain why it's not a kABI issue

Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

include/linux/sched.h
@@ -1017,14 +1017,6 @@ struct sched_domain {
 	};
 
 	unsigned int span_weight;
-	/*
-	 * Span of all CPUs in this domain.
-	 *
-	 * NOTE: this field is variable length. (Allocated dynamically
-	 * by attaching extra space to the end of the structure,
-	 * depending on how many CPUs the kernel has booted up with)
-	 */
-	unsigned long span[0];
 
 #ifndef __GENKSYMS__
 	/* CONFIG_SCHEDSTATS */
@@ -1058,6 +1050,15 @@ struct sched_domain {
 	unsigned int ttwu_move_affine;
 	unsigned int ttwu_move_balance;
 #endif /* __GENKSYMS__ */
+
+	/*
+	 * Span of all CPUs in this domain.
+	 *
+	 * NOTE: this field is variable length. (Allocated dynamically
+	 * by attaching extra space to the end of the structure,
+	 * depending on how many CPUs the kernel has booted up with)
+	 */
+	unsigned long span[0];
 };
 
 static inline struct cpumask *sched_domain_span(struct sched_domain *sd)