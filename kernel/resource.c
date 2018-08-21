54afd99c341ed3dbef039dd14b5aa15592624f6d
Gary Hook <ghook@redhat.com>
2018-08-21 16:58:28 -0400

    [kernel] resource: Handle resource flags properly
    Message-id: <1534870733-6662-3-git-send-email-ghook@redhat.com>
    Patchwork-id: 227352
    O-Subject: [RHEL7.6 BZ 1361286 v4 02/27] resource: Handle resource flags properly
    Bugzilla: 1361286
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1361286
    Upstream Status: Upstream kernel version: 4.6-rc1
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=17976373
    Comment: Minor fuzz. No changes, just new position.
    
    commit a3650d53ba16ec412185abb98f231e9ba6bcdc65
    Author: Toshi Kani <toshi.kani@hpe.com>
    Date:   Tue Jan 26 21:57:18 2016 +0100
    
        resource: Handle resource flags properly
    
        I/O resource flags consist of I/O resource types and modifier
        bits. Therefore, checking an I/O resource type in 'flags' must
        be performed with a bitwise operation.
    
        Fix find_next_iomem_res() and region_intersects() that simply
        compare 'flags' against a given value.
    
        Also change __request_region() to set 'res->flags' from
        resource_type() and resource_ext_type() of the parent, so that
        children nodes will inherit the extended I/O resource type.
    
        Signed-off-by: Toshi Kani <toshi.kani@hpe.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Cc: Andrew Morton <akpm@linux-foundation.org>
        Cc: Andy Lutomirski <luto@amacapital.net>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Brian Gerst <brgerst@gmail.com>
        Cc: Dan Williams <dan.j.williams@intel.com>
        Cc: Denys Vlasenko <dvlasenk@redhat.com>
        Cc: H. Peter Anvin <hpa@zytor.com>
        Cc: Jakub Sitnicki <jsitnicki@gmail.com>
        Cc: Jiang Liu <jiang.liu@linux.intel.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Luis R. Rodriguez <mcgrof@suse.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Toshi Kani <toshi.kani@hp.com>
        Cc: Vinod Koul <vinod.koul@intel.com>
        Cc: linux-arch@vger.kernel.org
        Cc: linux-mm <linux-mm@kvack.org>
        Link: http://lkml.kernel.org/r/1453841853-11383-3-git-send-email-bp@alien8.de
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Cc: Gary R Hook <ghook@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- kernel/resource.c
+++ kernel/resource.c
@@ -368,7 +368,7 @@ static int find_next_iomem_res(struct resource *res, char *name,
 	read_lock(&resource_lock);
 
 	for (p = iomem_resource.child; p; p = next_resource(p, sibling_only)) {
-		if (p->flags != res->flags)
+		if ((p->flags & res->flags) != res->flags)
 			continue;
 		if (name && strcmp(p->name, name))
 			continue;
@@ -526,7 +526,8 @@ int region_intersects(resource_size_t start, size_t size, const char *name,
 
 	read_lock(&resource_lock);
 	for (p = iomem_resource.child; p ; p = p->sibling) {
-		bool is_type = strcmp(p->name, name) == 0 && p->flags == flags;
+		bool is_type = strcmp(p->name, name) == 0 &&
+				((p->flags & flags) == flags);
 
 		if (start >= p->start && start <= p->end)
 			is_type ? type++ : other++;
@@ -1144,7 +1145,7 @@ struct resource * __request_region(struct resource *parent,
 	for (;;) {
 		struct resource *conflict;
 
-		res->flags = resource_type(parent);
+		res->flags = resource_type(parent) | resource_ext_type(parent);
 		res->flags |= IORESOURCE_BUSY | flags;
 
 		conflict = __request_resource(parent, res);
