eed6f7e6790ff18d21c19262aca9ed116bbc86af
Gary Hook <ghook@redhat.com>
2018-08-21 16:58:38 -0400

    [x86] mm: Use encrypted access of boot related data with SEV
    Message-id: <1534870733-6662-13-git-send-email-ghook@redhat.com>
    Patchwork-id: 227364
    O-Subject: [RHEL7.6 BZ 1361286 v4 12/27] x86/mm: Use encrypted access of boot related data with SEV
    Bugzilla: 1361286
    RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1361286
    Upstream Status: Upstream kernel version: 4.15-rc1
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=17976373
    Comment: None
    
    commit 072f58c6ce29cf6cf429480fcd1b1e87d1d5ed18
    Author: Tom Lendacky <thomas.lendacky@amd.com>
    Date:   Fri Oct 20 09:30:47 2017 -0500
    
        x86/mm: Use encrypted access of boot related data with SEV
    
        When Secure Encrypted Virtualization (SEV) is active, boot data (such as
        EFI related data, setup data) is encrypted and needs to be accessed as
        such when mapped. Update the architecture override in early_memremap to
        keep the encryption attribute when mapping this data.
    
        Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
        Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Borislav Petkov <bp@suse.de>
        Tested-by: Borislav Petkov <bp@suse.de>
        Cc: Laura Abbott <labbott@redhat.com>
        Cc: kvm@vger.kernel.org
        Cc: Matt Fleming <matt@codeblueprint.co.uk>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Andy Lutomirski <luto@kernel.org>
        Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
        Link: https://lkml.kernel.org/r/20171020143059.3291-6-brijesh.singh@amd.com
    
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Suravee Suthikulpanit <ssuthiku@redhat.com>
    Cc: Gary R Hook <ghook@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/x86/mm/ioremap.c
+++ arch/x86/mm/ioremap.c
@@ -366,6 +366,9 @@ void unxlate_dev_mem_ptr(unsigned long phys, void *addr)
  * areas should be mapped decrypted. And since the encryption key can
  * change across reboots, persistent memory should also be mapped
  * decrypted.
+ *
+ * If SEV is active, that implies that BIOS/UEFI also ran encrypted so
+ * only persistent memory should be mapped decrypted.
  */
 static bool memremap_should_map_decrypted(resource_size_t phys_addr,
 					  unsigned long size)
@@ -403,6 +406,11 @@ static bool memremap_should_map_decrypted(resource_size_t phys_addr,
 	case E820_ACPI:
 	case E820_NVS:
 	case E820_UNUSABLE:
+		/* For SEV, these areas are encrypted */
+		if (sev_active())
+			break;
+		/* Fallthrough */
+
 	case E820_PRAM:
 		return true;
 	default:
@@ -526,7 +534,7 @@ static bool __init early_memremap_is_setup_data(resource_size_t phys_addr,
 bool arch_memremap_can_ram_remap(resource_size_t phys_addr, unsigned long size,
 				 unsigned long flags)
 {
-	if (!sme_active())
+	if (!mem_encrypt_active())
 		return true;
 
 	if (flags & MEMREMAP_ENC)
@@ -535,12 +543,13 @@ bool arch_memremap_can_ram_remap(resource_size_t phys_addr, unsigned long size,
 	if (flags & MEMREMAP_DEC)
 		return false;
 
-	if (memremap_is_setup_data(phys_addr, size) ||
-	    memremap_is_efi_data(phys_addr, size) ||
-	    memremap_should_map_decrypted(phys_addr, size))
-		return false;
+	if (sme_active()) {
+		if (memremap_is_setup_data(phys_addr, size) ||
+		    memremap_is_efi_data(phys_addr, size))
+			return false;
+	}
 
-	return true;
+	return !memremap_should_map_decrypted(phys_addr, size);
 }
 
 /*
@@ -553,17 +562,24 @@ pgprot_t __init early_memremap_pgprot_adjust(resource_size_t phys_addr,
 					     unsigned long size,
 					     pgprot_t prot)
 {
-	if (!sme_active())
+	bool encrypted_prot;
+
+	if (!mem_encrypt_active())
 		return prot;
 
-	if (early_memremap_is_setup_data(phys_addr, size) ||
-	    memremap_is_efi_data(phys_addr, size) ||
-	    memremap_should_map_decrypted(phys_addr, size))
-		prot = pgprot_decrypted(prot);
-	else
-		prot = pgprot_encrypted(prot);
+	encrypted_prot = true;
 
-	return prot;
+	if (sme_active()) {
+		if (early_memremap_is_setup_data(phys_addr, size) ||
+		    memremap_is_efi_data(phys_addr, size))
+			encrypted_prot = false;
+	}
+
+	if (encrypted_prot && memremap_should_map_decrypted(phys_addr, size))
+		encrypted_prot = false;
+
+	return encrypted_prot ? pgprot_encrypted(prot)
+			      : pgprot_decrypted(prot);
 }
 
 bool phys_mem_access_encrypted(unsigned long phys_addr, unsigned long size)
