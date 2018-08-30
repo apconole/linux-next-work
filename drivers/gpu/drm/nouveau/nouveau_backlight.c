ea1952727a0b02ef2199dcfe086c086a5087f7fc
Rob Clark <rclark@redhat.com>
2018-08-30 18:15:50 -0400

    [gpu] drm/nouveau: Use drm_connector_list_iter_* for iterating connectors
    Message-id: <20180830181612.21286-6-rclark@redhat.com>
    Patchwork-id: 227744
    O-Subject: [RHEL7 drm 05/27] drm/nouveau: Use drm_connector_list_iter_* for iterating connectors
    Bugzilla: 1600569
    RH-Acked-by: Lyude Paul <lyude@redhat.com>
    RH-Acked-by: Dave Airlie <airlied@redhat.com>
    RH-Acked-by: Jerome Glisse <jglisse@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1600569
    Upstream: since v4.17.10
    
    commit c89b8c6f8dd304feb29e4f6f17efa50951f87971
    
    Author:     Lyude Paul <lyude@redhat.com>
    AuthorDate: Fri Jul 13 13:06:32 2018 -0400
    Commit:     Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CommitDate: Wed Jul 25 11:26:09 2018 +0200
    
        drm/nouveau: Use drm_connector_list_iter_* for iterating connectors
    
        commit 22b76bbe089cd901f5260ecb9a3dc41f9edb97a0 upstream.
    
        Every codepath in nouveau that loops through the connector list
        currently does so using the old method, which is prone to race
        conditions from MST connectors being created and destroyed. This has
        been causing a multitude of problems, including memory corruption from
        trying to access connectors that have already been freed!
    
        Signed-off-by: Lyude Paul <lyude@redhat.com>
        Cc: stable@vger.kernel.org
        Cc: Karol Herbst <karolherbst@gmail.com>
        Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Rob Clark <rclark@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_backlight.c
+++ drivers/gpu/drm/nouveau/nouveau_backlight.c
@@ -266,10 +266,12 @@ nouveau_backlight_init(struct drm_device *dev)
 	struct nouveau_drm *drm = nouveau_drm(dev);
 	struct nvif_device *device = &drm->client.device;
 	struct drm_connector *connector;
+	struct drm_connector_list_iter conn_iter;
 
 	INIT_LIST_HEAD(&drm->bl_connectors);
 
-	list_for_each_entry(connector, &dev->mode_config.connector_list, head) {
+	drm_connector_list_iter_begin(dev, &conn_iter);
+	drm_for_each_connector_iter(connector, &conn_iter) {
 		if (connector->connector_type != DRM_MODE_CONNECTOR_LVDS &&
 		    connector->connector_type != DRM_MODE_CONNECTOR_eDP)
 			continue;
@@ -286,7 +288,7 @@ nouveau_backlight_init(struct drm_device *dev)
 			break;
 		}
 	}
-
+	drm_connector_list_iter_end(&conn_iter);
 
 	return 0;
 }
