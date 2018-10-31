ed7a867d01771577bc4d9d422815df9eaa07720c
Waiman Long <longman@redhat.com>
2018-10-31 15:47:18 -0400

    [kernel] locking/lockdep: Fix debug_locks off performance problem
    Message-id: <1541000838-28223-4-git-send-email-longman@redhat.com>
    Patchwork-id: 232136
    O-Subject: [RHEL7.7 PATCH 3/3] locking/lockdep: Fix debug_locks off performance problem
    Bugzilla: 1451547
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Christoph von Recklinghausen <crecklin@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1451547
    Conflicts: There are a minor fuzz that is easily resolved.
    
    commit 9506a7425b094d2f1d9c877ed5a78f416669269b
    Author: Waiman Long <longman@redhat.com>
    Date:   Thu Oct 18 21:45:17 2018 -0400
    
        locking/lockdep: Fix debug_locks off performance problem
    
        It was found that when debug_locks was turned off because of a problem
        found by the lockdep code, the system performance could drop quite
        significantly when the lock_stat code was also configured into the
        kernel. For instance, parallel kernel build time on a 4-socket x86-64
        server nearly doubled.
    
        Further analysis into the cause of the slowdown traced back to the
        frequent call to debug_locks_off() from the __lock_acquired() function
        probably due to some inconsistent lockdep states with debug_locks
        off. The debug_locks_off() function did an unconditional atomic xchg
        to write a 0 value into debug_locks which had already been set to 0.
        This led to severe cacheline contention in the cacheline that held
        debug_locks.  As debug_locks is being referenced in quite a few different
        places in the kernel, this greatly slow down the system performance.
    
        To prevent that trashing of debug_locks cacheline, lock_acquired()
        and lock_contended() now checks the state of debug_locks before
        proceeding. The debug_locks_off() function is also modified to check
        debug_locks before calling __debug_locks_off().
    
        Signed-off-by: Waiman Long <longman@redhat.com>
        Cc: Andrew Morton <akpm@linux-foundation.org>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Will Deacon <will.deacon@arm.com>
        Link: http://lkml.kernel.org/r/1539913518-15598-1-git-send-email-longman@redhat.com
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -3822,7 +3822,7 @@ void lock_contended(struct lockdep_map *lock, unsigned long ip)
 {
 	unsigned long flags;
 
-	if (unlikely(!lock_stat))
+	if (unlikely(!lock_stat || !debug_locks))
 		return;
 
 	if (unlikely(current->lockdep_recursion))
@@ -3842,7 +3842,7 @@ void lock_acquired(struct lockdep_map *lock, unsigned long ip)
 {
 	unsigned long flags;
 
-	if (unlikely(!lock_stat))
+	if (unlikely(!lock_stat || !debug_locks))
 		return;
 
 	if (unlikely(current->lockdep_recursion))
