f4d4cc879c22d5ba155e70e7adc442e61a2a0d7e
David Arcari <darcari@redhat.com>
2018-11-08 13:27:37 -0500

    [mfd] intel-lpss: Pass I2C configuration via properties on BXT
    Message-id: <1541683661-82408-3-git-send-email-darcari@redhat.com>
    Patchwork-id: 232642
    O-Subject: [RHEL7.7 BZ 1643267 2/6] mfd: intel-lpss: Pass I2C configuration via properties on BXT
    Bugzilla: 1643267
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1643267
    Conflicts: RHEL uses device_add_properties as upstream commit f4d052660323
    	   has been applied
    
    commit 0343b2f4e4a52c907d7676ce3159e0b5e7f0301c
    Author: Mika Westerberg <mika.westerberg@linux.intel.com>
    Date:   Tue Jan 26 14:17:49 2016 +0200
    
        mfd: intel-lpss: Pass I2C configuration via properties on BXT
    
        I2C host controller need to be configured properly in order to meet I2C
        timings specified in the I2C protocol specification. Some Intel Broxton
        based machines do not have this information in the ACPI namespace (or the
        boot firmware does not support ACPI at all) so we use build-in device
        properties instead.
    
        Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
        Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
        Signed-off-by: Lee Jones <lee.jones@linaro.org>
    
    Cc: David Arcari <darcari@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/mfd/intel-lpss-pci.c
+++ drivers/mfd/intel-lpss-pci.c
@@ -98,8 +98,16 @@ static const struct intel_lpss_platform_info bxt_uart_info = {
 	.clk_con_id = "baudclk",
 };
 
+static struct property_entry bxt_i2c_properties[] = {
+	PROPERTY_ENTRY_U32("i2c-sda-hold-time-ns", 42),
+	PROPERTY_ENTRY_U32("i2c-sda-falling-time-ns", 171),
+	PROPERTY_ENTRY_U32("i2c-scl-falling-time-ns", 208),
+	{ },
+};
+
 static const struct intel_lpss_platform_info bxt_i2c_info = {
 	.clk_rate = 133000000,
+	.properties = bxt_i2c_properties,
 };
 
 static const struct pci_device_id intel_lpss_pci_ids[] = {
