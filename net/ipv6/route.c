1aec5367364a9becd6417407b4c65b7f07d477b3
Sabrina Dubroca <sdubroca@redhat.com>
2018-11-15 16:29:17 -0500

    [net] ipv6: rate-limit probes for neighbourless routes
    Message-id: <ae78c4e258d6fa910fd435400cf97dcd66644a25.1542298303.git.sdubroca@redhat.com>
    Patchwork-id: 233094
    O-Subject: [PATCH RHEL7.7 net 2/2] ipv6: rate-limit probes for neighbourless routes
    Bugzilla: 1637821
    RH-Acked-by: Xin Long <lxin@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1637821
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=19153302
    Tested: setup with 2 default routes, one without neigh entry. before
    the patch, one NS is sent for each route lookup. after the patch, NS
    are properly rate-limited.
    
    Conflicts: context in rt6_probe(), missing commits
     5e670d844b2a ("net/ipv6: Move nexthop data to fib6_nh")
    and
     dcd1f572954f ("net/ipv6: Remove fib6_idev").
    Because of 5e670d844b2a, we use rt->dst.dev instead of local dev.
    Also use an rh_reserved field to store last_probe in rt6_info.
    
    commit f547fac624be53ad8b07e9ebca7654a7827ba61b
    Author: Sabrina Dubroca <sd@queasysnail.net>
    Date:   Fri Oct 12 16:22:47 2018 +0200
    
        ipv6: rate-limit probes for neighbourless routes
    
        When commit 270972554c91 ("[IPV6]: ROUTE: Add Router Reachability
        Probing (RFC4191).") introduced router probing, the rt6_probe() function
        required that a neighbour entry existed. This neighbour entry is used to
        record the timestamp of the last probe via the ->updated field.
    
        Later, commit 2152caea7196 ("ipv6: Do not depend on rt->n in rt6_probe().")
        removed the requirement for a neighbour entry. Neighbourless routes skip
        the interval check and are not rate-limited.
    
        This patch adds rate-limiting for neighbourless routes, by recording the
        timestamp of the last probe in the fib6_info itself.
    
        Fixes: 2152caea7196 ("ipv6: Do not depend on rt->n in rt6_probe().")
        Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
        Reviewed-by: Stefano Brivio <sbrivio@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -552,8 +552,10 @@ static void rt6_probe_deferred(struct work_struct *w)
 
 static void rt6_probe(struct rt6_info *rt)
 {
-	struct __rt6_probe_work *work;
+	struct __rt6_probe_work *work = NULL;
 	struct neighbour *neigh;
+	struct inet6_dev *idev;
+
 	/*
 	 * Okay, this does not seem to be appropriate
 	 * for now, however, we need to check if it
@@ -565,24 +567,25 @@ static void rt6_probe(struct rt6_info *rt)
 	if (!rt || !(rt->rt6i_flags & RTF_GATEWAY))
 		return;
 	rcu_read_lock_bh();
+	idev = __in6_dev_get(rt->dst.dev);
 	neigh = __ipv6_neigh_lookup_noref(rt->dst.dev, &rt->rt6i_gateway);
 	if (neigh) {
-		work = NULL;
 		write_lock(&neigh->lock);
 		if (!(neigh->nud_state & NUD_VALID) &&
 		    time_after(jiffies,
-			       neigh->updated +
-			       rt->rt6i_idev->cnf.rtr_probe_interval)) {
+			       neigh->updated + idev->cnf.rtr_probe_interval)) {
 			work = kmalloc(sizeof(*work), GFP_ATOMIC);
 			if (work)
 				__neigh_set_probe_once(neigh);
 		}
 		write_unlock(&neigh->lock);
-	} else {
+	} else if (time_after(jiffies, rt->last_probe +
+				       idev->cnf.rtr_probe_interval)) {
 		work = kmalloc(sizeof(*work), GFP_ATOMIC);
 	}
 
 	if (work) {
+		rt->last_probe = jiffies;
 		INIT_WORK(&work->work, rt6_probe_deferred);
 		work->target = rt->rt6i_gateway;
 		dev_hold(rt->dst.dev);
