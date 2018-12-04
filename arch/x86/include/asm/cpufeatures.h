47ac2523bed57f6b27a14d14102e6ebdeba08d05
Steve Best <sbest@redhat.com>
2018-12-04 15:17:34 -0500

    [x86] cpufeatures: Enumerate MOVDIR64B instruction
    Message-id: <20181204151703.21627.96733.sendpatchset@ibm-p7r2-01.lab.bos.redhat.com>
    Patchwork-id: 234407
    O-Subject: [PATCH RHEL7.7 BZ1482755 2/2] x86/cpufeatures: Enumerate MOVDIR64B instruction
    Bugzilla: 1482755
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1482755
    
    Description:
    ------------
    MOVDIR64B moves 64-bytes as direct-store with 64-bytes write atomicity.
    Direct store is implemented by using write combining (WC) for writing
    data directly into memory without caching the data.
    
    In low latency offload (e.g. Non-Volatile Memory, etc), MOVDIR64B writes
    work descriptors (and data in some cases) to device-hosted work-queues
    atomically without cache pollution.
    
    Availability of the MOVDIR64B instruction is indicated by the
    presence of the CPUID feature flag MOVDIR64B (CPUID.0x07.0x0:ECX[bit 28]).
    
    Please check the latest Intel Architecture Instruction Set Extensions
    and Future Features Programming Reference for more details on the CPUID
    feature MOVDIR64B flag.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Ashok Raj <ashok.raj@intel.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ravi V Shankar <ravi.v.shankar@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1540418237-125817-3-git-send-email-fenghua.yu@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    Upstream Status:
    ----------------
    commit ace6485a03266cc3c198ce8e927a1ce0ce139699
    
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ace6485a03266cc3c198ce8e927a1ce0ce139699
    
    ----------------
    Steve Best
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -324,6 +324,7 @@
 #define X86_FEATURE_AVX512_BITALG (16*32+12) /* Support for VPOPCNT[B,W] and VPSHUF-BITQMB */
 #define X86_FEATURE_AVX512_VPOPCNTDQ (16*32+14) /* POPCNT for vectors of DW/QW */
 #define X86_FEATURE_MOVDIRI		(16*32+27) /* MOVDIRI instruction */
+#define X86_FEATURE_MOVDIR64B		(16*32+28) /* MOVDIR64B instruction */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (EDX), word 18 */
 #define X86_FEATURE_AVX512_4VNNIW	(18*32+ 2) /* AVX-512 Neural Network Instructions */
