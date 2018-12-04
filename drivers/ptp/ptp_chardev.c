0abab0d2a54beb718c471dd9ec4488762b526a77
Hangbin Liu <haliu@redhat.com>
2018-12-04 07:52:49 -0500

    [net] ptp: reorder declarations in ptp_ioctl()
    Message-id: <1543909979-8207-2-git-send-email-haliu@redhat.com>
    Patchwork-id: 234361
    O-Subject: [RHEL7.7 net PATCH 01/11] ptp: reorder declarations in ptp_ioctl()
    Bugzilla: 1643974
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1643974
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=19358015
    Upstream Status: net-next.git commit fbb960ac2617
    
    Conflicts: content conflicts due to missing commit ed7c6317bc599 ("ptp: remove
    32 bit get/set methods.").
    
    commit fbb960ac26172215b8dc6635eac13c3a0739d6ed
    Author: Miroslav Lichvar <mlichvar@redhat.com>
    Date:   Fri Nov 9 11:14:42 2018 +0100
    
        ptp: reorder declarations in ptp_ioctl()
    
        Reorder declarations of variables as reversed Christmas tree.
    
        Cc: Richard Cochran <richardcochran@gmail.com>
        Suggested-by: Richard Cochran <richardcochran@gmail.com>
        Signed-off-by: Miroslav Lichvar <mlichvar@redhat.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/ptp/ptp_chardev.c
+++ drivers/ptp/ptp_chardev.c
@@ -123,19 +123,19 @@ int ptp_open(struct posix_clock *pc, fmode_t fmode)
 
 long ptp_ioctl(struct posix_clock *pc, unsigned int cmd, unsigned long arg)
 {
-	struct ptp_clock_caps caps;
-	struct ptp_clock_request req;
-	struct ptp_sys_offset *sysoff = NULL;
-	struct ptp_sys_offset_precise precise_offset;
-	struct ptp_pin_desc pd;
 	struct ptp_clock *ptp = container_of(pc, struct ptp_clock, clock);
-	struct ptp_clock_info *ops = ptp->info;
-	struct ptp_clock_time *pct;
-	struct timespec64 ts;
+	struct ptp_sys_offset_precise precise_offset;
 	struct system_device_crosststamp xtstamp;
+	struct ptp_clock_info *ops = ptp->info;
+	struct ptp_sys_offset *sysoff = NULL;
+	struct ptp_clock_request req;
+	struct ptp_clock_caps caps;
+	struct ptp_clock_time *pct;
+	unsigned int i, pin_index;
+	struct ptp_pin_desc pd;
+	struct timespec64 ts;
 	struct timespec t2;
 	int enable, err = 0;
-	unsigned int i, pin_index;
 
 	switch (cmd) {
 
