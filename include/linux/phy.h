9dac71c9e3e1ac2d7f4a89c500077fd54ddaaf65
Ivan Vecera <ivecera@redhat.com>
2018-12-06 18:49:37 -0500

    [netdrv] phy: make phy_(read|write)_mmd() generic MMD accessors
    Message-id: <20181206184956.14150-37-ivecera@redhat.com>
    Patchwork-id: 234666
    O-Subject: [RHEL7.7 PATCH 36/55] net: phy: make phy_(read|write)_mmd() generic MMD accessors
    Bugzilla: 1655590
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1655590
    
    Upstream commit(s):
    commit 1ee6b9bc6206cd0837bc16e46f580e40fe663384
    Author: Russell King <rmk+kernel@armlinux.org.uk>
    Date:   Tue Mar 21 16:36:43 2017 +0000
    
        net: phy: make phy_(read|write)_mmd() generic MMD accessors
    
        Make phy_(read|write)_mmd() generic 802.3 clause 45 register accessors
        for both Clause 22 and Clause 45 PHYs, using either the direct register
        reading for Clause 45, or the indirect method for Clause 22 PHYs.
        Allow this behaviour to be overriden by PHY drivers where necessary.
    
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -536,6 +536,31 @@ struct phy_driver {
 	/* Determines the auto negotiation result */
 	RH_KABI_EXTEND(int (*aneg_done)(struct phy_device *phydev))
 
+	/*
+	 * Phy specific driver override for reading a MMD register.
+	 * This function is optional for PHY specific drivers.  When
+	 * not provided, the default MMD read function will be used
+	 * by phy_read_mmd(), which will use either a direct read for
+	 * Clause 45 PHYs or an indirect read for Clause 22 PHYs.
+	 *  devnum is the MMD device number within the PHY device,
+	 *  regnum is the register within the selected MMD device.
+	 */
+	RH_KABI_EXTEND(int (*read_mmd)(struct phy_device *dev, int devnum,
+				       u16 regnum))
+
+	/*
+	 * Phy specific driver override for writing a MMD register.
+	 * This function is optional for PHY specific drivers.  When
+	 * not provided, the default MMD write function will be used
+	 * by phy_write_mmd(), which will use either a direct write for
+	 * Clause 45 PHYs, or an indirect write for Clause 22 PHYs.
+	 *  devnum is the MMD device number within the PHY device,
+	 *  regnum is the register within the selected MMD device.
+	 *  val is the value to be written.
+	 */
+	RH_KABI_EXTEND(int (*write_mmd)(struct phy_device *dev, int devnum,
+					u16 regnum, u16 val))
+
 	/* A function provided by a phy specific driver to override the
 	 * the PHY driver framework support for reading a MMD register
 	 * from the PHY. If not supported, return -1. This function is
