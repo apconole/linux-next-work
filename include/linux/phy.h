4f519b68a2a8699205fc1a2027bcfa6400c60b5a
Ivan Vecera <ivecera@redhat.com>
2018-12-06 18:49:36 -0500

    [netdrv] phy: move phy MMD accessors to phy-core.c
    Message-id: <20181206184956.14150-36-ivecera@redhat.com>
    Patchwork-id: 234668
    O-Subject: [RHEL7.7 PATCH 35/55] net: phy: move phy MMD accessors to phy-core.c
    Bugzilla: 1655590
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1655590
    
    Upstream commit(s):
    commit 9860118b58241169f67ba77dfeb935fcf53ce4cd
    Author: Russell King <rmk+kernel@armlinux.org.uk>
    Date:   Tue Mar 21 16:36:37 2017 +0000
    
        net: phy: move phy MMD accessors to phy-core.c
    
        Move the phy_(read|write)__mmd() helpers out of line, they will become
        our main MMD accessor functions, and so will be a little more complex.
        This complexity doesn't belong in an inline function.  Also move the
        _indirect variants as well to keep like functionality together.
    
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -583,14 +583,7 @@ struct phy_fixup {
  *
  * Same rules as for phy_read();
  */
-static inline int phy_read_mmd(struct phy_device *phydev, int devad, u32 regnum)
-{
-	if (!phydev->is_c45)
-		return -EOPNOTSUPP;
-
-	return mdiobus_read(phydev->mdio_bus, phydev->mdio_addr,
-			    MII_ADDR_C45 | (devad << 16) | (regnum & 0xffff));
-}
+int phy_read_mmd(struct phy_device *phydev, int devad, u32 regnum);
 
 /**
  * phy_read_mmd_indirect - reads data from the MMD registers
@@ -663,16 +656,7 @@ static inline bool phy_is_internal(struct phy_device *phydev)
  *
  * Same rules as for phy_write();
  */
-static inline int phy_write_mmd(struct phy_device *phydev, int devad,
-				u32 regnum, u16 val)
-{
-	if (!phydev->is_c45)
-		return -EOPNOTSUPP;
-
-	regnum = MII_ADDR_C45 | ((devad & 0x1f) << 16) | (regnum & 0xffff);
-
-	return mdiobus_write(phydev->mdio_bus, phydev->mdio_addr, regnum, val);
-}
+int phy_write_mmd(struct phy_device *phydev, int devad, u32 regnum, u16 val);
 
 /**
  * phy_write_mmd_indirect - writes data to the MMD registers
