96072b168363b827bbf5c6768d0840b4f6f020e3
Petr Oros <poros@redhat.com>
2018-12-17 10:13:36 -0500

    [net] devlink: Add param set command
    Message-id: <c678b113081ec604ddc8d75bd3e070b8eebb0dff.1545039206.git.poros@redhat.com>
    Patchwork-id: 235897
    O-Subject: [RHEL7.7 net PATCH 03/23] devlink: Add param set command
    Bugzilla: 1653433
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1653433
    
    Backport note: Adjusted due to missing netlink_ext_ack support
    
    Upstream commit(s):
    commit e3b7ca18ad7b2f47ebd3b6e6ce58a42c6ec24746
    Author: Moshe Shemesh <moshe@mellanox.com>
    Date:   Wed Jul 4 14:30:30 2018 +0300
    
        devlink: Add param set command
    
        Add param set command to set value for a parameter.
        Value can be set to any of the supported configuration modes.
    
        Signed-off-by: Moshe Shemesh <moshe@mellanox.com>
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -328,6 +328,7 @@ struct devlink_param_gset_ctx {
  *       configuration modes
  * @set: set parameter value, used for runtime and permanent
  *       configuration modes
+ * @validate: validate input value is applicable (within value range, etc.)
  *
  * This struct should be used by the driver to fill the data for
  * a parameter it registers.
@@ -342,6 +343,8 @@ struct devlink_param {
 		   struct devlink_param_gset_ctx *ctx);
 	int (*set)(struct devlink *devlink, u32 id,
 		   struct devlink_param_gset_ctx *ctx);
+	int (*validate)(struct devlink *devlink, u32 id,
+			union devlink_param_value val);
 };
 
 struct devlink_param_item {
