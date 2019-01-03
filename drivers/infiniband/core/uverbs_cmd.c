52854a2f2eecb586200b29b10da6a1de03c35e6f
Kamal Heib <kheib@redhat.com>
2019-01-03 09:23:35 -0500

    [infiniband] rdma/uverbs: Don't overwrite NULL pointer with ZERO_SIZE_PTR
    Message-id: <20190103092638.12749-51-kheib@redhat.com>
    Patchwork-id: 236473
    O-Subject: [PATCH RHEL-7.7 050/233] RDMA/uverbs: Don't overwrite NULL pointer with ZERO_SIZE_PTR
    Bugzilla: 1655920
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Honggang Li <honli@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1655920
    
    commit a5cc9831af05e658543593abaee45a29d061bac4
    Author: Leon Romanovsky <leonro@mellanox.com>
    Date:   Sun Jun 24 11:23:47 2018 +0300
    
        RDMA/uverbs: Don't overwrite NULL pointer with ZERO_SIZE_PTR
    
        Number of specs is provided by user and in valid case can be equal to zero.
        Such argument causes to call to kcalloc() with zero-length request and in
        return the ZERO_SIZE_PTR is assigned. This pointer is different from NULL
        and makes various if (..) checks to success.
    
        Fixes: b6ba4a9aa59f ("IB/uverbs: Add support for flow counters")
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
        Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -2803,6 +2803,9 @@ static struct ib_uflow_resources *flow_resources_alloc(size_t num_specs)
 	if (!resources)
 		return NULL;
 
+	if (!num_specs)
+		goto out;
+
 	resources->counters =
 		kcalloc(num_specs, sizeof(*resources->counters), GFP_KERNEL);
 	resources->collection =
@@ -2811,8 +2814,8 @@ static struct ib_uflow_resources *flow_resources_alloc(size_t num_specs)
 	if (!resources->counters || !resources->collection)
 		goto err;
 
+out:
 	resources->max = num_specs;
-
 	return resources;
 
 err:
