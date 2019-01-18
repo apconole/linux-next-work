891e3e35f02c37875255a8b140030abc724fc6ee
Paolo Abeni <pabeni@redhat.com>
2019-01-18 14:01:08 -0500

    [net] sysfs: Fix memory leak in XPS configuration
    Message-id: <a17d2e9d4a3a43b26d4d6ada025ddec2cb9b5731.1547816644.git.pabeni@redhat.com>
    Patchwork-id: 237670
    O-Subject: [RHEL7.7 net 5/5] net-sysfs: Fix memory leak in XPS configuration
    Bugzilla: 1623813
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1623813
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=19873196
    Tested: LNST, Tier1 and bz repro
    
    Upstream commit:
    commit 664088f8d68178809b848ca450f2797efb34e8e7
    Author: Alexander Duyck <alexander.h.duyck@intel.com>
    Date:   Thu May 31 15:59:46 2018 -0400
    
        net-sysfs: Fix memory leak in XPS configuration
    
        This patch reorders the error cases in showing the XPS configuration so
        that we hold off on memory allocation until after we have verified that we
        can support XPS on a given ring.
    
        Fixes: 184c449f91fe ("net: Add support for XPS with QoS via traffic classes")
        Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Paolo Abeni <Pabeni@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -1177,9 +1177,6 @@ static ssize_t show_xps_map(struct netdev_queue *queue,
 	cpumask_var_t mask;
 	unsigned long index;
 
-	if (!zalloc_cpumask_var(&mask, GFP_KERNEL))
-		return -ENOMEM;
-
 	index = get_netdev_queue_index(queue);
 
 	if (dev->num_tc) {
@@ -1188,6 +1185,9 @@ static ssize_t show_xps_map(struct netdev_queue *queue,
 		if (tc < 0)
 			return -EINVAL;
 	}
+
+	if (!zalloc_cpumask_var(&mask, GFP_KERNEL))
+		return -ENOMEM;
 
 	rcu_read_lock();
 	dev_maps = rcu_dereference(dev->xps_maps);
