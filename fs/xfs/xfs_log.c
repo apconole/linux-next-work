78977ec041a01d63f95e48091b171d391a19cd04
Brian Foster <bfoster@redhat.com>
2019-01-21 16:29:52 -0500

    [fs] xfs: Fix bool initialization/comparison
    Message-id: <20190121163147.19377-49-bfoster@redhat.com>
    Patchwork-id: 237773
    O-Subject: [RHEL7.7 v2 PATCH 048/163] xfs: Fix bool initialization/comparison
    Bugzilla: 1613405
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    RH-Acked-by: Bill O'Donnell <billodo@redhat.com>
    
    commit 749f24f33e87c1706d716c283027595b72a034f3
    Author: Thomas Meyer <thomas@m3y3r.de>
    Date:   Mon Oct 9 11:38:54 2017 -0700
    
        xfs: Fix bool initialization/comparison
    
        Bool initializations should use true and false. Bool tests don't need
        comparisons.
    
        Signed-off-by: Thomas Meyer <thomas@m3y3r.de>
        Reviewed-by: Brian Foster <bfoster@redhat.com>
        Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
        Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/xfs/xfs_log.c
+++ fs/xfs/xfs_log.c
@@ -2506,7 +2506,7 @@ next_lv:
 				if (lv)
 					vecp = lv->lv_iovecp;
 			}
-			if (record_cnt == 0 && ordered == false) {
+			if (record_cnt == 0 && !ordered) {
 				if (!lv)
 					return 0;
 				break;
