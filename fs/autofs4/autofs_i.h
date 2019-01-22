c1f5319c23ca0be2668a8638f614b70138949b8a
Ian Kent <ikent@redhat.com>
2019-01-22 10:12:37 -0500

    [fs] autofs: make expire flags usage consistent with v5 params
    Message-id: <154815195667.28923.10229814860989538228.stgit@pluto-themaw-net>
    Patchwork-id: 237935
    O-Subject: [RHEL 7.7 PATCH 5/9] autofs: make expire flags usage consistent with v5 params
    Bugzilla: 1653208
    RH-Acked-by: Steve Dickson <SteveD@redhat.com>
    RH-Acked-by: J. Bruce Fields <bfields@redhat.com>
    RH-Acked-by: Scott Mayhew <smayhew@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1653208
    commit e5c85e1fe19c03777cbacf4b5a5167b2f5ff90fb
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=19887902
    Testing: installed updated kernel, checked autofs functioned as normal,
    	 installed updated autofs user space and checked expire function
    	 worked as expected with this patch series.
    Comments: This patch is needed becuase the automount daemon uses the
    	  module subversion to check if functionality is present in the
    	  kernel and the first six patches of this series results in an
    	  increase of the subversion. Consequently they must be included
    	  along with the three strictexpire option patches.
    
    Make the usage of the expire flags consistent by naming the expire flags
    the same as it is named in the version 5 miscelaneous ioctl parameters and
    only check the bit flags when needed.
    
    Link: http://lkml.kernel.org/r/152937734046.21213.9454131988766280028.stgit@pluto.themaw.net
    Signed-off-by: Ian Kent <raven@themaw.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- fs/autofs4/autofs_i.h
+++ fs/autofs4/autofs_i.h
@@ -152,7 +152,7 @@ int autofs4_expire_run(struct super_block *, struct vfsmount *,
 		       struct autofs_sb_info *,
 		       struct autofs_packet_expire __user *);
 int autofs4_do_expire_multi(struct super_block *sb, struct vfsmount *mnt,
-			    struct autofs_sb_info *sbi, int when);
+			    struct autofs_sb_info *sbi, unsigned int how);
 int autofs4_expire_multi(struct super_block *, struct vfsmount *,
 			 struct autofs_sb_info *, int __user *);
 
