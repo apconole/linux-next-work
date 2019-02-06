1e63147f84d8206d549b3dbe8abeff40eb137a8e
Alaa Hleihel <ahleihel@redhat.com>
2019-02-06 12:41:09 -0500

    [netdrv] mlx5: Set uid as part of RQ commands
    Message-id: <1549457075-6314-524-git-send-email-ahleihel@redhat.com>
    Patchwork-id: 240478
    O-Subject: [RHEL7.7 BZ 1642498 523/729] net/mlx5: Set uid as part of RQ commands
    Bugzilla: 1642498
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Honggang Li <honli@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1642498
    Upstream: v4.20-rc1
    
    commit d269b3afffcb107375d9cf73127fc2e0181bc90b
    Author: Yishai Hadas <yishaih@mellanox.com>
    Date:   Thu Sep 20 21:35:22 2018 +0300
    
        net/mlx5: Set uid as part of RQ commands
    
        Set uid as part of RQ commands so that the firmware can manage the
        RQ object in a secured way.
    
        That will enable using an RQ that was created by verbs application
        to be used by the DEVX flow in case the uid is equal.
    
        Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
        Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    
    Cc: Erez Alfasi <ealfasi@redhat.com>
    Cc: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -5480,7 +5480,7 @@ enum {
 
 struct mlx5_ifc_modify_rq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -6156,7 +6156,7 @@ struct mlx5_ifc_destroy_rq_out_bits {
 
 struct mlx5_ifc_destroy_rq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
@@ -6839,7 +6839,7 @@ struct mlx5_ifc_create_rq_out_bits {
 
 struct mlx5_ifc_create_rq_in_bits {
 	u8         opcode[0x10];
-	u8         reserved_at_10[0x10];
+	u8         uid[0x10];
 
 	u8         reserved_at_20[0x10];
 	u8         op_mod[0x10];
