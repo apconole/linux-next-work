01ccf7c7346e44dc083f1bfe17d52fccb44098be
Jiri Olsa <jolsa@redhat.com>
2019-02-11 23:25:22 -0500

    [tools] perf llvm: Display eBPF compiling command in debug output
    Message-id: <20190211232742.16489-132-jolsa@redhat.com>
    Patchwork-id: 241543
    O-Subject: [PATCH RHEL7 BZ1672651 131/271] perf llvm: Display eBPF compiling command in debug output
    Bugzilla: 1672651
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1672651
    https://bugzilla.redhat.com/show_bug.cgi?id=1672651
    
    upstream
    ========
    commit 5eab5a7ee032acaab3da8fc95a3614fec14687ac
    Author: Jiri Olsa <jolsa@kernel.org>
    Date: Mon Mar 12 10:43:02 2018 +0100
    
    description
    ===========
    In addition to template, display also the real compile command line with
    all the variables substituted.
    
      llvm compiling command template: $CLANG_EXEC -D__KERNEL__ -D__NR_CPUS__=$NR_CPUS ...
      llvm compiling command : /usr/bin/clang -D__KERNEL__ -D__NR_CPUS__=24 -DLINUX_VERSION_CODE=0x41000 ...
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- tools/perf/util/llvm-utils.c
+++ tools/perf/util/llvm-utils.c
@@ -432,6 +432,7 @@ int llvm__compile_bpf(const char *path, void **p_obj_buf,
 	char clang_path[PATH_MAX], nr_cpus_avail_str[64], abspath[PATH_MAX];
 	char *kbuild_dir = NULL, *kbuild_include_opts = NULL;
 	const char *template = llvm_param.clang_bpf_cmd_template;
+	char *command_echo, *command_out;
 
 	if (path[0] != '-' && realpath(path, abspath) == NULL) {
 		err = errno;
@@ -486,6 +487,16 @@ int llvm__compile_bpf(const char *path, void **p_obj_buf,
 		      (path[0] == '-') ? path : abspath);
 
 	pr_debug("llvm compiling command template: %s\n", template);
+
+	if (asprintf(&command_echo, "echo -n \"%s\"", template) < 0)
+		goto errout;
+
+	err = read_from_pipe(command_echo, (void **) &command_out, NULL);
+	if (err)
+		goto errout;
+
+	pr_debug("llvm compiling command : %s\n", command_out);
+
 	err = read_from_pipe(template, &obj_buf, &obj_buf_sz);
 	if (err) {
 		pr_err("ERROR:\tunable to compile %s\n", path);
@@ -496,6 +507,8 @@ int llvm__compile_bpf(const char *path, void **p_obj_buf,
 		goto errout;
 	}
 
+	free(command_echo);
+	free(command_out);
 	free(kbuild_dir);
 	free(kbuild_include_opts);
 
@@ -508,6 +521,7 @@ int llvm__compile_bpf(const char *path, void **p_obj_buf,
 		*p_obj_buf_sz = obj_buf_sz;
 	return 0;
 errout:
+	free(command_echo);
 	free(kbuild_dir);
 	free(kbuild_include_opts);
 	free(obj_buf);
