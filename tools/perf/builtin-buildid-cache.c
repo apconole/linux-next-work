d471a57c7135432f6152a42877bead22970b3e26
Jiri Olsa <jolsa@redhat.com>
2019-02-18 13:02:07 -0500

    [tools] perf buildid-cache: Support --list option
    Message-id: <20190218130507.16170-8-jolsa@redhat.com>
    Patchwork-id: 242034
    O-Subject: [PATCH RHEL7 BZ1672893 007/187] perf buildid-cache: Support --list option
    Bugzilla: 1672893
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1672893
    https://bugzilla.redhat.com/show_bug.cgi?id=1672893
    
    upstream
    ========
    commit 8e1e0d74672f84e6c4b1e26ce2bb2e529aaf8676
    Author: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>
    Date: Tue Apr 17 09:43:45 2018 +0530
    
    description
    ===========
    'perf buildid-cache' allows to add/remove files into cache but there is
    no option to list all cached files. Add --list option to list all
    _valid_ cached files.
    
    Ex,
      # perf buildid-cache --add /tmp/a.out
      # perf buildid-cache -l
        8a86ef73e44067bca52cc3f6cd3e5446c783391c /tmp/a.out
    
    RHEL7
    =====
    Context conflict with missing missing namespace support.
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- tools/perf/builtin-buildid-cache.c
+++ tools/perf/builtin-buildid-cache.c
@@ -23,6 +23,7 @@
 #include "util/session.h"
 #include "util/symbol.h"
 #include "util/time-utils.h"
+#include "util/probe-file.h"
 
 static int build_id_cache__kcore_buildid(const char *proc_dir, char *sbuildid)
 {
@@ -281,12 +282,34 @@ static int build_id_cache__update_file(const char *filename)
 	return err;
 }
 
+static int build_id_cache__show_all(void)
+{
+	struct strlist *bidlist;
+	struct str_node *nd;
+	char *buf;
+
+	bidlist = build_id_cache__list_all(true);
+	if (!bidlist) {
+		pr_debug("Failed to get buildids: -%d\n", errno);
+		return -1;
+	}
+	strlist__for_each_entry(nd, bidlist) {
+		buf = build_id_cache__origname(nd->s);
+		fprintf(stdout, "%s %s\n", nd->s, buf);
+		free(buf);
+	}
+	strlist__delete(bidlist);
+	return 0;
+}
+
 int cmd_buildid_cache(int argc, const char **argv)
 {
 	struct strlist *list;
 	struct str_node *pos;
 	int ret = 0;
 	bool force = false;
+	bool list_files = false;
+	bool opts_flag = false;
 	char const *add_name_list_str = NULL,
 		   *remove_name_list_str = NULL,
 		   *purge_name_list_str = NULL,
@@ -309,6 +332,7 @@ int cmd_buildid_cache(int argc, const char **argv)
 		    "file(s) to remove"),
 	OPT_STRING('p', "purge", &purge_name_list_str, "file list",
 		    "file(s) to remove (remove old caches too)"),
+	OPT_BOOLEAN('l', "list", &list_files, "list all cached files"),
 	OPT_STRING('M', "missing", &missing_filename, "file",
 		   "to find missing build ids in the cache"),
 	OPT_BOOLEAN('f', "force", &force, "don't complain, do it"),
@@ -325,10 +349,18 @@ int cmd_buildid_cache(int argc, const char **argv)
 	argc = parse_options(argc, argv, buildid_cache_options,
 			     buildid_cache_usage, 0);
 
-	if (argc || (!add_name_list_str && !kcore_filename &&
-		     !remove_name_list_str && !purge_name_list_str &&
-		     !missing_filename && !update_name_list_str))
+	opts_flag = add_name_list_str || kcore_filename ||
+		remove_name_list_str || purge_name_list_str ||
+		missing_filename || update_name_list_str;
+
+	if (argc || !(list_files || opts_flag))
 		usage_with_options(buildid_cache_usage, buildid_cache_options);
+
+	/* -l is exclusive. It can not be used with other options. */
+	if (list_files && opts_flag) {
+		usage_with_options_msg(buildid_cache_usage,
+			buildid_cache_options, "-l is exclusive.\n");
+	}
 
 	if (missing_filename) {
 		data.file.path = missing_filename;
@@ -343,6 +375,11 @@ int cmd_buildid_cache(int argc, const char **argv)
 		goto out;
 
 	setup_pager();
+
+	if (list_files) {
+		ret = build_id_cache__show_all();
+		goto out;
+	}
 
 	if (add_name_list_str) {
 		list = strlist__new(add_name_list_str, NULL);
