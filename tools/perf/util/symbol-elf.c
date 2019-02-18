79d916122fc0b5ee05da1c22cf4b4229eec5fb47
Jiri Olsa <jolsa@redhat.com>
2019-02-18 13:03:28 -0500

    [tools] perf kcore_copy: Keep phdr data in a list
    Message-id: <20190218130507.16170-89-jolsa@redhat.com>
    Patchwork-id: 242101
    O-Subject: [PATCH RHEL7 BZ1672893 088/187] perf kcore_copy: Keep phdr data in a list
    Bugzilla: 1672893
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    From: Jiri Olsa <jolsa@kernel.org>
    
    Bugzilla: 1672893
    https://bugzilla.redhat.com/show_bug.cgi?id=1672893
    
    upstream
    ========
    commit f6838209484d5cfb368ca5c61d150cc4054eef59
    Author: Adrian Hunter <adrian.hunter@intel.com>
    Date: Tue May 22 13:54:38 2018 +0300
    
    description
    ===========
    Currently, kcore_copy makes 2 program headers, one for the kernel text
    (namely kernel_map) and one for the modules (namely modules_map). Now
    more program headers are needed, but treating each program header as a
    special case results in much more code.
    
    Instead, in preparation to add more program headers, change to keep
    program header data (phdr_data) in a list.
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- tools/perf/util/symbol-elf.c
+++ tools/perf/util/symbol-elf.c
@@ -1387,6 +1387,7 @@ struct phdr_data {
 	off_t offset;
 	u64 addr;
 	u64 len;
+	struct list_head node;
 };
 
 struct kcore_copy_info {
@@ -1398,6 +1399,7 @@ struct kcore_copy_info {
 	u64 last_module_symbol;
 	struct phdr_data kernel_map;
 	struct phdr_data modules_map;
+	struct list_head phdrs;
 };
 
 static int kcore_copy__process_kallsyms(void *arg, const char *name, char type,
@@ -1508,6 +1510,11 @@ static int kcore_copy__read_maps(struct kcore_copy_info *kci, Elf *elf)
 {
 	if (elf_read_maps(elf, true, kcore_copy__read_map, kci) < 0)
 		return -1;
+
+	if (kci->kernel_map.len)
+		list_add_tail(&kci->kernel_map.node, &kci->phdrs);
+	if (kci->modules_map.len)
+		list_add_tail(&kci->modules_map.node, &kci->phdrs);
 
 	return 0;
 }
@@ -1676,6 +1683,8 @@ int kcore_copy(const char *from_dir, const char *to_dir)
 	struct kcore_copy_info kci = { .stext = 0, };
 	char kcore_filename[PATH_MAX];
 	char extract_filename[PATH_MAX];
+
+	INIT_LIST_HEAD(&kci.phdrs);
 
 	if (kcore_copy__copy_file(from_dir, to_dir, "kallsyms"))
 		return -1;
