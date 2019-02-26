b055582119c84759f7c149d15ebb85db1574f18b
Paolo Abeni <pabeni@redhat.com>
2019-02-26 14:12:09 -0500

    [net] sched: fix qdisc->running lockdep annotations
    Message-id: <b6456d9ad2b2d8323f41c50678f4aa8103d1c59b.1551187674.git.pabeni@redhat.com>
    Patchwork-id: 243268
    O-Subject: [RHEL7.7 net] net: sched: fix qdisc->running lockdep annotations
    Bugzilla: 1680442
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: David S. Miller <davem@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1680442
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=20369877
    Tested: vs bugzilla testcase, LNST and tier1
    
    Changes from upstream commit:
     - we already have a partial backport of the upstream commit
       52fbb2907988 ("net: sched: fix qdisc->running lockdep annotations"),
       This is partial backport pulling the omitted parts and dropping the
       placeholder inserted with rhel commit 3ede499ed9fc ("net: sched: fix
       qdisc->running lockdep annotations")
    
    Upstream commit:
    commit 52fbb2907988aa0583c6d9d53a56aee090b2df7e
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Thu Jun 9 07:45:11 2016 -0700
    
        net: sched: fix qdisc->running lockdep annotations
    
        1) qdisc_run_begin() is really using the equivalent of a trylock.
          Instead of using write_seqcount_begin(), use a combination of
          raw_write_seqcount_begin() and correct lockdep annotation.
    
        2) sch_direct_xmit() should use regular spin_lock(root_lock)
    
        Fixes: f9eb8aea2a1e ("net_sched: transform qdisc running bit into a seqcount")
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Reported-by: David Ahern <dsa@cumulusnetworks.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -135,10 +135,11 @@ static inline bool qdisc_run_begin(struct Qdisc *qdisc)
 {
 	if (qdisc_is_running(qdisc))
 		return false;
-	/* RHEL: The seqcount structure has not lockdep functionality so
-	 * we use plain write_seqcount_begin() here.
+	/* Variant of write_seqcount_begin() telling lockdep a trylock
+	 * was attempted.
 	 */
-	write_seqcount_begin(&qdisc->running);
+	raw_write_seqcount_begin(&qdisc->running);
+	seqcount_acquire(&qdisc->running.dep_map, 0, 1, _RET_IP_);
 	return true;
 }
 
