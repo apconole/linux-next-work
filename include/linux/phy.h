21e052430be99776427e10301427106ecaa1588c
Ivan Vecera <ivecera@redhat.com>
2019-03-06 13:55:08 -0500

    [netdrv] phy: add paged phy register accessors
    Message-id: <20190306135521.25468-13-ivecera@redhat.com>
    Patchwork-id: 245113
    O-Subject: [RHEL7.7 PATCH 12/25] net: phy: add paged phy register accessors
    Bugzilla: 1685646
    RH-Acked-by: Stefan Assmann <sassmann@redhat.com>
    RH-Acked-by: Neil Horman <nhorman@redhat.com>
    RH-Acked-by: Petr Oros <poros@redhat.com>
    RH-Acked-by: Corinna Vinschen <vinschen@redhat.com>
    RH-Acked-by: John Linville <linville@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/show_bug.cgi?id=1685646
    
    Conflicts: Using phydev->mdio_* instead of phydev->mdio.*. Need to wrap new
    phy_driver fields by RH_KABI_EXTEND macro.
    
    Upstream commit(s):
    commit 78ffc4acceff48522b92d8fbf8f4a0ffe78838b2
    Author: Russell King <rmk+kernel@armlinux.org.uk>
    Date:   Tue Jan 2 10:58:43 2018 +0000
    
        net: phy: add paged phy register accessors
    
        Add a set of paged phy register accessors which are inherently safe in
        their design against other accesses interfering with the paged access.
    
        Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
        Reviewed-by: Andrew Lunn <andrew@lunn.ch>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -565,6 +565,9 @@ struct phy_driver {
 	RH_KABI_EXTEND(int (*write_mmd)(struct phy_device *dev, int devnum,
 					u16 regnum, u16 val))
 
+	RH_KABI_EXTEND(int (*read_page)(struct phy_device *dev))
+	RH_KABI_EXTEND(int (*write_page)(struct phy_device *dev, int page))
+
 	/* PHY API is not on symbol whitelist now so we can extend
 	 * this structure directly via simple RH_KABI_EXTEND.
 	 */
@@ -683,6 +686,14 @@ static inline bool phy_is_internal(struct phy_device *phydev)
  * Same rules as for phy_write();
  */
 int phy_write_mmd(struct phy_device *phydev, int devad, u32 regnum, u16 val);
+
+int phy_save_page(struct phy_device *phydev);
+int phy_select_page(struct phy_device *phydev, int page);
+int phy_restore_page(struct phy_device *phydev, int oldpage, int ret);
+int phy_read_paged(struct phy_device *phydev, int page, u32 regnum);
+int phy_write_paged(struct phy_device *phydev, int page, u32 regnum, u16 val);
+int phy_modify_paged(struct phy_device *phydev, int page, u32 regnum,
+		     u16 mask, u16 set);
 
 struct phy_device *phy_device_create(struct mii_bus *bus, int addr, int phy_id,
 				     bool is_c45,
