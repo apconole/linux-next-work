172e3bd2b5c0ffa3cd54cf5af2461a666382c1e8
Gary Hook <ghook@redhat.com>
2019-03-15 22:20:58 -0400

    [x86] mce/amd: Read MCx_MISC block addresses on any CPU
    Message-id: <20190315222102.13986-5-ghook@redhat.com>
    Patchwork-id: 246326
    O-Subject: [RHEL7.7 BZ 1685269 4/8] x86/MCE/AMD: Read MCx_MISC block addresses on any CPU
    Bugzilla: 1685269
    RH-Acked-by: David Arcari <darcari@redhat.com>
    RH-Acked-by: Dean Nelson <dnelson@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1685269
    Upstream Status: v4.17-rc6
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=20582067
    Conflicts: The RH 7 code for amd_threshold_interrupt is a different
    implementation upstream; here it contains a call to get_block_address().
    Update the call as the API for that function has changed in this patch.
    
    commit fbf96cf904dc154a28338fe68f72902e9af57afc
    Author: Borislav Petkov <bp@suse.de>
    Date:   Thu May 17 18:32:33 2018 +0200
    
        x86/MCE/AMD: Read MCx_MISC block addresses on any CPU
    
        We used rdmsr_safe_on_cpu() to make sure we're reading the proper CPU's
        MISC block addresses. However, that caused trouble with CPU hotplug due to
        the _on_cpu() helper issuing an IPI while IRQs are disabled.
    
        But we don't have to do that: the block addresses are the same on any CPU
        so we can read them on any CPU. (What practically happens is, we read them
        on the BSP and cache them, and for later reads, we service them from the
        cache).
    
        Suggested-by: Yazen Ghannam <Yazen.Ghannam@amd.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: David Arcari <darcari@redhat.com>
    Cc: Gary R Hook <ghook@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/x86/kernel/cpu/mcheck/mce_amd.c
+++ arch/x86/kernel/cpu/mcheck/mce_amd.c
@@ -445,8 +445,7 @@ static void deferred_error_interrupt_enable(struct cpuinfo_x86 *c)
 	wrmsr(MSR_CU_DEF_ERR, low, high);
 }
 
-static u32 smca_get_block_address(unsigned int cpu, unsigned int bank,
-				  unsigned int block)
+static u32 smca_get_block_address(unsigned int bank, unsigned int block)
 {
 	u32 low, high;
 	u32 addr = 0;
@@ -465,13 +464,13 @@ static u32 smca_get_block_address(unsigned int cpu, unsigned int bank,
 	 * For SMCA enabled processors, BLKPTR field of the first MISC register
 	 * (MCx_MISC0) indicates presence of additional MISC regs set (MISC1-4).
 	 */
-	if (rdmsr_safe_on_cpu(cpu, MSR_AMD64_SMCA_MCx_CONFIG(bank), &low, &high))
+	if (rdmsr_safe(MSR_AMD64_SMCA_MCx_CONFIG(bank), &low, &high))
 		goto out;
 
 	if (!(low & MCI_CONFIG_MCAX))
 		goto out;
 
-	if (!rdmsr_safe_on_cpu(cpu, MSR_AMD64_SMCA_MCx_MISC(bank), &low, &high) &&
+	if (!rdmsr_safe(MSR_AMD64_SMCA_MCx_MISC(bank), &low, &high) &&
 	    (low & MASK_BLKPTR_LO))
 		addr = MSR_AMD64_SMCA_MCx_MISCy(bank, block - 1);
 
@@ -480,7 +479,7 @@ out:
 	return addr;
 }
 
-static u32 get_block_address(unsigned int cpu, u32 current_addr, u32 low, u32 high,
+static u32 get_block_address(u32 current_addr, u32 low, u32 high,
 			     unsigned int bank, unsigned int block)
 {
 	u32 addr = 0, offset = 0;
@@ -489,7 +488,7 @@ static u32 get_block_address(unsigned int cpu, u32 current_addr, u32 low, u32 hi
 		return addr;
 
 	if (mce_flags.smca)
-		return smca_get_block_address(cpu, bank, block);
+		return smca_get_block_address(bank, block);
 
 	/* Fall back to method we used for older processors: */
 	switch (block) {
@@ -567,7 +566,7 @@ void mce_amd_feature_init(struct cpuinfo_x86 *c)
 			smca_configure(bank, cpu);
 
 		for (block = 0; block < NR_BLOCKS; ++block) {
-			address = get_block_address(cpu, address, low, high, bank, block);
+			address = get_block_address(address, low, high, bank, block);
 			if (!address)
 				break;
 
@@ -907,7 +906,7 @@ static void amd_threshold_interrupt(void)
 		if (!(per_cpu(bank_map, cpu) & (1 << bank)))
 			continue;
 		for (block = 0; block < NR_BLOCKS; ++block) {
-			address = get_block_address(cpu, address, low, high, bank, block);
+			address = get_block_address(address, low, high, bank, block);
 			if (!address)
 				break;
 
@@ -1159,7 +1158,7 @@ static int allocate_threshold_blocks(unsigned int cpu, unsigned int bank,
 	if (err)
 		goto out_free;
 recurse:
-	address = get_block_address(cpu, address, low, high, bank, ++block);
+	address = get_block_address(address, low, high, bank, ++block);
 	if (!address)
 		return 0;
 
