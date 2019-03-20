4157a3bcbb456ae64ad6010ac4560235a0680c67
Desnes Augusto Nunes do Rosario <drosario@redhat.com>
2019-03-19 20:03:03 -0400

    [powerpc] pseries/mobility: Extend start/stop topology update scope
    Message-id: <20190319200303.23761-3-drosario@redhat.com>
    Patchwork-id: 247085
    O-Subject: [RHEL-7.7 BZ1671799 PATCH 2/2] powerpc/pseries/mobility: Extend start/stop topology update scope
    Bugzilla: 1671799
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    RHBZ#:
    ------
    https://bugzilla.redhat.com/show_bug.cgi?id=1671799
    
    DESCRIPTION:
    ------------
    This patch is a backport of commit: <65b9fdadfc4d>
    Author: Michael Bringmann <mwb@linux.vnet.ibm.com>
    Date: Tue, 9 Oct 2018 15:12:14 -0500
    
        The powerpc mobility code may receive RTAS requests to perform PRRN
        (Platform Resource Reassignment Notification) topology changes at any
        time, including during LPAR migration operations.
    
        In some configurations where the affinity of CPUs or memory is being
        changed on that platform, the PRRN requests may apply or refer to
        outdated information prior to the complete update of the device-tree.
    
        This patch changes the duration for which topology updates are
        suppressed during LPAR migrations from just the rtas_ibm_suspend_me()
        / 'ibm,suspend-me' call(s) to cover the entire migration_store()
        operation to allow all changes to the device-tree to be applied prior
        to accepting and applying any PRRN requests.
    
        For tracking purposes, pr_info notices are added to the functions
        start_topology_update() and stop_topology_update() of 'numa.c'.
    
        Signed-off-by: Michael Bringmann <mwb@linux.vnet.ibm.com>
        Reviewed-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>
        Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    * Patch refresh - no functional change was performed.
    
    Signed-off-by: Desnes A. Nunes do Rosario <desnesn@linux.vnet.ibm.com>
    
    UPSTREAM STATUS:
    ----------------
    commit 65b9fdadfc4d87e2577b791fb3495cd39c93d8c0
    
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=65b9fdadfc4d87e2577b791fb3495cd39c93d8c0

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -1854,6 +1854,10 @@ int start_topology_update(void)
 		}
 	}
 
+	pr_info("Starting topology update%s%s\n",
+		(prrn_enabled ? " prrn_enabled" : ""),
+		(vphn_enabled ? " vphn_enabled" : ""));
+
 	return rc;
 }
 
@@ -1874,6 +1878,8 @@ int stop_topology_update(void)
 		vphn_enabled = 0;
 		rc = del_timer_sync(&topology_timer);
 	}
+
+	pr_info("Stopping topology update\n");
 
 	return rc;
 }
