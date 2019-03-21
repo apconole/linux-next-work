d4d9719ef6a76a01a704fa3ee307ccf38c71543f
Miklos Szeredi <mszeredi@redhat.com>
2019-03-21 15:52:03 -0400

    [fs] fuse: continue to send FUSE_RELEASEDIR when FUSE_OPEN returns ENOSYS
    Message-id: <20190321155210.22206-79-mszeredi@redhat.com>
    Patchwork-id: 247286
    O-Subject: [RHEL7.7 PATCH 78/85] fuse: continue to send FUSE_RELEASEDIR when FUSE_OPEN returns ENOSYS
    Bugzilla: 1581904
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1581904
    Upstream status: Linus
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=20685393
    Testing: libfuse/test; fsx-linux
    
    Difference from upstream: context difference and removed hunk due to
    missing independent upstream patch.
    
    commit 2e64ff154ce6ce9a8dc0f9556463916efa6ff460
    Author: Chad Austin <chadaustin@fb.com>
    Date:   Mon Dec 10 10:54:52 2018 -0800
    
        fuse: continue to send FUSE_RELEASEDIR when FUSE_OPEN returns ENOSYS
    
        When FUSE_OPEN returns ENOSYS, the no_open bit is set on the connection.
    
        Because the FUSE_RELEASE and FUSE_RELEASEDIR paths share code, this
        incorrectly caused the FUSE_RELEASEDIR request to be dropped and never sent
        to userspace.
    
        Pass an isdir bool to distinguish between FUSE_RELEASE and FUSE_RELEASEDIR
        inside of fuse_file_put.
    
        Fixes: 7678ac50615d ("fuse: support clients that don't implement 'open'")
        Cc: <stable@vger.kernel.org> # v3.14
        Signed-off-by: Chad Austin <chadaustin@fb.com>
        Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    ---
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -122,12 +122,12 @@ static void fuse_release_end(struct fuse_conn *fc, struct fuse_req *req)
 	}
 }
 
-static void fuse_file_put(struct fuse_file *ff, bool sync)
+static void fuse_file_put(struct fuse_file *ff, bool sync, bool isdir)
 {
 	if (atomic_dec_and_test(&ff->count)) {
 		struct fuse_req *req = ff->reserved_req;
 
-		if (ff->fc->no_open) {
+		if (ff->fc->no_open && !isdir) {
 			/*
 			 * Drop the release request when client does not
 			 * implement 'open'
@@ -279,10 +279,11 @@ static void fuse_prepare_release(struct fuse_file *ff, int flags, int opcode)
 	req->in.args[0].value = inarg;
 }
 
-void fuse_release_common(struct file *file, int opcode)
+void fuse_release_common(struct file *file, bool isdir)
 {
 	struct fuse_file *ff;
 	struct fuse_req *req;
+	int opcode = isdir ? FUSE_RELEASEDIR : FUSE_RELEASE;
 
 	ff = file->private_data;
 	if (unlikely(!ff))
@@ -310,7 +311,7 @@ void fuse_release_common(struct file *file, int opcode)
 	 * synchronous RELEASE is allowed (and desirable) in this case
 	 * because the server can be trusted not to screw up.
 	 */
-	fuse_file_put(ff, ff->fc->destroy_req != NULL);
+	fuse_file_put(ff, ff->fc->destroy_req != NULL, isdir);
 }
 
 static int fuse_open(struct inode *inode, struct file *file)
@@ -326,7 +327,7 @@ static int fuse_release(struct inode *inode, struct file *file)
 	if (fc->writeback_cache)
 		write_inode_now(inode, 1);
 
-	fuse_release_common(file, FUSE_RELEASE);
+	fuse_release_common(file, false);
 
 	/* return value is ignored by VFS */
 	return 0;
@@ -848,7 +849,7 @@ static void fuse_readpages_end(struct fuse_conn *fc, struct fuse_req *req)
 		page_cache_release(page);
 	}
 	if (req->ff)
-		fuse_file_put(req->ff, false);
+		fuse_file_put(req->ff, false, false);
 }
 
 static void fuse_send_readpages(struct fuse_req *req, struct file *file)
@@ -1562,7 +1563,7 @@ static void fuse_writepage_free(struct fuse_conn *fc, struct fuse_req *req)
 		__free_page(req->pages[i]);
 
 	if (req->ff)
-		fuse_file_put(req->ff, false);
+		fuse_file_put(req->ff, false, false);
 }
 
 static void fuse_writepage_finish(struct fuse_conn *fc, struct fuse_req *req)
@@ -1719,7 +1720,7 @@ int fuse_write_inode(struct inode *inode, struct writeback_control *wbc)
 	ff = __fuse_write_file_get(fc, fi);
 	err = fuse_flush_times(inode, ff);
 	if (ff)
-		fuse_file_put(ff, 0);
+		fuse_file_put(ff, false, false);
 
 	return err;
 }
@@ -2033,7 +2034,7 @@ static int fuse_writepages(struct address_space *mapping,
 		err = 0;
 	}
 	if (data.ff)
-		fuse_file_put(data.ff, false);
+		fuse_file_put(data.ff, false, false);
 
 	kfree(data.orig_pages);
 out:
