7f7c0ce74bc4a3d37862b540b8d6ccba84dc48b1
Kairui Song <kasong@redhat.com>
2019-03-26 06:22:42 -0400

    [x86] gart: Exclude GART aperture from kcore
    Message-id: <20190326062242.4925-3-kasong@redhat.com>
    Patchwork-id: 247657
    O-Subject: [RHEL7.7 PATCH 2/2] x86/gart: Exclude GART aperture from kcore
    Bugzilla: 1532150
    RH-Acked-by: Lianbo Jiang <lijiang@redhat.com>
    RH-Acked-by: Baoquan He <bhe@redhat.com>
    RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1532150
    Upstream status: 2a3e83c6f96c513f43ce5a8c9034608ea584a255
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=20713229
    Tested: Tested on hp-dl385g7-03.rhts.eng.pek2.redhat.com
    Conflict:
        There are some improvement applied for kcore reading in upstream, that
        bring some conflict in fs/proc/kcore.c, adjusted the code accordingly.
        And there are some other minor conflict in include/linux/kcore.h that
        are easily resolved.
    
    commit ffc8599aa9763f39f6736a79da4d1575e7006f9a
    Author: Kairui Song <kasong@redhat.com>
    Date:   Fri Mar 8 11:05:08 2019 +0800
    
        x86/gart: Exclude GART aperture from kcore
    
        On machines where the GART aperture is mapped over physical RAM,
        /proc/kcore contains the GART aperture range. Accessing the GART range via
        /proc/kcore results in a kernel crash.
    
        vmcore used to have the same issue, until it was fixed with commit
        2a3e83c6f96c ("x86/gart: Exclude GART aperture from vmcore")', leveraging
        existing hook infrastructure in vmcore to let /proc/vmcore return zeroes
        when attempting to read the aperture region, and so it won't read from the
        actual memory.
    
        Apply the same workaround for kcore. First implement the same hook
        infrastructure for kcore, then reuse the hook functions introduced in the
        previous vmcore fix. Just with some minor adjustment, rename some functions
        for more general usage, and simplify the hook infrastructure a bit as there
        is no module usage yet.
    
        Suggested-by: Baoquan He <bhe@redhat.com>
        Signed-off-by: Kairui Song <kasong@redhat.com>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Jiri Bohac <jbohac@suse.cz>
        Acked-by: Baoquan He <bhe@redhat.com>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: "H. Peter Anvin" <hpa@zytor.com>
        Cc: Alexey Dobriyan <adobriyan@gmail.com>
        Cc: Andrew Morton <akpm@linux-foundation.org>
        Cc: Omar Sandoval <osandov@fb.com>
        Cc: Dave Young <dyoung@redhat.com>
        Link: https://lkml.kernel.org/r/20190308030508.13548-1-kasong@redhat.com
    
    Signed-off-by: Kairui Song <kasong@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/kcore.h
+++ include/linux/kcore.h
@@ -35,6 +35,7 @@ struct vmcoredd_node {
 
 #ifdef CONFIG_PROC_KCORE
 extern void kclist_add(struct kcore_list *, void *, size_t, int type);
+extern int __init register_mem_pfn_is_ram(int (*fn)(unsigned long pfn));
 #else
 static inline
 void kclist_add(struct kcore_list *new, void *addr, size_t size, int type)
