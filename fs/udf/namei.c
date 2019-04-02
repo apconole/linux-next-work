a4d10bf2f4c951b780c3963e87161c6b078bcddf
Miklos Szeredi <mszeredi@redhat.com>
2019-04-02 09:48:25 -0400

    [fs] do d_instantiate/unlock_new_inode combinations safely
    Message-id: <20190402094839.12802-61-mszeredi@redhat.com>
    Patchwork-id: 248570
    O-Subject: [RHEL7.7 PATCH 140/154] do d_instantiate/unlock_new_inode combinations safely
    Bugzilla: 1693457
    RH-Acked-by: Ian Kent <ikent@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1693457
    Upstream status: Linus
    Brew: https://brewweb.devel.redhat.com/taskinfo?taskID=20742384
    Testing: unionmount-testsuite, xfstests, pjd-fstest
    
    Difference from upstream: completely removed parts of patch that had merge
    conflicts and which are not configured in rhel7 (ecryptfs, ext2, f2fs,
    jffs2, jfs, nilfs2, orangefs, reiserfs, ufs) and difference due to missing
    upstream patches and other minor context difference
    
    commit 1e2e547a93a00ebc21582c06ca3c6cfea2a309ee
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Fri May 4 08:23:01 2018 -0400
    
        do d_instantiate/unlock_new_inode combinations safely
    
        For anything NFS-exported we do _not_ want to unlock new inode
        before it has grown an alias; original set of fixes got the
        ordering right, but missed the nasty complication in case of
        lockdep being enabled - unlock_new_inode() does
                lockdep_annotate_inode_mutex_key(inode)
        which can only be done before anyone gets a chance to touch
        ->i_mutex.  Unfortunately, flipping the order and doing
        unlock_new_inode() before d_instantiate() opens a window when
        mkdir can race with open-by-fhandle on a guessed fhandle, leading
        to multiple aliases for a directory inode and all the breakage
        that follows from that.
    
                Correct solution: a new primitive (d_instantiate_new())
        combining these two in the right order - lockdep annotate, then
        d_instantiate(), then the rest of unlock_new_inode().  All
        combinations of d_instantiate() with unlock_new_inode() should
        be converted to that.
    
        Cc: stable@kernel.org   # 2.6.29 and later
        Tested-by: Mike Marshall <hubcap@omnibond.com>
        Reviewed-by: Andreas Dilger <adilger@dilger.ca>
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    ---
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- fs/udf/namei.c
+++ fs/udf/namei.c
@@ -590,8 +590,7 @@ static int udf_create(struct inode *dir, struct dentry *dentry, umode_t mode,
 	if (fibh.sbh != fibh.ebh)
 		brelse(fibh.ebh);
 	brelse(fibh.sbh);
-	unlock_new_inode(inode);
-	d_instantiate(dentry, inode);
+	d_instantiate_new(dentry, inode);
 
 	return 0;
 }
@@ -691,8 +690,7 @@ static int udf_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
 	udf_write_fi(dir, &cfi, fi, &fibh, NULL, NULL);
 	inc_nlink(dir);
 	mark_inode_dirty(dir);
-	unlock_new_inode(inode);
-	d_instantiate(dentry, inode);
+	d_instantiate_new(dentry, inode);
 	if (fibh.sbh != fibh.ebh)
 		brelse(fibh.ebh);
 	brelse(fibh.sbh);
