b198046c2cda7ab23330164bf46e3596e871a0aa
Arjun Vynipadath <avynipad@redhat.com>
2019-04-17 06:05:20 -0400

    [scsi] csiostor: drop serial_number usage
    Message-id: <20190417060522.26350-3-avynipad@redhat.com>
    Patchwork-id: 250188
    O-Subject: [RHEL7.7 BZ 1696189 2/4] scsi: csiostor: drop serial_number usage
    Bugzilla: 1696189
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1696189
    Conflicts: Upstream commit "9cb78c1 scsi: use 64-bit LUNs"
    is not present in RHEL7, hence the difference in types.
    
    commit f50e76052321cc49f9d49fe444c3d3806f223dc3
    Author: Hannes Reinecke <hare@suse.com>
    Date:   Tue Feb 26 15:56:41 2019 +0100
    
        scsi: csiostor: drop serial_number usage
    
        Use request tag instead of the serial number when printing out logging
        messages.
    
        Signed-off-by: Hannes Reinecke <hare@suse.com>
        Reviewed-by: Bart Van Assche <bvanassche@acm.org>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Cc: Arjun Vynipadath <avynipad@redhat.com>
    Cc: Don Dutile <ddutile@redhat.com>
    Cc: Ewan D. Milne <emilne@redhat.com>
    Cc: Jarod Wilson <jarod@redhat.com>
    Cc: Jonathan Toppins <jtoppins@redhat.com>
    Cc: Maurizio Lombardi <mlombard@redhat.com>
    Cc: Neil Horman <nhorman@redhat.com>
    Cc: Venkatesh Pottem <vpottem@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- drivers/scsi/csiostor/csio_scsi.c
+++ drivers/scsi/csiostor/csio_scsi.c
@@ -2014,15 +2014,15 @@ inval_scmnd:
 	/* FW successfully aborted the request */
 	if (host_byte(cmnd->result) == DID_REQUEUE) {
 		csio_info(hw,
-			"Aborted SCSI command to (%d:%d) serial#:0x%lx\n",
+			"Aborted SCSI command to (%d:%d) tag %u\n",
 			cmnd->device->id, cmnd->device->lun,
-			cmnd->serial_number);
+			cmnd->request->tag);
 		return SUCCESS;
 	} else {
 		csio_info(hw,
-			"Failed to abort SCSI command, (%d:%d) serial#:0x%lx\n",
+			"Failed to abort SCSI command, (%d:%d) tag %u\n",
 			cmnd->device->id, cmnd->device->lun,
-			cmnd->serial_number);
+			cmnd->request->tag);
 		return FAILED;
 	}
 }
