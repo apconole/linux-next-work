9f9dbe018111fc6ec5600eb7e4eaa3fa20d21338
Aristeu Rozanski <aris@redhat.com>
2019-04-23 20:10:40 -0400

    [tty] Convert termios_mutex to termios_rwsem
    Message-id: <20190423201040.026692212@redhat.com>
    Patchwork-id: 250687
    O-Subject: [RHEL7 PATCH 1/3] tty: Convert termios_mutex to termios_rwsem
    Bugzilla: 1684982
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    RH-Acked-by: Joe Lawrence <joe.lawrence@redhat.com>
    
    BZ#1684982
    brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21256062
    Tested: by reporter and me using reproducer
    
    commit 6a1c0680cf3ba94356ecd58833e1540c93472a57
    Author: Peter Hurley <peter@hurleysoftware.com>
    Date:   Sat Jun 15 09:14:23 2013 -0400
    
        tty: Convert termios_mutex to termios_rwsem
    
        termios is commonly accessed unsafely (especially by N_TTY)
        because the existing mutex forces exclusive access.
        Convert existing usage.
    
        Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -10,6 +10,7 @@
 #include <linux/mutex.h>
 #include <linux/tty_flags.h>
 #include <uapi/linux/tty.h>
+#include <linux/rwsem.h>
 
 
 
@@ -244,9 +245,9 @@ struct tty_struct {
 
 	struct mutex atomic_write_lock;
 	struct mutex legacy_mutex;
-	struct mutex termios_mutex;
+	RH_KABI_DEPRECATE(struct mutex, termios_mutex)
 	spinlock_t ctrl_lock;
-	/* Termios values are protected by the termios mutex */
+	/* Termios values are protected by the termios rwsem */
 	struct ktermios termios, termios_locked;
 	struct termiox *termiox;	/* May be NULL for unsupported */
 	char name[64];
@@ -254,7 +255,7 @@ struct tty_struct {
 	struct pid *session;
 	unsigned long flags;
 	int count;
-	struct winsize winsize;		/* termios mutex */
+	struct winsize winsize;		/* termios rwsem */
 	unsigned char stopped:1, hw_stopped:1, flow_stopped:1, packet:1;
 	unsigned char ctrl_status;	/* ctrl_lock */
 	unsigned int receive_room;	/* Bytes free for queue */
@@ -281,6 +282,7 @@ struct tty_struct {
 	struct tty_port *port;
 
 	RH_KABI_EXTEND(struct ld_semaphore ldisc_sem)
+	RH_KABI_EXTEND(struct rw_semaphore termios_rwsem)
 };
 
 /* Each of a tty's open files has private_data pointing to tty_file_private */
