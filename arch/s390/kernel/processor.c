0c990371d83324e5fe0031dd378e13cc7ceec34e
Hendrik Brueckner <brueckner@redhat.com>
2019-04-26 09:45:08 -0400

    [s390] report new CPU capabilities
    Message-id: <20190426094508.14072-1-brueckner@redhat.com>
    Patchwork-id: 251685
    O-Subject: [RHEL7.7 PATCH] [s390] s390: report new CPU capabilities
    Bugzilla: 1690843
    RH-Acked-by: Steve Best <sbest@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Philipp Rudo <prudo@redhat.com>
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    
    Bugzilla: 1690843
    Upstream Status: kernel/git/s390/linux.git
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21317655
    Tested: by IBM
    
    commit a8fd61688dfad6fdce95fa64cacd8a66595697b8
    Author: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Date:   Tue Feb 5 16:15:01 2019 +0100
    
        s390: report new CPU capabilities
    
        Add hardware capability bits and features tags to /proc/cpuinfo
        for 4 new CPU features:
          "Vector-Enhancements Facility 2" (tag "vxe2", hwcap 2^15)
          "Vector-Packed-Decimal-Enhancement Facility" (tag "vxp", hwcap 2^16)
          "Enhanced-Sort Facility" (tag "sort", hwcap 2^17)
          "Deflate-Conversion Facility" (tag "dflt", hwcap 2^18)
    
        Reviewed-by: Heiko Carstens <heiko.carstens@de.ibm.com>
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- arch/s390/kernel/processor.c
+++ arch/s390/kernel/processor.c
@@ -63,7 +63,8 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 {
 	static const char *hwcap_str[] = {
 		"esan3", "zarch", "stfle", "msa", "ldisp", "eimm", "dfp",
-		"edat", "etf3eh", "highgprs", "te", "vx", "vxd", "vxe", "gs"
+		"edat", "etf3eh", "highgprs", "te", "vx", "vxd", "vxe", "gs",
+		"vxe2", "vxp", "sort", "dflt"
 	};
 	static const char * const int_hwcap_str[] = {
 		"sie"
