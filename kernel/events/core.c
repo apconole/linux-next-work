d52d3632f0cd84ce079b283c5282b8df83f5f53c
Jiri Olsa <jolsa@redhat.com>
2019-04-29 08:14:47 -0400

    [kernel] perf: Make perf_callchain function static
    Message-id: <20190429081454.24988-9-jolsa@redhat.com>
    Patchwork-id: 251748
    O-Subject: [PATCH RHEL7 BZ1482406 08/15] perf: Make perf_callchain function static
    Bugzilla: 1482406
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: 1482406
    https://bugzilla.redhat.com/show_bug.cgi?id=1482406
    
    upstream
    ========
    commit 8cf7e0e22414f5acf85ecb7cd0d4482e6c9696ae
    Author: Jiri Olsa <jolsa@kernel.org>
    Date: Sun Jan 7 17:03:49 2018 +0100
    
    description
    ===========
    And move it to core.c, because there's no caller of this function other
    than the one in core.c
    
    RHEL7
    =====
    Conflict with missing max_stack support.
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6188,6 +6188,21 @@ static u64 perf_virt_to_phys(u64 virt)
 	return phys_addr;
 }
 
+static struct perf_callchain_entry *
+perf_callchain(struct perf_event *event, struct pt_regs *regs)
+{
+	bool kernel = !event->attr.exclude_callchain_kernel;
+	bool user   = !event->attr.exclude_callchain_user;
+	/* Disallow cross-task user callchains. */
+	bool crosstask = event->ctx->task && event->ctx->task != current;
+
+	if (!kernel && !user)
+		return NULL;
+
+	return get_perf_callchain(regs, 0, kernel, user,
+				  crosstask, true);
+}
+
 void perf_prepare_sample(struct perf_event_header *header,
 			 struct perf_sample_data *data,
 			 struct perf_event *event,
