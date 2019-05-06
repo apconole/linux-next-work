ab6c891785bc21174dd45dd317660ba8d89ddfea
Sabrina Dubroca <sdubroca@redhat.com>
2019-05-06 13:56:54 -0400

    [net] xfrm: make local error reporting more robust
    Message-id: <1c43c3ee945ab99b8fc31175323078657fae457b.1557143462.git.sdubroca@redhat.com>
    Patchwork-id: 253781
    O-Subject: [PATCH RHEL7.7 net 17/18] xfrm: make local error reporting more robust
    Bugzilla: 1705575
    RH-Acked-by: Marcelo Leitner <mleitner@redhat.com>
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1705575
    Brew: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=21465172
    Tested: basic IPsec tests
    
    Conflicts: \
     - net/ipv6/xfrm6_output.c, context, already backported 93efac3f2e03
     - include/net/xfrm.h, context
       - already backported commits d511337a1eda and 2f32b51b609f
       - missing 573ce1c11b0d
     - use RH_KABI_EXTEND in xfrm_state_afinfo. it's internal only but changing
       this struct breaks kabi through a chain of pointers.
    
    commit 628e341f319f1a64a4639088faba952e4ec8f0a8
    Author: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Date:   Wed Aug 14 13:05:23 2013 +0200
    
        xfrm: make local error reporting more robust
    
        In xfrm4 and xfrm6 we need to take care about sockets of the other
        address family. This could happen because a 6in4 or 4in6 tunnel could
        get protected by ipsec.
    
        Because we don't want to have a run-time dependency on ipv6 when only
        using ipv4 xfrm we have to embed a pointer to the correct local_error
        function in xfrm_state_afinet and look it up when returning an error
        depending on the socket address family.
    
        Thanks to vi0ss for the great bug report:
        <https://bugzilla.kernel.org/show_bug.cgi?id=58691>
    
        v2:
        a) fix two more unsafe interpretations of skb->sk as ipv6 socket
           (xfrm6_local_dontfrag and __xfrm6_output)
        v3:
        a) add an EXPORT_SYMBOL_GPL(xfrm_local_error) to fix a link error when
           building ipv6 as a module (thanks to Steffen Klassert)
    
        Reported-by: <vi0oss@gmail.com>
        Cc: Steffen Klassert <steffen.klassert@secunet.com>
        Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    
    Signed-off-by: Sabrina Dubroca <sdubroca@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -37,9 +37,6 @@
 
 static unsigned int xfrm_state_hashmax __read_mostly = 1 * 1024 * 1024;
 
-static struct xfrm_state_afinfo *xfrm_state_get_afinfo(unsigned int family);
-static void xfrm_state_put_afinfo(struct xfrm_state_afinfo *afinfo);
-
 static inline unsigned int xfrm_dst_hash(struct net *net,
 					 const xfrm_address_t *daddr,
 					 const xfrm_address_t *saddr,
@@ -1952,7 +1949,7 @@ int xfrm_state_unregister_afinfo(struct xfrm_state_afinfo *afinfo)
 }
 EXPORT_SYMBOL(xfrm_state_unregister_afinfo);
 
-static struct xfrm_state_afinfo *xfrm_state_get_afinfo(unsigned int family)
+struct xfrm_state_afinfo *xfrm_state_get_afinfo(unsigned int family)
 {
 	struct xfrm_state_afinfo *afinfo;
 	if (unlikely(family >= NPROTO))
@@ -1964,7 +1961,7 @@ static struct xfrm_state_afinfo *xfrm_state_get_afinfo(unsigned int family)
 	return afinfo;
 }
 
-static void xfrm_state_put_afinfo(struct xfrm_state_afinfo *afinfo)
+void xfrm_state_put_afinfo(struct xfrm_state_afinfo *afinfo)
 {
 	rcu_read_unlock();
 }
