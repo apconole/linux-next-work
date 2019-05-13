bbab7d138474c1815bd3e0790a54b1ad33dbfb69
Vitaly Kuznetsov <vkuznets@redhat.com>
2019-05-13 16:57:09 -0400

    [mm] tlb, x86/mm: Support invalidating TLB caches for RCU_TABLE_FREE
    Message-id: <20190513165711.3629-2-vkuznets@redhat.com>
    Patchwork-id: 255104
    O-Subject: [RHEL7.7 PATCH 1/3] mm/tlb, x86/mm: Support invalidating TLB caches for RCU_TABLE_FREE
    Bugzilla: 1448188
    RH-Acked-by: Andrea Arcangeli <aarcange@redhat.com>
    RH-Acked-by: Aaron Tomlin <atomlin@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    commit d86564a2f085b79ec046a5cba90188e612352806
    Author: Peter Zijlstra <peterz@infradead.org>
    Date:   Wed Aug 22 17:30:15 2018 +0200
    
        mm/tlb, x86/mm: Support invalidating TLB caches for RCU_TABLE_FREE
    
        Jann reported that x86 was missing required TLB invalidates when he
        hit the !*batch slow path in tlb_remove_table().
    
        This is indeed the case; RCU_TABLE_FREE does not provide TLB (cache)
        invalidates, the PowerPC-hash where this code originated and the
        Sparc-hash where this was subsequently used did not need that. ARM
        which later used this put an explicit TLB invalidate in their
        __p*_free_tlb() functions, and PowerPC-radix followed that example.
    
        But when we hooked up x86 we failed to consider this. Fix this by
        (optionally) hooking tlb_remove_table() into the TLB invalidate code.
    
        NOTE: s390 was also needing something like this and might now
              be able to use the generic code again.
    
        [ Modified to be on top of Nick's cleanups, which simplified this patch
          now that tlb_flush_mmu_tlbonly() really only flushes the TLB - Linus ]
    
        Fixes: 9e52fc2b50de ("x86/mm: Enable RCU based page table freeing (CONFIG_HAVE_RCU_TABLE_FREE=y)")
        Reported-by: Jann Horn <jannh@google.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Acked-by: Rik van Riel <riel@surriel.com>
        Cc: Nicholas Piggin <npiggin@gmail.com>
        Cc: David Miller <davem@davemloft.net>
        Cc: Will Deacon <will.deacon@arm.com>
        Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
        Cc: Michael Ellerman <mpe@ellerman.id.au>
        Cc: stable@kernel.org
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    	arch/x86/Kconfig (context)
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- mm/memory.c
+++ mm/memory.c
@@ -327,6 +327,21 @@ int __tlb_remove_page(struct mmu_gather *tlb, struct page *page)
  * See the comment near struct mmu_table_batch.
  */
 
+/*
+ * If we want tlb_remove_table() to imply TLB invalidates.
+ */
+static inline void tlb_table_invalidate(struct mmu_gather *tlb)
+{
+#ifdef CONFIG_HAVE_RCU_TABLE_INVALIDATE
+	/*
+	 * Invalidate page-table caches used by hardware walkers. Then we still
+	 * need to RCU-sched wait while freeing the pages because software
+	 * walkers can still be in-flight.
+	 */
+	tlb_flush_mmu_tlbonly(tlb);
+#endif
+}
+
 static void tlb_remove_table_smp_sync(void *arg)
 {
 	/* Simply deliver the interrupt */
@@ -363,6 +378,7 @@ void tlb_table_flush(struct mmu_gather *tlb)
 	struct mmu_table_batch **batch = &tlb->batch;
 
 	if (*batch) {
+		tlb_table_invalidate(tlb);
 		call_rcu_sched(&(*batch)->rcu, tlb_remove_table_rcu);
 		*batch = NULL;
 	}
@@ -384,11 +400,13 @@ void tlb_remove_table(struct mmu_gather *tlb, void *table)
 	if (*batch == NULL) {
 		*batch = (struct mmu_table_batch *)__get_free_page(GFP_NOWAIT | __GFP_NOWARN);
 		if (*batch == NULL) {
+			tlb_table_invalidate(tlb);
 			tlb_remove_table_one(table);
 			return;
 		}
 		(*batch)->nr = 0;
 	}
+
 	(*batch)->tables[(*batch)->nr++] = table;
 	if ((*batch)->nr == MAX_TABLE_BATCH)
 		tlb_table_flush(tlb);
