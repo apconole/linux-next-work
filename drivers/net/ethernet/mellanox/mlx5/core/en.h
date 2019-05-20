8d02508455659246896a38e38680b641280c4192
Alaa Hleihel <ahleihel@redhat.com>
2019-05-20 06:53:01 -0400

    [netdrv] mlx5e: Turn on HW tunnel offload in all TIRs
    Message-id: <20190520065306.1236-13-ahleihel@redhat.com>
    Patchwork-id: 259063
    O-Subject: [RHEL7.7 BZ 1710423 12/17] net/mlx5e: Turn on HW tunnel offload in all TIRs
    Bugzilla: 1710423
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Don Dutile <ddutile@redhat.com>
    RH-Acked-by: Ivan Vecera <ivecera@redhat.com>
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1710423
    Upstream: net-next/master
    
    commit 69dad68d1bcf26dde3cc4b08b08c2260ae575ab6
    Author: Tariq Toukan <tariqt@mellanox.com>
    Date:   Sun Jan 20 11:04:34 2019 +0200
    
        net/mlx5e: Turn on HW tunnel offload in all TIRs
    
        Hardware requires that all TIRs that steer traffic to the same RQ
        should share identical tunneled_offload_en value.
        For that, the tunneled_offload_en bit should be set/unset (according to
        the HW capability) for all TIRs', not only the ones dedicated for
        tunneled (inner) traffic.
    
        Fixes: 1b223dd39162 ("net/mlx5e: Fix checksum handling for non-stripped vlan packets")
        Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -245,6 +245,7 @@ struct mlx5e_params {
 	bool rx_cqe_compress_def;
 	struct net_dim_cq_moder rx_cq_moderation;
 	struct net_dim_cq_moder tx_cq_moderation;
+	bool tunneled_offload_en;
 	bool lro_en;
 	u32 lro_wqe_sz;
 	u8  tx_min_inline_mode;
