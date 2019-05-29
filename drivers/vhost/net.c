b6d3bae465b6bca7a6dc5d0c7f256aa175e2b877
Jason Wang <jasowang@redhat.com>
2019-05-29 09:02:05 -0400

    [vhost] vhost_net: fix possible infinite loop
    Message-id: <1559120526-62706-6-git-send-email-jasowang@redhat.com>
    Patchwork-id: 261165
    O-Subject: [RHEL7.7 PATCH 5/6] vhost_net: fix possible infinite loop
    Bugzilla: 1702943
    CVE: CVE-2019-3900
    RH-Acked-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    RH-Acked-by: Jens Freimann <jfreimann@redhat.com>
    
    Upstream: in mst's vhost tree
    Notes: conflict since we're lacking:
    - 0d20bdf34dc7 ("vhost_net: split out datacopy logic") and tx batching
    - be294a51adfc ("vhost_net: Avoid rx queue wake-ups during busypoll")
    - 8241a1e466cd ("vhost_net: stop polling socket during rx processing")
    
    When the rx buffer is too small for a packet, we will discard the vq
    descriptor and retry it for the next packet:
    
    while ((sock_len = vhost_net_rx_peek_head_len(net, sock->sk,
    					      &busyloop_intr))) {
    ...
    	/* On overrun, truncate and discard */
    	if (unlikely(headcount > UIO_MAXIOV)) {
    		iov_iter_init(&msg.msg_iter, READ, vq->iov, 1, 1);
    		err = sock->ops->recvmsg(sock, &msg,
    					 1, MSG_DONTWAIT | MSG_TRUNC);
    		pr_debug("Discarded rx packet: len %zd\n", sock_len);
    		continue;
    	}
    ...
    }
    
    This makes it possible to trigger a infinite while..continue loop
    through the co-opreation of two VMs like:
    
    1) Malicious VM1 allocate 1 byte rx buffer and try to slow down the
       vhost process as much as possible e.g using indirect descriptors or
       other.
    2) Malicious VM2 generate packets to VM1 as fast as possible
    
    Fixing this by checking against weight at the end of RX and TX
    loop. This also eliminate other similar cases when:
    
    - userspace is consuming the packets in the meanwhile
    - theoretical TOCTOU attack if guest moving avail index back and forth
      to hit the continue after vhost find guest just add new buffers
    
    This addresses CVE-2019-3900.
    
    Fixes: d8316f3991d20 ("vhost: fix total length when packets are too short")
    Fixes: 3a4d5c94e9593 ("vhost_net: a kernel-level virtio server")
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit e2412c07f8f3040593dfb88207865a3cd58680c0)
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Bruno E. O. Meneguele <bmeneg@redhat.com>
    
    Conflicts:
    	drivers/vhost/net.c

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -497,7 +497,7 @@ static void handle_tx(struct vhost_net *net)
 	hdr_size = nvq->vhost_hlen;
 	zcopy = nvq->ubufs;
 
-	for (;;) {
+	do {
 		/* Release DMAs done buffers first */
 		if (zcopy)
 			vhost_zerocopy_signal_used(net, vq);
@@ -599,10 +599,8 @@ static void handle_tx(struct vhost_net *net)
 		else
 			vhost_zerocopy_signal_used(net, vq);
 		vhost_net_tx_packet(net);
-		if (unlikely(vhost_exceeds_weight(vq, ++sent_pkts,
-						  total_len)))
-			break;
-	}
+	} while (likely(!vhost_exceeds_weight(vq, ++sent_pkts, total_len)));
+
 out:
 	mutex_unlock(&vq->mutex);
 }
@@ -812,7 +810,10 @@ static void handle_rx(struct vhost_net *net)
 		vq->log : NULL;
 	mergeable = vhost_has_feature(vq, VIRTIO_NET_F_MRG_RXBUF);
 
-	while ((sock_len = vhost_net_rx_peek_head_len(net, sock->sk))) {
+	do {
+		sock_len = vhost_net_rx_peek_head_len(net, sock->sk);
+		if (!sock_len)
+			break;
 		sock_len += sock_hlen;
 		vhost_len = sock_len + vhost_hlen;
 		headcount = get_rx_bufs(vq, vq->heads + nvq->done_idx,
@@ -888,9 +889,8 @@ static void handle_rx(struct vhost_net *net)
 			vhost_log_write(vq, vq_log, log, vhost_len,
 					vq->iov, in);
 		total_len += vhost_len;
-		if (unlikely(vhost_exceeds_weight(vq, ++recv_pkts, total_len)))
-			goto out;
-	}
+	} while (likely(!vhost_exceeds_weight(vq, ++recv_pkts, total_len)));
+
 out:
 	vhost_rx_signal_used(nvq);
 	mutex_unlock(&vq->mutex);
@@ -979,7 +979,7 @@ static int vhost_net_open(struct inode *inode, struct file *f)
 	}
 	r = vhost_dev_init(dev, vqs, VHOST_NET_VQ_MAX,
 			   UIO_MAXIOV + VHOST_RX_BATCH,
-			   VHOST_NET_WEIGHT, VHOST_NET_PKT_WEIGHT);
+			   VHOST_NET_PKT_WEIGHT, VHOST_NET_WEIGHT);
 	if (r < 0) {
 		kfree(n);
 		kfree(vqs);
