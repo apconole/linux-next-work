fa95171c433916b519d8a7d0a066098c1ecc51a1
Abhijith Das <adas@redhat.com>
2019-05-30 20:33:03 -0400

    [fs] gfs2: time journal recovery steps accurately
    Message-id: <20190530203308.48540-3-adas@redhat.com>
    Patchwork-id: 261338
    O-Subject: [RHEL7 fs PATCH v2 2/7] gfs2: time journal recovery steps accurately
    Bugzilla: 1655575
    RH-Acked-by: Robert S Peterson <rpeterso@redhat.com>
    RH-Acked-by: Andreas Grunbacher <agruenba@redhat.com>
    
    Bugzilla: 1655575
    Upstream Status: https://git.kernel.org/pub/scm/linux/kernel/git/gfs2/linux-gfs2.git/commit/?h=for-next&id=5e86d9d122d0d6fae00d9dff41c22d6f4d09f566
    Build Info:  https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=21929139
    Tested: On host-047.virt.msp using xfstests
    
    commit 5e86d9d122d0d6fae00d9dff41c22d6f4d09f566
    Author: Abhi Das <adas@redhat.com>
    Date:   Thu Mar 29 10:41:27 2018 -0700
    
        gfs2: time journal recovery steps accurately
    
        This patch spits out the time taken by the various steps in the
        journal recover process. Previously, the journal recovery time
        didn't account for finding the journal head in the log which takes
        up a significant portion of time.
    
        Signed-off-by: Abhi Das <adas@redhat.com>
        Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    
    Signed-off-by: Abhi Das <adas@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/gfs2/recovery.c
+++ fs/gfs2/recovery.c
@@ -14,6 +14,7 @@
 #include <linux/buffer_head.h>
 #include <linux/gfs2_ondisk.h>
 #include <linux/crc32.h>
+#include <linux/ktime.h>
 
 #include "gfs2.h"
 #include "incore.h"
@@ -455,12 +456,13 @@ void gfs2_recover_func(struct work_struct *work)
 	struct gfs2_sbd *sdp = GFS2_SB(jd->jd_inode);
 	struct gfs2_log_header_host head;
 	struct gfs2_holder j_gh, ji_gh, t_gh;
-	unsigned long t;
+	ktime_t t_start, t_jlck, t_jhd, t_tlck, t_rep;
 	int ro = 0;
 	unsigned int pass;
 	int error = 0;
 	int jlocked = 0;
 
+	t_start = ktime_get();
 	if (sdp->sd_args.ar_spectator)
 		goto fail;
 	if (jd->jd_jid != sdp->sd_lockstruct.ls_jid) {
@@ -493,6 +495,7 @@ void gfs2_recover_func(struct work_struct *work)
 		fs_info(sdp, "jid=%u, already locked for use\n", jd->jd_jid);
 	}
 
+	t_jlck = ktime_get();
 	fs_info(sdp, "jid=%u: Looking at journal...\n", jd->jd_jid);
 
 	error = gfs2_jdesc_check(jd);
@@ -502,12 +505,11 @@ void gfs2_recover_func(struct work_struct *work)
 	error = gfs2_find_jhead(jd, &head);
 	if (error)
 		goto fail_gunlock_ji;
+	t_jhd = ktime_get();
 
 	if (!(head.lh_flags & GFS2_LOG_HEAD_UNMOUNT)) {
 		fs_info(sdp, "jid=%u: Acquiring the transaction lock...\n",
 			jd->jd_jid);
-
-		t = jiffies;
 
 		/* Acquire a shared hold on the transaction lock */
 
@@ -542,6 +544,7 @@ void gfs2_recover_func(struct work_struct *work)
 			goto fail_gunlock_tr;
 		}
 
+		t_tlck = ktime_get();
 		fs_info(sdp, "jid=%u: Replaying journal...\n", jd->jd_jid);
 
 		for (pass = 0; pass < 2; pass++) {
@@ -558,9 +561,14 @@ void gfs2_recover_func(struct work_struct *work)
 			goto fail_gunlock_tr;
 
 		gfs2_glock_dq_uninit(&t_gh);
-		t = DIV_ROUND_UP(jiffies - t, HZ);
-		fs_info(sdp, "jid=%u: Journal replayed in %lus\n",
-			jd->jd_jid, t);
+		t_rep = ktime_get();
+		fs_info(sdp, "jid=%u: Journal replayed in %lldms [jlck:%lldms, "
+			"jhead:%lldms, tlck:%lldms, replay:%lldms]\n",
+			jd->jd_jid, ktime_ms_delta(t_rep, t_start),
+			ktime_ms_delta(t_jlck, t_start),
+			ktime_ms_delta(t_jhd, t_jlck),
+			ktime_ms_delta(t_tlck, t_jhd),
+			ktime_ms_delta(t_rep, t_tlck));
 	}
 
 	gfs2_recovery_done(sdp, jd->jd_jid, LM_RD_SUCCESS);
