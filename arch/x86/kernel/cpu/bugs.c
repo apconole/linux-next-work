4f80c6532959bbfbd1a0099639786b98dfc1de18
Waiman Long <longman@redhat.com>
2019-08-06 19:45:39 -0400

    [x86] x86/speculation/swapgs: Exclude ATOMs from speculation through SWAPGS
    Message-id: <20190806194541.29606-5-longman@redhat.com>
    Patchwork-id: 268357
    O-Subject: [RHEL7.8 PATCH 4/6] x86/speculation/swapgs: Exclude ATOMs from speculation through SWAPGS
    Bugzilla: 1724510
    RH-Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    RH-Acked-by: Jan Stancek <jstancek@redhat.com>
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1724510
    Upstream Status: commit f36cf386e3fec258a341d446915862eded3e13d8
    
    Conflicts:
    There are merge conflicts in:
     - bugs.c: due to code difference because of dynamic PTI in RHEL7.
     - common.c: due to missing commit 1a576b23d637 ("x86/bugs: Add Hygon
    	     Dhyana to the respective mitigation machinery") and its
    	     derivatives.
    
    commit f36cf386e3fec258a341d446915862eded3e13d8
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Wed Jul 17 21:18:59 2019 +0200
    
        x86/speculation/swapgs: Exclude ATOMs from speculation through SWAPGS
    
        Intel provided the following information:
    
         On all current Atom processors, instructions that use a segment register
         value (e.g. a load or store) will not speculatively execute before the
         last writer of that segment retires. Thus they will not use a
         speculatively written segment value.
    
        That means on ATOMs there is no speculation through SWAPGS, so the SWAPGS
        entry paths can be excluded from the extra LFENCE if PTI is disabled.
    
        Create a separate bug flag for the through SWAPGS speculation and mark all
        out-of-order ATOMs and AMD/HYGON CPUs as not affected. The in-order ATOMs
        are excluded from the whole mitigation mess anyway.
    
        Reported-by: Andrew Cooper <andrew.cooper3@citrix.com>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Reviewed-by: Tyler Hicks <tyhicks@canonical.com>
        Reviewed-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -232,18 +232,6 @@ static const char * const spectre_v1_strings[] = {
 	[SPECTRE_V1_MITIGATION_AUTO] = "Mitigation: Load fences, usercopy/swapgs barriers and __user pointer sanitization",
 };
 
-static bool is_swapgs_serializing(void)
-{
-	/*
-	 * Technically, swapgs isn't serializing on AMD (despite it previously
-	 * being documented as such in the APM).  But according to AMD, %gs is
-	 * updated non-speculatively, and the issuing of %gs-relative memory
-	 * operands will be blocked until the %gs update completes, which is
-	 * good enough for our purposes.
-	 */
-	return boot_cpu_data.x86_vendor == X86_VENDOR_AMD;
-}
-
 /*
  * Does SMAP provide full mitigation against speculative kernel access to
  * userspace?
@@ -291,9 +279,10 @@ static void __init spectre_v1_select_mitigation(void)
 		if (!smap_works_speculatively()) {
 			/*
 			 * Mitigation can be provided from SWAPGS itself if
-			 * it is serializing. If not, mitigate with an LFENCE.
+			 * it is serializing. If not, mitigate with an LFENCE
+			 * to stop speculation through swapgs.
 			 */
-			if (!is_swapgs_serializing())
+			if (boot_cpu_has_bug(X86_BUG_SWAPGS))
 				setup_force_cpu_cap(X86_FEATURE_FENCE_SWAPGS_USER);
 
 			/*
