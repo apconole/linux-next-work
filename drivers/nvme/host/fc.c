bc78005a33ecc161fbbadbab737e4f7419e2082c
Ewan Milne <emilne@redhat.com>
2019-08-05 20:21:16 -0400

    [scsi] scsi: scsi_transport_fc: nvme: display FC-NVMe port roles
    Message-id: <20190805202117.30029-51-emilne@redhat.com>
    Patchwork-id: 268178
    O-Subject: [RHEL7.8 PATCH e-stor v2 50/51] scsi: scsi_transport_fc: nvme: display FC-NVMe port roles
    Bugzilla: 1737602
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    
    commit a6a6d0589ac4ff3e1eb962f6c53e67d8f6386c18
    Author: Hannes Reinecke <hare@suse.de>
    Date:   Wed Apr 10 16:16:19 2019 +0200
    
        scsi: scsi_transport_fc: nvme: display FC-NVMe port roles
    
        Currently the FC-NVMe driver is leverating the SCSI FC transport class to
        access the remote ports. Which means that all FC-NVMe remote ports will be
        visible to the fc transport layer, but due to missing definitions the port
        roles will always be 'unknown'.  This patch adds the missing definitions to
        the fc transport class to that the port roles are correctly displayed.
    
        Signed-off-by: Hannes Reinecke <hare@suse.de>
        Reviewed-by: James Smart <james.smart@broadcom.com>
        Reviewed-by: Ewan D. Milne <emilne@redhat.com>
        Reviewed-by: Giridhar Malavali <gmalavali@marvell.com>
        Reviewed-by: Himanshu Madhani <hmadhani@marvell.com>
        Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    (Modified for RHEL7 -- RHEL7 requires #include <scsi/scsi_device.h> in
     drivers/nvme/host/fc.c because it does not have other upstream changes)
    
    Signed-off-by: Ewan D. Milne <emilne@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/nvme/host/fc.c
+++ drivers/nvme/host/fc.c
@@ -25,7 +25,8 @@
 #include "fabrics.h"
 #include <linux/nvme-fc-driver.h>
 #include <linux/nvme-fc.h>
-
+#include <scsi/scsi_device.h>
+#include <scsi/scsi_transport_fc.h>
 
 /* *************************** Data Structures/Defines ****************** */
 
