7df51ca14fcbd36867aabcbdbcaa2ec3b23b23ea
Alaa Hleihel <ahleihel@redhat.com>
2019-08-08 15:54:42 -0400

    [netdrv] net/mlx5e: Use termination table for VLAN push actions
    Message-id: <20190808155519.15358-13-ahleihel@redhat.com>
    Patchwork-id: 268566
    O-Subject: [RHEL7.8 BZ 1737464 12/49] net/mlx5e: Use termination table for VLAN push actions
    Bugzilla: 1737464 1676558
    RH-Acked-by: Kamal Heib <kheib@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Tony Camuso <tcamuso@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1676558
    Bugzilla: http://bugzilla.redhat.com/1737464
    Upstream: v5.3-rc1
    Backport notes:
     - drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads_termtbl.c
       Due to missing commit:
       b05af6aacdb9 ("net/mlx5: E-Switch, Normalize the name of uplink vport number")
       ---> We have FDB_UPLINK_VPORT instead of MLX5_VPORT_UPLINK.
     - drivers/net/ethernet/mellanox/mlx5/core/Makefile
       Context diff due to missing patches:
       80f09dfc237f ("net/mlx5: Eswitch, enable RoCE loopback traffic")
       591905ba9679 ("net/mlx5: Introduce Mellanox SmartNIC and modify page management logic")
    
    commit 10caabdaad5ace85577a453da97d1f8d3b944427
    Author: Oz Shlomo <ozsh@mellanox.com>
    Date:   Thu Apr 18 16:45:29 2019 +0300
    
        net/mlx5e: Use termination table for VLAN push actions
    
        HW does not support push VLAN action in the RX direction (packets
        arriving from the wire). The FW works around this limitation by haripining
        the packet. The hairpin workaround applies only when the push VLAN action
        is specified in a termination table, assuring that there are no actions
        following the haripin.
    
        Instantiate termination table for push VLAN actions. Re-use identical
        terminating tables for increased HW cache efficiency.
    
        Signed-off-by: Oz Shlomo <ozsh@mellanox.com>
        Reviewed-by: Paul Blakey <paulb@mellanox.com>
        Reviewed-by: Eli Britstein <elibr@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -172,6 +172,8 @@ struct mlx5_esw_offload {
 	struct mutex peer_mutex;
 	DECLARE_HASHTABLE(encap_tbl, 8);
 	DECLARE_HASHTABLE(mod_hdr_tbl, 8);
+	DECLARE_HASHTABLE(termtbl_tbl, 8);
+	struct mutex termtbl_mutex; /* protects termtbl hash */
 	u8 inline_mode;
 	u64 num_flows;
 	u8 encap;
@@ -252,6 +254,25 @@ void mlx5_eswitch_del_send_to_vport_rule(struct mlx5_flow_handle *rule);
 
 struct mlx5_flow_spec;
 struct mlx5_esw_flow_attr;
+struct mlx5_termtbl_handle;
+
+bool
+mlx5_eswitch_termtbl_required(struct mlx5_eswitch *esw,
+			      struct mlx5_flow_act *flow_act,
+			      struct mlx5_flow_spec *spec);
+
+struct mlx5_flow_handle *
+mlx5_eswitch_add_termtbl_rule(struct mlx5_eswitch *esw,
+			      struct mlx5_flow_table *ft,
+			      struct mlx5_flow_spec *spec,
+			      struct mlx5_esw_flow_attr *attr,
+			      struct mlx5_flow_act *flow_act,
+			      struct mlx5_flow_destination *dest,
+			      int num_dest);
+
+void
+mlx5_eswitch_termtbl_put(struct mlx5_eswitch *esw,
+			 struct mlx5_termtbl_handle *tt);
 
 struct mlx5_flow_handle *
 mlx5_eswitch_add_offloaded_rule(struct mlx5_eswitch *esw,
@@ -322,6 +343,7 @@ struct mlx5_esw_flow_attr {
 		struct mlx5_eswitch_rep *rep;
 		struct mlx5_core_dev *mdev;
 		u32 encap_id;
+		struct mlx5_termtbl_handle *termtbl;
 	} dests[MLX5_MAX_FLOW_FWD_VPORTS];
 	u32	mod_hdr_id;
 	u8	match_level;
