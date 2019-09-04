3b22b377233e53a0577bc03fcfb1d794b96a426e
Phil Auld <pauld@redhat.com>
2019-09-04 14:58:12 -0400

    [kernel] sched/core: Use TASK_ON_RQ_MIGRATING in __migrate_swap_task
    Message-id: <20190904145811.GF26720@pauld.bos.csb>
    Patchwork-id: 270913
    O-Subject: Re: [RHEL7.8 v2 12/12] sched/core: Use TASK_ON_RQ_MIGRATING in __migrate_swap_task
    Bugzilla: 1616140
    RH-Acked-by: Juri Lelli <juri.lelli@redhat.com>
    RH-Acked-by: Waiman Long <longman@redhat.com>
    
    BugZilla: https://bugzilla.redhat.com/show_bug.cgi?id=1616140
    BrewBuild: https://brewweb.devel.redhat.com/taskinfo?taskID=23207618
    Upstream Status: RHEL only
    Tested: With reproducer, running migration intensive tests with deadline policy on NUMA system.
        Ran kt1, stress/ltp and some local scheduler sanity and stress tests.
    
    Notes:  Cherry picked one hunk of upstream commit 3ea94de15ce9 (sched/core: Fix
           	incorrect wait time and wait count statistics) to set TASK_ON_RQ_MIGRATING
           	in __migrate_swap_task. RHEL commit 0a80d9a4b35 already pulled in
           	part of this patch. The other hunks either require more patches not in
           	the tree yet or are ineffective without the rest. This allows us to pull
    	in the rest as the proper commit later if needed.
    
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1159,9 +1159,11 @@ static void __migrate_swap_task(struct task_struct *p, int cpu)
 		src_rq = task_rq(p);
 		dst_rq = cpu_rq(cpu);
 
+		p->on_rq = TASK_ON_RQ_MIGRATING;
 		deactivate_task(src_rq, p, 0);
 		set_task_cpu(p, cpu);
 		activate_task(dst_rq, p, 0);
+		p->on_rq = TASK_ON_RQ_QUEUED;
 		check_preempt_curr(dst_rq, p, 0);
 	} else {
 		/*
