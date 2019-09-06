6d3984637995667a5cd7addfeafb7191a36a0b34
Florian Westphal <fwestpha@redhat.com>
2019-09-06 15:37:50 -0400

    [net] netfilter: ctnetlink: fix deadlock due to acquire _expect_lock twice
    Message-id: <20190906153802.9861-8-fwestpha@redhat.com>
    Patchwork-id: 271094
    O-Subject: [RHEL7.8 net 07/19] netfilter: ctnetlink: fix deadlock due to acquire _expect_lock twice
    Bugzilla: 1749777
    RH-Acked-by: Stefano Brivio <sbrivio@redhat.com>
    RH-Acked-by: Phil Sutter <psutter@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1749777
    Upstream Status: mainline
    Conflicts: net/netfilter/nf_conntrack_netlink.c
    
    Upstream renamed ctnetlink_nfqueue_parse to ctnetlink_glue_parse.
    No other changes.
    
    commit 88be4c09d9008f9ff337cbf48c5d0f06c8f872e7
    Author: Liping Zhang <zlpnobody@gmail.com>
    Date:   Mon Apr 17 21:18:56 2017 +0800
    
        netfilter: ctnetlink: fix deadlock due to acquire _expect_lock twice
    
        Currently, ctnetlink_change_conntrack is always protected by _expect_lock,
        but this will cause a deadlock when deleting the helper from a conntrack,
        as the _expect_lock will be acquired again by nf_ct_remove_expectations:
    
                 CPU0
                ----
          lock(nf_conntrack_expect_lock);
          lock(nf_conntrack_expect_lock);
    
          *** DEADLOCK ***
          May be due to missing lock nesting notation
    
          2 locks held by lt-conntrack_gr/12853:
          #0:  (&table[i].mutex){+.+.+.}, at: [<ffffffffa05e2009>]
               nfnetlink_rcv_msg+0x399/0x6a9 [nfnetlink]
          #1:  (nf_conntrack_expect_lock){+.....}, at: [<ffffffffa05f2c1f>]
               ctnetlink_new_conntrack+0x17f/0x408 [nf_conntrack_netlink]
    
          Call Trace:
           dump_stack+0x85/0xc2
           __lock_acquire+0x1608/0x1680
           ? ctnetlink_parse_tuple_proto+0x10f/0x1c0 [nf_conntrack_netlink]
           lock_acquire+0x100/0x1f0
           ? nf_ct_remove_expectations+0x32/0x90 [nf_conntrack]
           _raw_spin_lock_bh+0x3f/0x50
           ? nf_ct_remove_expectations+0x32/0x90 [nf_conntrack]
           nf_ct_remove_expectations+0x32/0x90 [nf_conntrack]
           ctnetlink_change_helper+0xc6/0x190 [nf_conntrack_netlink]
           ctnetlink_new_conntrack+0x1b2/0x408 [nf_conntrack_netlink]
           nfnetlink_rcv_msg+0x60a/0x6a9 [nfnetlink]
           ? nfnetlink_rcv_msg+0x1b9/0x6a9 [nfnetlink]
           ? nfnetlink_bind+0x1a0/0x1a0 [nfnetlink]
           netlink_rcv_skb+0xa4/0xc0
           nfnetlink_rcv+0x87/0x770 [nfnetlink]
    
        Since the operations are unrelated to nf_ct_expect, so we can drop the
        _expect_lock. Also note, after removing the _expect_lock protection,
        another CPU may invoke nf_conntrack_helper_unregister, so we should
        use rcu_read_lock to protect __nf_conntrack_helper_find invoked by
        ctnetlink_change_helper.
    
        Fixes: ca7433df3a67 ("netfilter: conntrack: seperate expect locking from nf_conntrack_lock")
        Signed-off-by: Liping Zhang <zlpnobody@gmail.com>
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>
    Signed-off-by: Jiri Benc <jbenc@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- net/netfilter/nf_conntrack_netlink.c
+++ net/netfilter/nf_conntrack_netlink.c
@@ -1457,23 +1457,29 @@ ctnetlink_change_helper(struct nf_conn *ct, const struct nlattr * const cda[])
 		return 0;
 	}
 
+	rcu_read_lock();
 	helper = __nf_conntrack_helper_find(helpname, nf_ct_l3num(ct),
 					    nf_ct_protonum(ct));
-	if (helper == NULL)
+	if (helper == NULL) {
+		rcu_read_unlock();
 		return -EOPNOTSUPP;
+	}
 
 	if (help) {
 		if (help->helper == helper) {
 			/* update private helper data if allowed. */
 			if (helper->from_nlattr)
 				helper->from_nlattr(helpinfo, ct);
-			return 0;
+			err = 0;
 		} else
-			return -EBUSY;
+			err = -EBUSY;
+	} else {
+		/* we cannot set a helper for an existing conntrack */
+		err = -EOPNOTSUPP;
 	}
 
-	/* we cannot set a helper for an existing conntrack */
-	return -EOPNOTSUPP;
+	rcu_read_unlock();
+	return err;
 }
 
 static inline int
@@ -1893,9 +1899,7 @@ ctnetlink_new_conntrack(struct sock *ctnl, struct sk_buff *skb,
 	err = -EEXIST;
 	ct = nf_ct_tuplehash_to_ctrack(h);
 	if (!(nlh->nlmsg_flags & NLM_F_EXCL)) {
-		spin_lock_bh(&nf_conntrack_expect_lock);
 		err = ctnetlink_change_conntrack(ct, cda);
-		spin_unlock_bh(&nf_conntrack_expect_lock);
 		if (err == 0) {
 			nf_conntrack_eventmask_report((1 << IPCT_REPLY) |
 						      (1 << IPCT_ASSURED) |
@@ -2227,11 +2231,7 @@ ctnetlink_nfqueue_parse(const struct nlattr *attr, struct nf_conn *ct)
 	if (ret < 0)
 		return ret;
 
-	spin_lock_bh(&nf_conntrack_expect_lock);
-	ret = ctnetlink_nfqueue_parse_ct((const struct nlattr **)cda, ct);
-	spin_unlock_bh(&nf_conntrack_expect_lock);
-
-	return ret;
+	return ctnetlink_nfqueue_parse_ct((const struct nlattr **)cda, ct);
 }
 
 static int ctnetlink_nfqueue_exp_parse(const struct nlattr * const *cda,
