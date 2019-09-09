e5b1c99308ec4591132e42ba86ac12b8647b8426
Eugenio Pérez <eperezma@redhat.com>
2019-09-09 19:03:40 -0400

    [net] Introduce generic failover module
    Message-id: <20190909190347.31516-2-eperezma@redhat.com>
    Patchwork-id: 271149
    O-Subject: [PATCH RHEL7.8 v5 1/8] net: Introduce generic failover module
    Bugzilla: 1589888
    RH-Acked-by: Jason Wang <jasowang@redhat.com>
    RH-Acked-by: Jens Freimann <jfreimann@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: Sridhar Samudrala <sridhar.samudrala@intel.com>
    
    The failover module provides a generic interface for paravirtual drivers
    to register a netdev and a set of ops with a failover instance. The ops
    are used as event handlers that get called to handle netdev register/
    unregister/link change/name change events on slave pci ethernet devices
    with the same mac address as the failover netdev.
    
    This enables paravirtual drivers to use a VF as an accelerated low latency
    datapath. It also allows migration of VMs with direct attached VFs by
    failing over to the paravirtual datapath when the VF is unplugged.
    
    Signed-off-by: Sridhar Samudrala <sridhar.samudrala@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 30c8bd5aa8b2c78546c3e52337101b9c85879320)
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1590,6 +1590,8 @@ struct net_device_ops {
  * @IFF_PHONY_HEADROOM: the headroom value is controlled by an external
  *	entity (i.e. the master device for bridged veth)
  * @IFF_MACSEC: device is a MACsec device
+ * @IFF_FAILOVER: device is a failover master device
+ * @IFF_FAILOVER_SLAVE: device is lower dev of a failover master device
  */
 enum netdev_priv_flags {
 	IFF_802_1Q_VLAN			= 1<<0,
@@ -1621,6 +1623,8 @@ enum netdev_priv_flags {
 	IFF_NO_QUEUE			= 1<<26,
 	IFF_PHONY_HEADROOM		= 1<<27,
 	IFF_MACSEC			= 1<<28,
+	IFF_FAILOVER			= 1<<27,
+	IFF_FAILOVER_SLAVE		= 1<<28,
 };
 
 #define IFF_802_1Q_VLAN			IFF_802_1Q_VLAN
@@ -1651,6 +1655,8 @@ enum netdev_priv_flags {
 #define IFF_TEAM			IFF_TEAM
 #define IFF_RXFH_CONFIGURED		IFF_RXFH_CONFIGURED
 #define IFF_MACSEC			IFF_MACSEC
+#define IFF_FAILOVER			IFF_FAILOVER
+#define IFF_FAILOVER_SLAVE		IFF_FAILOVER_SLAVE
 
 /*
  *	The DEVICE structure.
@@ -4447,6 +4453,16 @@ static inline bool netif_is_lag_master(const struct net_device *dev)
 static inline bool netif_is_lag_port(const struct net_device *dev)
 {
 	return netif_is_bond_slave(dev) || netif_is_team_port(dev);
+}
+
+static inline bool netif_is_failover(const struct net_device *dev)
+{
+	return dev->priv_flags & IFF_FAILOVER;
+}
+
+static inline bool netif_is_failover_slave(const struct net_device *dev)
+{
+	return dev->priv_flags & IFF_FAILOVER_SLAVE;
 }
 
 /* This device needs to keep skb dst for qdisc enqueue or ndo_start_xmit() */
