ad015af46938b5efe056dec0d4c01454e8310123
Ondrej Mosnacek <omosnace@redhat.com>
2019-10-21 11:55:01 -0400

    [fs] libfs.c: new helper - next_positive()
    Message-id: <20191021115503.8279-4-omosnace@redhat.com>
    Patchwork-id: 280433
    O-Subject: [RHEL7.8 PATCH 3/5] libfs.c: new helper - next_positive()
    Bugzilla: 1510603
    RH-Acked-by: Vladis Dronov <vdronov@redhat.com>
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    
    BZ: 1510603
    Conflicts:
      - fs/libfs.c:
        - d_child vs. d_u.d_child fuzz
        - dcache_readdir() has old interface, needed manual merge (which was
          trivial, fortunately)
    
    commit 4f42c1b5b9c27b6228e6b9c57eee4beb3118b6b0
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Mon Jun 6 19:37:13 2016 -0400
    
        libfs.c: new helper - next_positive()
    
        Return nth positive child after given or NULL if there's
        less than n left.  dcache_readdir() and dcache_dir_lseek()
        switched to it.
    
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/libfs.c
+++ fs/libfs.c
@@ -88,6 +88,39 @@ int dcache_dir_close(struct inode *inode, struct file *file)
 }
 EXPORT_SYMBOL(dcache_dir_close);
 
+/* parent is locked at least shared */
+static struct dentry *next_positive(struct dentry *parent,
+				    struct list_head *from,
+				    int count)
+{
+	struct dentry *res = NULL;
+	struct list_head *p;
+
+	spin_lock(&parent->d_lock);
+	for (p = from->next; p != &parent->d_subdirs; p = p->next) {
+		struct dentry *d = list_entry(p, struct dentry, d_u.d_child);
+		if (simple_positive(d) && !--count) {
+			res = d;
+			break;
+		}
+	}
+	spin_unlock(&parent->d_lock);
+	return res;
+}
+
+static void move_cursor(struct dentry *cursor, struct list_head *after)
+{
+	struct dentry *parent = cursor->d_parent;
+
+	spin_lock(&parent->d_lock);
+	__list_del(cursor->d_u.d_child.prev, cursor->d_u.d_child.next);
+	if (after)
+		list_add(&cursor->d_u.d_child, after);
+	else
+		list_add_tail(&cursor->d_u.d_child, &parent->d_subdirs);
+	spin_unlock(&parent->d_lock);
+}
+
 loff_t dcache_dir_lseek(struct file *file, loff_t offset, int whence)
 {
 	struct dentry *dentry = file->f_path.dentry;
@@ -105,23 +138,12 @@ loff_t dcache_dir_lseek(struct file *file, loff_t offset, int whence)
 	if (offset != file->f_pos) {
 		file->f_pos = offset;
 		if (file->f_pos >= 2) {
-			struct list_head *p;
 			struct dentry *cursor = file->private_data;
+			struct dentry *to;
 			loff_t n = file->f_pos - 2;
 
-			spin_lock(&dentry->d_lock);
-			/* d_lock not required for cursor */
-			list_del(&cursor->d_u.d_child);
-			p = dentry->d_subdirs.next;
-			while (n && p != &dentry->d_subdirs) {
-				struct dentry *next;
-				next = list_entry(p, struct dentry, d_u.d_child);
-				if (simple_positive(next))
-					n--;
-				p = p->next;
-			}
-			list_add_tail(&cursor->d_u.d_child, p);
-			spin_unlock(&dentry->d_lock);
+			to = next_positive(dentry, &dentry->d_subdirs, n);
+			move_cursor(cursor, to ? &to->d_u.d_child : NULL);
 		}
 	}
 	mutex_unlock(&dentry->d_inode->i_mutex);
@@ -145,7 +167,9 @@ int dcache_readdir(struct file * filp, void * dirent, filldir_t filldir)
 {
 	struct dentry *dentry = filp->f_path.dentry;
 	struct dentry *cursor = filp->private_data;
-	struct list_head *p, *q = &cursor->d_u.d_child;
+	struct list_head *p = &cursor->d_u.d_child;
+	struct dentry *next;
+	bool moved = false;
 	ino_t ino;
 	int i = filp->f_pos;
 
@@ -165,29 +189,20 @@ int dcache_readdir(struct file * filp, void * dirent, filldir_t filldir)
 			i++;
 			/* fallthrough */
 		default:
-			spin_lock(&dentry->d_lock);
 			if (filp->f_pos == 2)
-				list_move(q, &dentry->d_subdirs);
-
-			for (p=q->next; p != &dentry->d_subdirs; p=p->next) {
-				struct dentry *next;
-				next = list_entry(p, struct dentry, d_u.d_child);
-				if (!simple_positive(next))
-					continue;
-
-				spin_unlock(&dentry->d_lock);
+				p = &dentry->d_subdirs;
+			while ((next = next_positive(dentry, p, 1)) != NULL) {
 				if (filldir(dirent, next->d_name.name, 
 					    next->d_name.len, filp->f_pos, 
 					    next->d_inode->i_ino, 
 					    dt_type(next->d_inode)) < 0)
-					return 0;
-				spin_lock(&dentry->d_lock);
-				/* next is still alive */
-				list_move(q, p);
-				p = q;
+					break;
+				moved = true;
+				p = &next->d_u.d_child;
 				filp->f_pos++;
 			}
-			spin_unlock(&dentry->d_lock);
+			if (moved)
+				move_cursor(cursor, p);
 	}
 	return 0;
 }
