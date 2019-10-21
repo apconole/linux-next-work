574fd7413e3ee29ab9916cb73118c23182c30581
Ondrej Mosnacek <omosnace@redhat.com>
2019-10-21 11:55:03 -0400

    [fs] Fix the locking in dcache_readdir() and friends
    Message-id: <20191021115503.8279-6-omosnace@redhat.com>
    Patchwork-id: 280431
    O-Subject: [RHEL7.8 PATCH 5/5] Fix the locking in dcache_readdir() and friends
    Bugzilla: 1510603
    RH-Acked-by: Vladis Dronov <vdronov@redhat.com>
    RH-Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    
    BZ: 1510603
    Conflicts:
      - fs/libfs.c:
        - d_child vs. d_u.d_child fuzz
        - dcache_readdir() has old interface, needed manual merge (which was
          trivial, fortunately)
    
    commit d4f4de5e5ef8efde85febb6876cd3c8ab1631999
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date:   Sun Sep 15 12:12:39 2019 -0400
    
        Fix the locking in dcache_readdir() and friends
    
        There are two problems in dcache_readdir() - one is that lockless traversal
        of the list needs non-trivial cooperation of d_alloc() (at least a switch
        to list_add_rcu(), and probably more than just that) and another is that
        it assumes that no removal will happen without the directory locked exclusive.
        Said assumption had always been there, never had been stated explicitly and
        is violated by several places in the kernel (devpts and selinuxfs).
    
                * replacement of next_positive() with different calling conventions:
        it returns struct list_head * instead of struct dentry *; the latter is
        passed in and out by reference, grabbing the result and dropping the original
        value.
                * scan is under ->d_lock.  If we run out of timeslice, cursor is moved
        after the last position we'd reached and we reschedule; then the scan continues
        from that place.  To avoid livelocks between multiple lseek() (with cursors
        getting moved past each other, never reaching the real entries) we always
        skip the cursors, need_resched() or not.
                * returned list_head * is either ->d_child of dentry we'd found or
        ->d_subdirs of parent (if we got to the end of the list).
                * dcache_readdir() and dcache_dir_lseek() switched to new helper.
        dcache_readdir() always holds a reference to dentry passed to dir_emit() now.
        Cursor is moved to just before the entry where dir_emit() has failed or into
        the very end of the list, if we'd run out.
                * move_cursor() eliminated - it had sucky calling conventions and
        after fixing that it became simply list_move() (in lseek and scan_positives)
        or list_move_tail() (in readdir).
    
                All operations with the list are under ->d_lock now, and we do not
        depend upon having all file removals done with parent locked exclusive
        anymore.
    
        Cc: stable@vger.kernel.org
        Reported-by: "zhengbin (A)" <zhengbin13@huawei.com>
        Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/libfs.c
+++ fs/libfs.c
@@ -87,36 +87,47 @@ int dcache_dir_close(struct inode *inode, struct file *file)
 EXPORT_SYMBOL(dcache_dir_close);
 
 /* parent is locked at least shared */
-static struct dentry *next_positive(struct dentry *parent,
-				    struct list_head *from,
-				    int count)
+/*
+ * Returns an element of siblings' list.
+ * We are looking for <count>th positive after <p>; if
+ * found, dentry is grabbed and passed to caller via *<res>.
+ * If no such element exists, the anchor of list is returned
+ * and *<res> is set to NULL.
+ */
+static struct list_head *scan_positives(struct dentry *cursor,
+					struct list_head *p,
+					loff_t count,
+					struct dentry **res)
 {
-	struct dentry *res = NULL;
-	struct list_head *p;
+	struct dentry *dentry = cursor->d_parent, *found = NULL;
 
-	spin_lock(&parent->d_lock);
-	for (p = from->next; p != &parent->d_subdirs; p = p->next) {
+	spin_lock(&dentry->d_lock);
+	while ((p = p->next) != &dentry->d_subdirs) {
 		struct dentry *d = list_entry(p, struct dentry, d_u.d_child);
+		// we must at least skip cursors, to avoid livelocks
+		if (d->d_flags & DCACHE_DENTRY_CURSOR)
+			continue;
 		if (simple_positive(d) && !--count) {
-			res = d;
-			break;
+			spin_lock_nested(&d->d_lock, DENTRY_D_LOCK_NESTED);
+			if (simple_positive(d))
+				found = dget_dlock(d);
+			spin_unlock(&d->d_lock);
+			if (likely(found))
+				break;
+			count = 1;
+		}
+		if (need_resched()) {
+			list_move(&cursor->d_u.d_child, p);
+			p = &cursor->d_u.d_child;
+			spin_unlock(&dentry->d_lock);
+			cond_resched();
+			spin_lock(&dentry->d_lock);
 		}
 	}
-	spin_unlock(&parent->d_lock);
-	return res;
-}
-
-static void move_cursor(struct dentry *cursor, struct list_head *after)
-{
-	struct dentry *parent = cursor->d_parent;
-
-	spin_lock(&parent->d_lock);
-	__list_del(cursor->d_u.d_child.prev, cursor->d_u.d_child.next);
-	if (after)
-		list_add(&cursor->d_u.d_child, after);
-	else
-		list_add_tail(&cursor->d_u.d_child, &parent->d_subdirs);
-	spin_unlock(&parent->d_lock);
+	spin_unlock(&dentry->d_lock);
+	dput(*res);
+	*res = found;
+	return p;
 }
 
 loff_t dcache_dir_lseek(struct file *file, loff_t offset, int whence)
@@ -134,15 +145,25 @@ loff_t dcache_dir_lseek(struct file *file, loff_t offset, int whence)
 			return -EINVAL;
 	}
 	if (offset != file->f_pos) {
-		file->f_pos = offset;
-		if (file->f_pos >= 2) {
-			struct dentry *cursor = file->private_data;
-			struct dentry *to;
-			loff_t n = file->f_pos - 2;
+		struct dentry *cursor = file->private_data;
+		struct dentry *to = NULL;
+		struct list_head *p;
 
-			to = next_positive(dentry, &dentry->d_subdirs, n);
-			move_cursor(cursor, to ? &to->d_u.d_child : NULL);
+		file->f_pos = offset;
+
+		if (file->f_pos > 2) {
+			p = scan_positives(cursor, &dentry->d_subdirs,
+					   file->f_pos - 2, &to);
+			spin_lock(&dentry->d_lock);
+			list_move(&cursor->d_u.d_child, p);
+			spin_unlock(&dentry->d_lock);
+		} else {
+			spin_lock(&dentry->d_lock);
+			list_del_init(&cursor->d_u.d_child);
+			spin_unlock(&dentry->d_lock);
 		}
+
+		dput(to);
 	}
 	mutex_unlock(&dentry->d_inode->i_mutex);
 	return offset;
@@ -165,9 +186,9 @@ int dcache_readdir(struct file * filp, void * dirent, filldir_t filldir)
 {
 	struct dentry *dentry = filp->f_path.dentry;
 	struct dentry *cursor = filp->private_data;
-	struct list_head *p = &cursor->d_u.d_child;
-	struct dentry *next;
-	bool moved = false;
+	struct list_head *anchor = &dentry->d_subdirs;
+	struct dentry *next = NULL;
+	struct list_head *p;
 	ino_t ino;
 	int i = filp->f_pos;
 
@@ -188,19 +209,22 @@ int dcache_readdir(struct file * filp, void * dirent, filldir_t filldir)
 			/* fallthrough */
 		default:
 			if (filp->f_pos == 2)
-				p = &dentry->d_subdirs;
-			while ((next = next_positive(dentry, p, 1)) != NULL) {
+				p = anchor;
+			else
+				p = &cursor->d_u.d_child;
+
+			while ((p = scan_positives(cursor, p, 1, &next)) != anchor) {
 				if (filldir(dirent, next->d_name.name, 
 					    next->d_name.len, filp->f_pos, 
 					    next->d_inode->i_ino, 
 					    dt_type(next->d_inode)) < 0)
 					break;
-				moved = true;
-				p = &next->d_u.d_child;
 				filp->f_pos++;
 			}
-			if (moved)
-				move_cursor(cursor, p);
+			spin_lock(&dentry->d_lock);
+			list_move_tail(&cursor->d_u.d_child, p);
+			spin_unlock(&dentry->d_lock);
+			dput(next);
 	}
 	return 0;
 }
