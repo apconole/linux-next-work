d9d67a6ffab9ef015df4b03d5ceb0a42327ec07d
Waiman Long <longman@redhat.com>
2019-11-21 21:54:33 -0500

    [base] x86/speculation/taa: Add sysfs reporting for TSX Async Abort
    Message-id: <20191121215441.29460-6-longman@redhat.com>
    Patchwork-id: 287890
    O-Subject: [RHEL7.8 PATCH v4 05/13] x86/speculation/taa: Add sysfs reporting for TSX Async Abort
    Bugzilla: 1766540
    CVE: CVE-2019-11135
    RH-Acked-by: Rafael Aquini <aquini@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Phil Auld <pauld@redhat.com>
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1766540
    CVE: CVE-2019-11135 (TSX Asynchronous Abort)
    Conflicts:
     1) There is a conflict in include/linux/cpu.h that requires manual merge
        because of the presence of IFU patch.
     2) There are merge conflict in drivers/base/cpu.c because of
        the presence of the IFU changes as well as the fact that RHEL7
        vulnerabilities files have protection 0400 instead of 0444 upstream.
     3) There are fuzzes in arch/x86/kernel/cpu/bugs.c because of the
        IFU patch.
     4) The TAA functions are put in front of IFU functions to align with
        current upstream IFU/TAA conflict resolution.
    
    commit 6608b45ac5ecb56f9e171252229c39580cc85f0f
    Author: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Date:   Wed Oct 23 12:19:51 2019 +0200
    
        x86/speculation/taa: Add sysfs reporting for TSX Async Abort
    
        Add the sysfs reporting file for TSX Async Abort. It exposes the
        vulnerability and the mitigation state similar to the existing files for
        the other hardware vulnerabilities.
    
        Sysfs file path is:
        /sys/devices/system/cpu/vulnerabilities/tsx_async_abort
    
        Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
        Signed-off-by: Borislav Petkov <bp@suse.de>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Tested-by: Neelima Krishnan <neelima.krishnan@intel.com>
        Reviewed-by: Mark Gross <mgross@linux.intel.com>
        Reviewed-by: Tony Luck <tony.luck@intel.com>
        Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Reviewed-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -50,6 +50,9 @@ extern ssize_t cpu_show_l1tf(struct device *dev,
 			     struct device_attribute *attr, char *buf);
 extern ssize_t cpu_show_mds(struct device *dev,
 			    struct device_attribute *attr, char *buf);
+extern ssize_t cpu_show_tsx_async_abort(struct device *dev,
+					struct device_attribute *attr,
+					char *buf);
 extern ssize_t cpu_show_itlb_multihit(struct device *dev,
 				      struct device_attribute *attr, char *buf);
 
