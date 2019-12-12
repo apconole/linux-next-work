c6249cff15cf70133caafe88b0ac4543e97550d3
Paolo Bonzini <pbonzini@redhat.com>
2019-12-12 16:05:37 -0500

    [x86] kvm: x86: Mark expected switch fall-throughs
    Message-id: <20191212160539.18670-6-pbonzini@redhat.com>
    Patchwork-id: 291951
    Patchwork-instance: patchwork
    O-Subject: [RHEL7.8 virt PATCH v2 5/7] KVM: x86: Mark expected switch fall-throughs
    Bugzilla: 1779768
    CVE: CVE-2019-19338
    RH-Acked-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    RH-Acked-by: Peter Xu <peterx@redhat.com>
    
    From: Vitaly Kuznetsov <vkuznets@redhat.com>
    
    From: Gustavo A. R. Silva <gustavo@embeddedor.com>
    
    In preparation to enabling -Wimplicit-fallthrough, mark switch
    cases where we are expecting to fall through.
    
    This patch fixes the following warnings:
    
    arch/x86/kvm/lapic.c:1037:27: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/lapic.c:1876:3: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/hyperv.c:1637:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/svm.c:4396:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/mmu.c:4372:36: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/x86.c:3835:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/x86.c:7938:23: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/vmx/vmx.c:2015:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    arch/x86/kvm/vmx/vmx.c:1773:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    
    Warning level 3 was used: -Wimplicit-fallthrough=3
    
    This patch is part of the ongoing efforts to enabling -Wimplicit-fallthrough.
    
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit b2869f28e1476cd705bb28c58fd01b0bd661bb99)
    
    RHEL: remove hunks that do not apply
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3241,7 +3241,7 @@ static int vmx_get_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		if (!msr_info->host_initiated &&
 		    !guest_cpuid_has(vcpu, X86_FEATURE_RDTSCP))
 			return 1;
-		/* Otherwise falls through */
+		/* Else, falls through */
 	default:
 		msr = find_msr_entry(to_vmx(vcpu), msr_info->index);
 		if (msr) {
@@ -3403,7 +3403,7 @@ static int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		/* Check reserved bit, higher 32 bits should be zero */
 		if ((data >> 32) != 0)
 			return 1;
-		/* Otherwise falls through */
+		/* Else, falls through */
 	default:
 		msr = find_msr_entry(vmx, msr_index);
 		if (msr) {
