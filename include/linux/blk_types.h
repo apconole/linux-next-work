apconole/linux-next-work

commit ee30b97bb3ee24ade47eebe4370a57dc9082a74d

[block] blk-mq: mark request as REQ_TIMEOUT when .timeout() is called
Message-id: <20190919145016.12111-4-ming.lei@redhat.com>
Patchwork-id: 272542
O-Subject: [RHEL7.8 V2 3/4] blk-mq: mark request as REQ_TIMEOUT when .timeout() is called
Bugzilla: 1750202
RH-Acked-by: Gopal Tiwari <gtiwari@redhat.com>
RH-Acked-by: David Milburn <dmilburn@redhat.com>

BZ: 1750202
Upstream Status: RHEL only

For RHEL7, timedout request is always completed by block layer after
->timeout() is done, which is different with upstream in which
timedout request is completely handled by driver in case of
BLK_EH_HANDLED.

Add one request flag for driver to know if this request has been
timed out, so that driver can keep same timeout handling as
upstream.

Signed-off-by: Ming Lei <ming.lei@redhat.com>
Signed-off-by: Jan Stancek <jstancek@redhat.com>

include/linux/blk_types.h
@@ -211,6 +211,7 @@ enum rq_flag_bits {
 	__REQ_NO_TIMEOUT,	/* requests may never expire */
 #else
 	__REQ_STATS,
+	__REQ_TIMEOUT = __REQ_END, /* request is timed out, transisent state */
 #endif
 	__REQ_NR_BITS,		/* stops here */
 };
@@ -267,6 +268,9 @@ enum rq_flag_bits {
 #define REQ_MQ_INFLIGHT		(1ULL << __REQ_MQ_INFLIGHT)
 /* IO stats tracking on */
 #define REQ_STATS		(1ULL << __REQ_STATS)
+
+/* set when this request is timed out */
+#define REQ_TIMEOUT		(1ULL << __REQ_TIMEOUT)
 
 enum req_op {
 	REQ_OP_READ,

block/blk-mq.c
@@ -815,8 +815,11 @@ void blk_mq_rq_timed_out(struct request *req, bool reserved)
 	if (!test_bit(REQ_ATOM_STARTED, &req->atomic_flags))
 		return;
 
-	if (ops->timeout)
+	if (ops->timeout) {
+		req->cmd_flags |= REQ_TIMEOUT;
 		ret = ops->timeout(req, reserved);
+		req->cmd_flags &= ~REQ_TIMEOUT;
+	}
 
 	switch (ret) {
 	case BLK_EH_HANDLED:

commit 25163de7fa68237d23f645954a6b5207b0ebdb87

[block] block: don't change REQ_NR_BITS
Message-id: <20191205071600.22405-1-ming.lei@redhat.com>
Patchwork-id: 291149
O-Subject: [RHEL7.8] block: don't change REQ_NR_BITS
Bugzilla: 1779712
RH-Acked-by: Ewan Milne <emilne@redhat.com>
RH-Acked-by: Jeff Moyer <jmoyer@redhat.com>

BZ: 1779712
Upstream Status: RHEL only
BREW: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=25165180

ee30b97bb3ee ("[block] blk-mq: mark request as REQ_TIMEOUT when .timeout()
is called") re-uses __REQ_END for __REQ_TIMEOUT. However this way causes
the following __REQ_NR_BITS to be changed as __REQ_HASHED.

This way can cause build warning, and there is risk for breaking 3rd
party modules if these modules uses REQ_NR_BITS even though the risk
is quite small given there isn't reason to use REQ_NR_BITS for drivers.

Fixes: ee30b97bb3ee ("[block] blk-mq: mark request as REQ_TIMEOUT when .timeout() is called")
Signed-off-by: Ming Lei <ming.lei@redhat.com>
Signed-off-by: Jan Stancek <jstancek@redhat.com>

include/linux/blk_types.h
@@ -211,13 +211,15 @@ enum rq_flag_bits {
 	__REQ_KERNEL, 		/* direct IO to kernel pages */
 	__REQ_PM,		/* runtime pm request */
 	__REQ_END,		/* OBSOLETE */
+#ifndef __GENKSYMS__
+	__REQ_TIMEOUT = __REQ_END, /* request is timed out, transisent state */
+#endif
 	__REQ_HASHED,		/* on IO scheduler merge hash */
 	__REQ_MQ_INFLIGHT,	/* track inflight for MQ */
 #ifdef __GENKSYMS__
 	__REQ_NO_TIMEOUT,	/* requests may never expire */
 #else
 	__REQ_STATS,
-	__REQ_TIMEOUT = __REQ_END, /* request is timed out, transisent state */
 #endif
 	__REQ_NR_BITS,		/* stops here */
 };