0ae49a8c8031f5a9297c114d23fbb6621539727c
Philipp Rudo <prudo@redhat.com>
2020-02-21 17:35:24 -0500

    [s390] s390/diag: add tracepoint for diagnose calls
    Message-id: <20200221173524.123838-3-prudo@redhat.com>
    Patchwork-id: 295257
    Patchwork-instance: patchwork
    O-Subject: [RHEL7.9 PATCH 2/2] [s390] s390/diag: add tracepoint for diagnose calls
    Bugzilla: 1805195
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    RH-Acked-by: Jarod Wilson <jarod@redhat.com>
    RH-Acked-by: Steve Best <sbest@redhat.com>
    
    Bugzilla: 1805195
    Upstream Status: kernel/git/torvalds/linux.git
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=26650106
    Tested: by IBM
    Conflicts: None
    
    commit b5a6b71b1901b9ca495f669c9ad86f2181960aba
    Author: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Date:   Fri Aug 21 16:05:32 2015 +0200
    
        s390/diag: add tracepoint for diagnose calls
    
        To be able to analyse problems in regard to hypervisor overhead
        add a tracepoing for diagnose calls. It reports the number of
        the diagnose issued, e.g.
    
                    sshd-1385  [002] ....    42.701431: diagnose: nr=0x9c
                  <idle>-0     [001] ..s.    43.587528: diagnose: nr=0x9c
    
        Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Signed-off-by: Philipp Rudo <prudo@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- arch/s390/kernel/smp.c
+++ arch/s390/kernel/smp.c
@@ -438,11 +438,11 @@ void smp_yield(void)
 void smp_yield_cpu(int cpu)
 {
 	if (MACHINE_HAS_DIAG9C) {
-		diag_stat_inc(DIAG_STAT_X09C);
+		diag_stat_inc_norecursion(DIAG_STAT_X09C);
 		asm volatile("diag %0,0,0x9c"
 			     : : "d" (pcpu_devices[cpu].address));
 	} else if (MACHINE_HAS_DIAG44) {
-		diag_stat_inc(DIAG_STAT_X044);
+		diag_stat_inc_norecursion(DIAG_STAT_X044);
 		asm volatile("diag 0,0,0x44");
 	}
 }
