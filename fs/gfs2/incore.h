c2cb1c8092f85fb434bf9ff6ca66f25b4fd6f016
Bob Peterson <rpeterso@redhat.com>
2020-05-13 13:16:05 -0500

    gfs2: log error reform
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1364234
    Upstream Status: Upstream since 5.7
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=35999660
    Tested: On my bob7-0{1..3} cluster on host fs-i40c-02 using revolver
    Conflicts: SDF constant rework due to freeze/thaw differences.
    
    commit 036330c914365f449ead353ef152fb29411cd4cb
    Author: Bob Peterson <rpeterso@redhat.com>
    Date:   Wed Apr 10 11:46:35 2019 -0600
    
        gfs2: log error reform
    
        Before this patch, gfs2 kept track of journal io errors in two
        places sd_log_error and the SDF_AIL1_IO_ERROR flag in sd_flags.
        This patch consolidates the two into sd_log_error so that it
        reflects the first error encountered writing to the journal.
        In future patches, we will take advantage of this by checking
        this value rather than having to check both when reacting to
        io errors.
    
        In addition, this fixes a tight loop in unmount: If buffers
        get on the ail1 list and an io error occurs elsewhere, the
        ail1 list would never be cleared because they were always busy.
        So unmount would hang, waiting for the ail1 list to empty.
    
        Signed-off-by: Bob Peterson <rpeterso@redhat.com>
        Reviewed-by: Andreas Gruenbacher <agruenba@redhat.com>
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>

--- fs/gfs2/incore.h
+++ fs/gfs2/incore.h
@@ -588,7 +588,6 @@ enum {
 	SDF_RORECOVERY		= 7, /* read only recovery */
 	SDF_SKIP_DLM_UNLOCK	= 8,
 	SDF_FORCE_AIL_FLUSH     = 9,
-	SDF_AIL1_IO_ERROR	= 10,
 	SDF_WITHDRAWING		= 11, /* Will withdraw eventually */
 };
 
@@ -790,7 +789,7 @@ struct gfs2_sbd {
 	atomic_t sd_log_in_flight;
 	struct bio *sd_log_bio;
 	wait_queue_head_t sd_log_flush_wait;
-	int sd_log_error;
+	int sd_log_error; /* First log error */
 
 	unsigned int sd_log_flush_head;
 	u64 sd_log_flush_wrapped;
