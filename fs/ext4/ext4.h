e6653825daad9ab763c8a692586817a9de48d9b2
Lukas Czerner <lczerner@redhat.com>
2020-05-22 08:37:51 -0400

    [fs] ext4: limit number of scanned extents in status tree shrinker
    Message-id: <20200522083754.21822-4-lczerner@redhat.com>
    Patchwork-id: 313612
    Patchwork-instance: patchwork
    O-Subject: [RHEL7.9 fs PATCH 3/6] ext4: limit number of scanned extents in status tree shrinker
    Bugzilla: 1663720
    RH-Acked-by: Eric Sandeen <esandeen@redhat.com>
    RH-Acked-by: Dave Wysochanski <dwysocha@redhat.com>
    
    From: Jan Kara <jack@suse.cz>
    
    BZ 1663720
    https://bugzilla.redhat.com/show_bug.cgi?id=1663720
    
    BREW 28739688
    https://brewweb.devel.redhat.com/taskinfo?taskID=28739688
    
    commit dd4759255188771e60cf3455982959a1ba04f4eb
    
    Currently we scan extent status trees of inodes until we reclaim nr_to_scan
    extents. This can however require a lot of scanning when there are lots
    of delayed extents (as those cannot be reclaimed).
    
    Change shrinker to work as shrinkers are supposed to and *scan* only
    nr_to_scan extents regardless of how many extents did we actually
    reclaim. We however need to be careful and avoid scanning each status
    tree from the beginning - that could lead to a situation where we would
    not be able to reclaim anything at all when first nr_to_scan extents in
    the tree are always unreclaimable. We remember with each inode offset
    where we stopped scanning and continue from there when we next come
    across the inode.
    
    Note that we also need to update places calling __es_shrink() manually
    to pass reasonable nr_to_scan to have a chance of reclaiming anything and
    not just 1.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -955,6 +955,9 @@ struct ext4_inode_info {
 	rwlock_t i_es_lock;
 	struct list_head i_es_list;
 	unsigned int i_es_shk_nr;	/* protected by i_es_lock */
+	ext4_lblk_t i_es_shrink_lblk;	/* Offset where we start searching for
+					   extents to shrink. Protected by
+					   i_es_lock  */
 
 	/* ialloc */
 	ext4_group_t	i_last_alloc_group;
@@ -1392,7 +1395,7 @@ struct ext4_sb_info {
 
 	/* Reclaim extents from extent status tree */
 	struct shrinker s_es_shrinker;
-	struct list_head s_es_list;
+	struct list_head s_es_list;	/* List of inodes with reclaimable extents */
 	long s_es_nr_inode;
 	struct percpu_counter s_extent_cache_cnt;
 	spinlock_t s_es_lock ____cacheline_aligned_in_smp;
