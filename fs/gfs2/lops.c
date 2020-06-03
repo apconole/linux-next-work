2c4c7547f9b81e7c200e2499007a6a02cc716237
Abhijith Das <adas@redhat.com>
2020-06-03 04:46:59 -0400

    [fs] gfs2: Even more gfs2_find_jhead fixes
    Message-id: <20200603044700.14389-2-adas@redhat.com>
    Patchwork-id: 316296
    Patchwork-instance: patchwork
    O-Subject: [RHEL7 fs PATCH addendum 1/2] gfs2: Even more gfs2_find_jhead fixes
    Bugzilla: 1828454
    RH-Acked-by: Andrew Price <anprice@redhat.com>
    RH-Acked-by: Andreas Grunbacher <agruenba@redhat.com>
    
    Bugzilla: 1828454
    Upstream Status: 5.7
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=29019920
    Tested: QE tests
    
    A previous patch for this bz introduced a regression where filesystems with
    blocksize < PAGE_SIZE were failing. This is an addendum patch that fixes the
    issue.
    
    commit 20be493b787cd581c9fffad7fcd6bfbe6af1050c
    Author: Andreas Gruenbacher <agruenba@redhat.com>
    Date:   Tue May 26 20:11:51 2020 +0200
    
        gfs2: Even more gfs2_find_jhead fixes
    
        Fix several issues in the previous gfs2_find_jhead fix:
        * When updating @blocks_submitted, @block refers to the first block block not
          submitted yet, not the last block submitted, so fix an off-by-one error.
        * We want to ensure that @blocks_submitted is far enough ahead of @blocks_read
          to guarantee that there is in-flight I/O.  Otherwise, we'll eventually end up
          waiting for pages that haven't been submitted, yet.
        * It's much easier to compare the number of blocks added with the number of
          blocks submitted to limit the maximum bio size.
        * Even with bio chaining, we can keep adding blocks until we reach the maximum
          bio size, as long as we stop at a page boundary.  This simplifies the logic.
    
        Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
        Reviewed-by: Bob Peterson <rpeterso@redhat.com>
    
    Signed-off-by: Abhi Das <adas@redhat.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- fs/gfs2/lops.c
+++ fs/gfs2/lops.c
@@ -510,12 +510,12 @@ int gfs2_find_jhead(struct gfs2_jdesc *jd, struct gfs2_log_header_host *head,
 	unsigned int bsize = sdp->sd_sb.sb_bsize, off = 0;
 	unsigned int bsize_shift = sdp->sd_sb.sb_bsize_shift;
 	unsigned int shift = PAGE_SHIFT - bsize_shift;
-	unsigned int max_bio_size = 2 * 1024 * 1024;
+	unsigned int max_blocks = 2 * 1024 * 1024 >> bsize_shift;
 	struct gfs2_journal_extent *je;
 	int sz, ret = 0;
 	struct bio *bio = NULL;
 	struct page *page = NULL;
-	bool bio_chained = false, done = false;
+	bool done = false;
 
 	memset(head, 0, sizeof(*head));
 	if (list_empty(&jd->extent_list))
@@ -536,10 +536,7 @@ int gfs2_find_jhead(struct gfs2_jdesc *jd, struct gfs2_log_header_host *head,
 				off = 0;
 			}
 
-			if (!bio || (bio_chained && !off) ||
-			    bio->bi_size >= max_bio_size) {
-				/* start new bio */
-			} else {
+			if (bio && (off || block < blocks_submitted + max_blocks)) {
 				sector_t sector = dblock << sdp->sd_fsb2bb_shift;
 
 				if (bio_end_sector(bio) == sector) {
@@ -552,18 +549,16 @@ int gfs2_find_jhead(struct gfs2_jdesc *jd, struct gfs2_log_header_host *head,
 						(PAGE_SIZE - off) >> bsize_shift;
 
 					bio = gfs2_chain_bio(bio, blocks);
-					bio_chained = true;
 					goto add_block_to_new_bio;
 				}
 			}
 
 			if (bio) {
-				blocks_submitted = block + 1;
+				blocks_submitted = block;
 				submit_bio(READ, bio);
 			}
 
 			bio = gfs2_log_alloc_bio(sdp, dblock, gfs2_end_log_read);
-			bio_chained = false;
 add_block_to_new_bio:
 			sz = bio_add_page(bio, page, bsize, off);
 			BUG_ON(sz != bsize);
@@ -571,7 +566,7 @@ block_added:
 			off += bsize;
 			if (off == PAGE_SIZE)
 				page = NULL;
-			if (blocks_submitted < 2 * max_bio_size >> bsize_shift) {
+			if (blocks_submitted <= blocks_read + max_blocks) {
 				/* Keep at least one bio in flight */
 				continue;
 			}
