25077b49b47c1cdf224b54c837172ff820e8be88
Donghai Qiao <dqiao@redhat.com>
2021-05-20 16:30:16 -0400

    futex: Provide and use pi_state_update_owner()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1935108
    Upstream status: c5cade200ab9a2a3be9e7f32a752c8d86b502ec7
    CVE: CVE-2021-3347
    
    Conflicts:
    Updating the owner of pi_state requires that we remove the pi_state structure from
    the old owner's pi_state_list then add it to the new owner's pi_state_list. Because
    this action takes place in multiple occassions in the current upstream futex.c, so
    the similar code is duplicated in all these places. The purpose of this patch is to
    eliminate these code duplications with a new routine pi_state_update_owner().
    
    The conflicts in 7.9.z are caused by the differences in places where updating owner
    takes place. After sorting out the details, the relevant commit IDs as below :
    
    734009e96d19 ("futex: Change locking rules")
    b4abf91047cf ("rtmutex: Make wait_lock irq safe")
    
    commit c5cade200ab9a2a3be9e7f32a752c8d86b502ec7
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Tue, 19 Jan 2021 15:21:35 +0100
    
        futex: Provide and use pi_state_update_owner()
    
        Updating pi_state::owner is done at several places with the same
        code. Provide a function for it and use that at the obvious places.
    
        This is also a preparation for a bug fix to avoid yet another copy of the
        same code or alternatively introducing a completely unpenetratable mess of
        gotos.
    
        Originally-by: Peter Zijlstra <peterz@infradead.org>
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Cc: stable@vger.kernel.org
    
    Signed-off-by: Donghai Qiao <dqiao@redhat.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -640,6 +640,29 @@ static struct futex_pi_state * alloc_pi_state(void)
 	return pi_state;
 }
 
+static void pi_state_update_owner(struct futex_pi_state *pi_state,
+				  struct task_struct *new_owner)
+{
+	struct task_struct *old_owner = pi_state->owner;
+
+	lockdep_assert_held(&pi_state->pi_mutex.wait_lock);
+
+	if (old_owner) {
+		raw_spin_lock_irq(&old_owner->pi_lock);
+		WARN_ON(list_empty(&pi_state->list));
+		list_del_init(&pi_state->list);
+		raw_spin_unlock_irq(&old_owner->pi_lock);
+	}
+
+	if (new_owner) {
+		raw_spin_lock_irq(&new_owner->pi_lock);
+		WARN_ON(!list_empty(&pi_state->list));
+		list_add(&pi_state->list, &new_owner->pi_state_list);
+		pi_state->owner = new_owner;
+		raw_spin_unlock_irq(&new_owner->pi_lock);
+	}
+}
+
 static void free_pi_state(struct futex_pi_state *pi_state)
 {
 	if (!atomic_dec_and_test(&pi_state->refcount))
@@ -650,10 +673,7 @@ static void free_pi_state(struct futex_pi_state *pi_state)
 	 * and has cleaned up the pi_state already
 	 */
 	if (pi_state->owner) {
-		raw_spin_lock_irq(&pi_state->owner->pi_lock);
-		list_del_init(&pi_state->list);
-		raw_spin_unlock_irq(&pi_state->owner->pi_lock);
-
+		pi_state_update_owner(pi_state, NULL);
 		rt_mutex_proxy_unlock(&pi_state->pi_mutex, pi_state->owner);
 	}
 
@@ -1168,16 +1188,7 @@ static int wake_futex_pi(u32 __user *uaddr, u32 uval, struct futex_q *this)
 		return ret;
 	}
 
-	raw_spin_lock_irq(&pi_state->owner->pi_lock);
-	WARN_ON(list_empty(&pi_state->list));
-	list_del_init(&pi_state->list);
-	raw_spin_unlock_irq(&pi_state->owner->pi_lock);
-
-	raw_spin_lock_irq(&new_owner->pi_lock);
-	WARN_ON(!list_empty(&pi_state->list));
-	list_add(&pi_state->list, &new_owner->pi_state_list);
-	pi_state->owner = new_owner;
-	raw_spin_unlock_irq(&new_owner->pi_lock);
+	pi_state_update_owner(pi_state, new_owner);
 
 	raw_spin_unlock(&pi_state->pi_mutex.wait_lock);
 	rt_mutex_unlock(&pi_state->pi_mutex);
@@ -1953,19 +1964,7 @@ retry:
 	 * We fixed up user space. Now we need to fix the pi_state
 	 * itself.
 	 */
-	if (pi_state->owner != NULL) {
-		raw_spin_lock_irq(&pi_state->owner->pi_lock);
-		WARN_ON(list_empty(&pi_state->list));
-		list_del_init(&pi_state->list);
-		raw_spin_unlock_irq(&pi_state->owner->pi_lock);
-	}
-
-	pi_state->owner = newowner;
-
-	raw_spin_lock_irq(&newowner->pi_lock);
-	WARN_ON(!list_empty(&pi_state->list));
-	list_add(&pi_state->list, &newowner->pi_state_list);
-	raw_spin_unlock_irq(&newowner->pi_lock);
+	pi_state_update_owner(pi_state, newowner);
 
 	return newowner == current;
 
