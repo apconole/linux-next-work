e393267fbdf544732e2cb83c6ff232688b56eb92
Carlos Maiolino <cmaiolino@redhat.com>
2022-01-11 10:42:17 +0100

    buffer: eliminate the need to call free_more_memory() in __getblk_slow()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2030609
    Tested: By the customer
    Conflicts:
    	- fs/buffer.c - free_more_memory() implementation diverges from
    	  upstream, causing a small conflict.
    
    Since the previous commit removed any case where grow_buffers()
    would return failure due to memory allocations, we can safely
    remove the case where we have to call free_more_memory() in
    this function.
    
    Since this is also the last user of free_more_memory(), kill
    it off completely.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit bc48f001de12225b6430a243504aa60b5ae8a91a)
    
    Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>

--- fs/buffer.c
+++ fs/buffer.c
@@ -254,27 +254,6 @@ out:
 }
 
 /*
- * Kick the writeback threads then try to free up some ZONE_NORMAL memory.
- */
-static void free_more_memory(void)
-{
-	struct zone *zone;
-	int nid;
-
-	wakeup_flusher_threads(1024, WB_REASON_FREE_MORE_MEM);
-	yield();
-
-	for_each_online_node(nid) {
-		(void)first_zones_zonelist(node_zonelist(nid, GFP_NOFS),
-						gfp_zone(GFP_NOFS), NULL,
-						&zone);
-		if (zone)
-			try_to_free_pages(node_zonelist(nid, GFP_NOFS), 0,
-						GFP_NOFS, NULL);
-	}
-}
-
-/*
  * I/O completion handler for block_read_full_page() - pages
  * which come unlocked at the end of I/O.
  */
@@ -1053,8 +1032,6 @@ __getblk_slow(struct block_device *bdev, sector_t block, int size)
 		ret = grow_buffers(bdev, block, size);
 		if (ret < 0)
 			return NULL;
-		if (ret == 0)
-			free_more_memory();
 	}
 }
 
